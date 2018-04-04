5e6f04491b4966333f86c34bb897cd85e4ebc1d1
candirufish <buras007@mail2world.com>
2018-04-03 21:04:53 -0700

    skip prunning take 2

--- src/search.cpp
+++ src/search.cpp
@@ -892,12 +892,12 @@ moves_loop: // When in check, search starts from here
       // Step 14. Pruning at shallow depth (~170 Elo)
       if (  !rootNode
           && pos.non_pawn_material(pos.side_to_move())
-		  && !pos.pawn_on_7th()
           && bestValue > VALUE_MATED_IN_MAX_PLY)
       {
           if (   !captureOrPromotion
               && !givesCheck
-              && (!pos.advanced_pawn_push(move) || pos.non_pawn_material() >= Value(5000)))
+              && (!pos.advanced_pawn_push(move) || pos.non_pawn_material() >= Value(5000))
+			  && !pos.pawn_on_7th())
           {
               // Move count based pruning (~30 Elo)
               if (moveCountPruning)
