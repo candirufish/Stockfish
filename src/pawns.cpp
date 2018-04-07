8873da2962b8e5718c17664835d1d3b7ddf9b6d9
candirufish <buras007@mail2world.com>
2018-04-07 01:38:28 -0700

    less bonus on kingside castle

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -287,7 +287,7 @@ Score Entry::do_king_safety(const Position& pos, Square ksq) {
 
   // If we can castle use the bonus after the castling if it is bigger
   if (pos.can_castle(MakeCastling<Us, KING_SIDE>::right))
-      bonus = std::max(bonus, shelter_storm<Us>(pos, relative_square(Us, SQ_G1)));
+      bonus = std::max(bonus, shelter_storm<Us>(pos, relative_square(Us, SQ_G1))-12);
 
   if (pos.can_castle(MakeCastling<Us, QUEEN_SIDE>::right))
       bonus = std::max(bonus, shelter_storm<Us>(pos, relative_square(Us, SQ_C1)));
