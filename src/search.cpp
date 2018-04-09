46f499349d52d1ba7ec5ca83f23941e647c324c7
candirufish <buras007@mail2world.com>
2018-04-08 21:53:46 -0700

    reset stat take 3

--- src/search.cpp
+++ src/search.cpp
@@ -1085,7 +1085,8 @@ moves_loop: // When in check, search starts from here
               else
               {
                   assert(value >= beta); // Fail high
-                  ss->statScore = std::max(ss->statScore, 0);
+				  if (moveCountPruning)
+						ss->statScore = std::max(ss->statScore, 0);
                   break;
               }
           }
