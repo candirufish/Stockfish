667a63c9eaffc7cef6c7153baddbb991325d095b
candirufish <buras007@mail2world.com>
2018-05-19 10:21:18 -0700

    try using contHist 3 on countermove pruning

--- src/search.cpp
+++ src/search.cpp
@@ -943,7 +943,8 @@ moves_loop: // When in check, search starts from here
               // Countermoves based pruning (~20 Elo)
               if (   lmrDepth < 3
                   && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
-                  && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
+                  && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
+				  && (*contHist[3])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
                   continue;
 
               // Futility pruning: parent node (~2 Elo)
