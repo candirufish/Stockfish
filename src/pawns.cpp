ab2fb077d6a5feb087b37b01b80308002931f38c
candirufish <buras007@mail2world.com>
2018-05-18 21:45:02 -0700

    try penalty 15 for both sides

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -260,10 +260,10 @@ Score Entry::do_king_safety(const Position& pos, Square ksq) {
 
   // If we can castle use the bonus after the castling if it is bigger
   if (pos.can_castle(MakeCastling<Us, KING_SIDE>::right))
-      bonus = std::max(bonus, evaluate_shelter<Us>(pos, relative_square(Us, SQ_G1)) - Value(16));
+      bonus = std::max(bonus, evaluate_shelter<Us>(pos, relative_square(Us, SQ_G1)) - Value(15));
 
   if (pos.can_castle(MakeCastling<Us, QUEEN_SIDE>::right))
-      bonus = std::max(bonus, evaluate_shelter<Us>(pos, relative_square(Us, SQ_C1)) - Value(12));
+      bonus = std::max(bonus, evaluate_shelter<Us>(pos, relative_square(Us, SQ_C1)) - Value(15));
 
   return make_score(bonus, -16 * minKingPawnDistance);
 }
