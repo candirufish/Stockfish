d42d30d0462a1568a95818de317eb1ae2b5af8d8
candirufish <buras007@mail2world.com>
2018-05-21 16:41:44 -0700

    cap the delta to max 20

--- src/search.cpp
+++ src/search.cpp
@@ -380,7 +380,9 @@ void Thread::search() {
           if (rootDepth >= 5 * ONE_PLY)
           {
               Value previousScore = rootMoves[PVIdx].previousScore;
-			  delta = Value(rootDepth > 12 ? 18 : 14 + researchesLastRootDepth);
+			  delta = Value(16 + researchesLastRootDepth);
+			  if (delta > 20)
+				  delta = Value(20);
 			  
               alpha = std::max(previousScore - delta,-VALUE_INFINITE);
               beta  = std::min(previousScore + delta, VALUE_INFINITE);
