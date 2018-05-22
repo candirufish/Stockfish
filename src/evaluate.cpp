fc292ac3e22393f7b4922e862f5aa5a4b8ea429e
candirufish <buras007@mail2world.com>
2018-05-22 14:00:52 -0700

    bonus for pawn/rook vs pawnless

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -652,6 +652,11 @@ namespace {
 
         int r = relative_rank(Us, s);
         int w = PassedDanger[r];
+		
+		if (pos.count<PAWN>(Them) == 0
+		&& pos.count<PAWN>(Us) > 1
+		&& pos.count<ROOK>(Us) > 1)
+		w += 1;
 
         Score bonus = PassedRank[r];
 
