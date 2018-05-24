1a6930a6df78e65bc0974d4d0568e99e894f76af
candirufish <buras007@mail2world.com>
2018-05-24 12:24:04 -0700

    don't reduce cut nodes when in check

--- src/search.cpp
+++ src/search.cpp
@@ -1019,7 +1019,7 @@ moves_loop: // When in check, search starts from here
 
               // Increase reduction for cut nodes (~5 Elo)
               if (cutNode)
-                  r += 2 * ONE_PLY;
+                  r += (inCheck ? 0 : 2) * ONE_PLY;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
