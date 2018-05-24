b28216a78d0a3bb7ac85c49a222cfa2d00d2dcde
candirufish <buras007@mail2world.com>
2018-05-24 16:06:16 -0700

    decrease only if advance pawn is in vicinity of king

--- src/search.cpp
+++ src/search.cpp
@@ -1021,7 +1021,11 @@ moves_loop: // When in check, search starts from here
 
               // Increase reduction for cut nodes (~5 Elo)
               if (cutNode)
-			      r += (AdvPwnPush ? 1 : 2) * ONE_PLY;
+			      r += 2 * ONE_PLY;
+			  
+			  if (AdvPwnPush
+			  && distance(pos.square<KING>(pos.side_to_move()), to_sq(move)) <= 3)
+			      r -= ONE_PLY;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
