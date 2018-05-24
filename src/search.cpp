44e31e973b715ad0a2fabdde3ef7a8f412e3d936
candirufish <buras007@mail2world.com>
2018-05-24 15:39:11 -0700

    decrease less if cutnode is advance pawn push

--- src/search.cpp
+++ src/search.cpp
@@ -1021,10 +1021,7 @@ moves_loop: // When in check, search starts from here
 
               // Increase reduction for cut nodes (~5 Elo)
               if (cutNode)
-                  r += 2 * ONE_PLY;
-			  
-			  if (AdvPwnPush)
-				  r -= ONE_PLY;
+			      r += (AdvPwnPush ? 1 : 2) * ONE_PLY;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
