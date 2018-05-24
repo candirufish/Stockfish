dc5e205a89e8c29092c1b7ff500643133e3bc639
candirufish <buras007@mail2world.com>
2018-05-24 15:04:29 -0700

    treat advance pawn push like capture in lmr

--- src/search.cpp
+++ src/search.cpp
@@ -540,7 +540,7 @@ namespace {
     Depth extension, newDepth;
     Value bestValue, value, ttValue, eval, maxValue;
     bool ttHit, inCheck, givesCheck, improving;
-    bool captureOrPromotion, doFullDepthSearch, moveCountPruning, skipQuiets, ttCapture, pvExact;
+    bool captureOrPromotion, doFullDepthSearch, moveCountPruning, skipQuiets, ttCapture, pvExact, AdvPwnPush;
     Piece movedPiece;
     int moveCount, captureCount, quietCount;
 
@@ -891,6 +891,8 @@ moves_loop: // When in check, search starts from here
 
       moveCountPruning =   depth < 16 * ONE_PLY
                         && moveCount >= FutilityMoveCounts[improving][depth / ONE_PLY];
+						
+	  AdvPwnPush = pos.advanced_pawn_push(move);
 
       // Step 13. Extensions (~70 Elo)
 
@@ -931,7 +933,7 @@ moves_loop: // When in check, search starts from here
       {
           if (   !captureOrPromotion
               && !givesCheck
-              && (!pos.advanced_pawn_push(move) || pos.non_pawn_material() >= Value(5000)))
+              && (!AdvPwnPush || pos.non_pawn_material() >= Value(5000)))
           {
               // Move count based pruning (~30 Elo)
               if (moveCountPruning)
@@ -994,7 +996,7 @@ moves_loop: // When in check, search starts from here
       {
           Depth r = reduction<PvNode>(improving, depth, moveCount);
 
-          if (captureOrPromotion) // (~5 Elo)
+          if (captureOrPromotion || AdvPwnPush) // (~5 Elo)
           {
               //Increase reduction by comparing opponent's stat score
               if (   (ss-1)->statScore >= 0 
