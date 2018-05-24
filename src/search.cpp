e62136306015bae77baff4e080c42ed57327a63c
candirufish <buras007@mail2world.com>
2018-05-24 14:40:11 -0700

    don't replace stat with cap history

--- src/search.cpp
+++ src/search.cpp
@@ -997,10 +997,11 @@ moves_loop: // When in check, search starts from here
           if (captureOrPromotion) // (~5 Elo)
           {
               //Increase or decrease reduction by comparing opponent's stat score
-			  ss->statScore = thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())];
-			  if ((ss - 1)->statScore >= 0 && ss->statScore < 0)
+			  if ((ss - 1)->statScore >= 0 
+			  && thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] < 0)
 				r += ONE_PLY;
-			  else if (ss->statScore >= 0 && (ss - 1)->statScore < 0)
+			  else if (thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] >= 0 
+			  && (ss - 1)->statScore < 0)
 				r -= ONE_PLY;
                             
               r -= r ? ONE_PLY : DEPTH_ZERO;
