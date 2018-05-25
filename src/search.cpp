66a5c2cd28aa25c09bb93fad2cb03e8b04fbe534
candirufish <buras007@mail2world.com>
2018-05-25 12:12:49 -0700

    reduce reduction for see moves

--- src/search.cpp
+++ src/search.cpp
@@ -1000,6 +1000,9 @@ moves_loop: // When in check, search starts from here
               if (   (ss-1)->statScore >= 0 
                   && thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] < 0)
                   r += ONE_PLY;
+				  
+			  if (pos.see_ge(move))
+				  r -= ONE_PLY;
                             
               r -= r ? ONE_PLY : DEPTH_ZERO;
           }
