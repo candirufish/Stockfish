8c870751f20322793c000090b507fd122f87c0dc
candirufish <buras007@mail2world.com>
2018-05-27 10:57:27 -0700

    only increase stat reduction at non pvnode

--- src/search.cpp
+++ src/search.cpp
@@ -1038,10 +1038,10 @@ moves_loop: // When in check, search starts from here
               if (ss->statScore >= 0 && (ss-1)->statScore < 0)
                   r -= ONE_PLY;
 
-              else if ((ss-1)->statScore >= 0 && ss->statScore < 0)
+              else if ((ss-1)->statScore >= 0 && ss->statScore < 0 && !pvExact)
                   r += ONE_PLY;
 			  
-			  else if  ((ss-2)->statScore < 0 && (ss)->statScore < (ss-2)->statScore)
+			  else if  ((ss-2)->statScore < 0 && (ss)->statScore < (ss-2)->statScore && !pvExact)
 				  r += ONE_PLY;
 
               // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
