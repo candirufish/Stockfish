ef1e8f5bc537c9342bdd41adcc44cb786b30f15c
candirufish <buras007@mail2world.com>
2018-05-28 12:06:22 -0700

    set statscore then reduce if improving

--- src/search.cpp
+++ src/search.cpp
@@ -996,10 +996,15 @@ moves_loop: // When in check, search starts from here
 
           if (captureOrPromotion) // (~5 Elo)
           {
+			  ss->statScore = thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] - 8000;
+			  
               //Increase reduction by comparing opponent's stat score
               if (   (ss-1)->statScore >= 0 
                   && thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] < 0)
                   r += ONE_PLY;
+				  
+			  if  ((ss-2)->statScore > 0 && (ss)->statScore > (ss - 2)->statScore)
+				  r -= ONE_PLY;
                             
               r -= r ? ONE_PLY : DEPTH_ZERO;
           }
