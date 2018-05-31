d7cdfce1eeb7fafe921234b831ccc2a22dcf89fa
candirufish <buras007@mail2world.com>
2018-05-31 11:18:25 -0700

    increase cut node based on opponent statscore

--- src/search.cpp
+++ src/search.cpp
@@ -1013,12 +1013,12 @@ moves_loop: // When in check, search starts from here
                   r -= ONE_PLY;
 
               // Increase reduction if ttMove is a capture (~0 Elo)
-              if (ttCapture && (ss-1)->statScore >= 0)
+              if (ttCapture)
                   r += ONE_PLY;
 
               // Increase reduction for cut nodes (~5 Elo)
               if (cutNode)
-                  r += 2 * ONE_PLY;
+				  r += (ss-1)->statScore >= 0 ? 2 * ONE_PLY : ONE_PLY;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
