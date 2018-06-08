1969bc3b7301aee21d38cce8a4b6d29b112eb940
candirufish <buras007@mail2world.com>
2018-06-08 15:41:25 -0700

    limit check extension by depth

--- src/search.cpp
+++ src/search.cpp
@@ -918,6 +918,7 @@ moves_loop: // When in check, search starts from here
       }
       else if (    givesCheck // Check extension (~2 Elo)
                && !moveCountPruning
+		       && depth < 10
                &&  pos.see_ge(move))
           extension = ONE_PLY;
 
