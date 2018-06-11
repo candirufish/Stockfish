8b3b648ecdd5ab869cd8dbf970f83c31637d7f70
candirufish <buras007@mail2world.com>
2018-06-10 18:37:39 -0700

    also use opponent stats for calculation of reduction

--- src/search.cpp
+++ src/search.cpp
@@ -1041,7 +1041,7 @@ moves_loop: // When in check, search starts from here
                   r += ONE_PLY;
 
               // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
-              r = std::max(DEPTH_ZERO, (r / ONE_PLY - ss->statScore / 20000) * ONE_PLY);
+              r = std::max(DEPTH_ZERO, (r / ONE_PLY - (ss->statScore - (ss-1)->statScore) / 10000) * ONE_PLY);
           }
 
           Depth d = std::max(newDepth - r, ONE_PLY);
