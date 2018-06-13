99ae20d9d545dd6ee401c7b62f79d595cd381122
candirufish <buras007@mail2world.com>
2018-06-12 17:10:40 -0700

    only reduce pvnodes if stats didn't cause increase

--- src/search.cpp
+++ src/search.cpp
@@ -990,7 +990,7 @@ moves_loop: // When in check, search starts from here
               // Increase reduction by comparing opponent's stat score
               if ((ss-1)->statScore >= 0)
                   r += ONE_PLY;
-			  if (pvExact)
+			  else if (pvExact)
                   r -= r > ONE_PLY ? ONE_PLY : DEPTH_ZERO;
 
               r -= r ? ONE_PLY : DEPTH_ZERO;
