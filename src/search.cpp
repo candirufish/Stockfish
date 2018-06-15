2326c6a05fb3cdda107c0f33b09fbc53639ec007
candirufish <buras007@mail2world.com>
2018-06-15 14:01:51 -0700

    try if the see condition in ttcapture helps with lmr

--- src/search.cpp
+++ src/search.cpp
@@ -965,7 +965,7 @@ moves_loop: // When in check, search starts from here
           continue;
       }
 
-      if (move == ttMove && captureOrPromotion)
+      if (move == ttMove && captureOrPromotion && pos.see_ge(move, KnightValueMg-BishopValueMg))
           ttCapture = true;
 
       // Update the current move (this must be done after singular extension search)
@@ -987,6 +987,9 @@ moves_loop: // When in check, search starts from here
           {
               // Increase reduction by comparing opponent's stat score
               if ((ss-1)->statScore >= 0)
+                  r += ONE_PLY;
+			  
+			  if (ttCapture)
                   r += ONE_PLY;
 
               r -= r ? ONE_PLY : DEPTH_ZERO;
