ade87cc383b77c73cd2adfa0676cfd24d3cd55b6
candirufish <buras007@mail2world.com>
2018-06-15 22:20:22 -0700

    reduce ttcapture with high opponent stats

--- src/search.cpp
+++ src/search.cpp
@@ -988,6 +988,8 @@ moves_loop: // When in check, search starts from here
               // Increase reduction by comparing opponent's stat score
               if ((ss-1)->statScore >= 0)
                   r += ONE_PLY;
+			  if (ttCapture && (ss - 1)->statScore >= 20000)
+				  r += ONE_PLY;
 
               r -= r ? ONE_PLY : DEPTH_ZERO;
           }
