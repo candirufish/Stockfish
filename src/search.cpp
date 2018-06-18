1b64dad0973dde0d5955aa9cd670ed8883a071aa
candirufish <buras007@mail2world.com>
2018-06-18 13:08:01 -0700

    if capture gives check reduce reduction

--- src/search.cpp
+++ src/search.cpp
@@ -988,7 +988,10 @@ moves_loop: // When in check, search starts from here
               // Increase reduction by comparing opponent's stat score
               if ((ss-1)->statScore >= 0)
                   r += ONE_PLY;
-
+			  
+			  if (givesCheck)
+				  r -= r > ONE_PLY ? ONE_PLY : DEPTH_ZERO;
+			  
               r -= r ? ONE_PLY : DEPTH_ZERO;
           }
           else
