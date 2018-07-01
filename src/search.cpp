f91a6466cc80f7a2aab0f93a4bd869cbc1cc766c
candirufish <buras007@mail2world.com>
2018-07-01 11:14:01 -0700

    double reduction for ttcapture with stats

--- src/search.cpp
+++ src/search.cpp
@@ -991,9 +991,8 @@ moves_loop: // When in check, search starts from here
               if ((ss-1)->statScore >= 0)
                   r += ONE_PLY;
 			  
-			  else if  (ttCapture 
-			  && ((ss-2)->statScore < ss->statScore))
-			      r += ONE_PLY;
+			  else if ((ss-2)->statScore < ss->statScore)
+				  r += ttCapture ? 2 * ONE_PLY : ONE_PLY;
 			
 			
               r -= r ? ONE_PLY : DEPTH_ZERO;
