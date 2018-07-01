88d50f2482837703fee5dfea4ade420274db731f
candirufish <buras007@mail2world.com>
2018-07-01 07:20:13 -0700

    reverse condition and reduce reduction

--- src/search.cpp
+++ src/search.cpp
@@ -985,9 +985,14 @@ moves_loop: // When in check, search starts from here
 
           if (captureOrPromotion) // (~5 Elo)
           {
+			  ss->statScore = -1;
+			  
               // Increase reduction by comparing opponent's stat score
               if ((ss-1)->statScore >= 0)
                   r += ONE_PLY;
+			  
+			  else if ((ss-2)->statScore > ss->statScore)
+				  r -= r > ONE_PLY ? ONE_PLY : DEPTH_ZERO;
 
               r -= r ? ONE_PLY : DEPTH_ZERO;
           }
