35e0482719f7d5f5403316c8d298a16e5774346a
candirufish <buras007@mail2world.com>
2018-07-01 13:52:04 -0700

    using capt history for ss2 reduction

--- src/search.cpp
+++ src/search.cpp
@@ -984,12 +984,17 @@ moves_loop: // When in check, search starts from here
           Depth r = reduction<PvNode>(improving, depth, moveCount);
 
           if (captureOrPromotion) // (~5 Elo)
-          {
+          {			  			  
               // Increase reduction by comparing opponent's stat score
               if ((ss-1)->statScore >= 0)
                   r += ONE_PLY;
+			  
+			  else if ((ss-2)->statScore < (thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] - 8000))
+				  r += ONE_PLY;
 
               r -= r ? ONE_PLY : DEPTH_ZERO;
+			  
+			  ss->statScore = -1;
           }
           else
           {
