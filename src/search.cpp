a30caac800ea7c2248e155a775cd8fa0f5eef211
candirufish <buras007@mail2world.com>
2018-07-03 16:47:07 -0700

    remove min staticeval condition for endgame extension tweak

--- src/search.cpp
+++ src/search.cpp
@@ -911,6 +911,13 @@ moves_loop: // When in check, search starts from here
                && !moveCountPruning
                &&  pos.see_ge(move))
           extension = ONE_PLY;
+		  
+	   else if (pos.non_pawn_material() == 0
+       			&&  abs(ss->staticEval) <= Value(160)
+       			&&  pos.rule50_count() <= 10
+				&&  depth >= 4 * ONE_PLY 
+ 				&&  (PvNode || improving))	// Endgame extension
+       	  extension = ONE_PLY;
 
       // Calculate new depth for this move
       newDepth = depth - ONE_PLY + extension;
