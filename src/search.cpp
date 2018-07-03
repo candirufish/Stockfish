401392b328b823b29f95748ff7c7a2fe997345bf
candirufish <buras007@mail2world.com>
2018-07-03 02:47:39 -0700

    reset ss stat only we do reduction

--- src/search.cpp
+++ src/search.cpp
@@ -987,7 +987,10 @@ moves_loop: // When in check, search starts from here
           {
               // Increase reduction by comparing opponent's stat score
               if ((ss-1)->statScore >= 0)
+			  {
+				  ss->statScore = -1;
                   r += ONE_PLY;
+			  }
 
               r -= r ? ONE_PLY : DEPTH_ZERO;
           }
