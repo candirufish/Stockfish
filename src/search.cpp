11e26b8d61fca02c66ba35c30f5c3fba06d404d7
candirufish <buras007@mail2world.com>
2018-07-04 15:46:32 -0700

    try the reverse reset ss first before reductions

--- src/search.cpp
+++ src/search.cpp
@@ -984,13 +984,13 @@ moves_loop: // When in check, search starts from here
           Depth r = reduction<PvNode>(improving, depth, moveCount);
 
           if (captureOrPromotion) // (~5 Elo)
-          {
-			  ss->statScore = thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] - 8000;
+          {			  
+			  ss->statScore = -1;
 			  
               // Increase reduction by comparing opponent's stat score
-              if ((ss-1)->statScore >= 0 || !((ss-2)->statScore > 0 && (ss)->statScore > (ss - 2)->statScore))
+              if ((ss-1)->statScore >= 0 || ((ss-2)->statScore < ss->statScore))
 			  {
-				  ss->statScore = -1;
+				  ss->statScore = thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] - 8000;
                   r += ONE_PLY;
 			  }
               r -= r ? ONE_PLY : DEPTH_ZERO;
