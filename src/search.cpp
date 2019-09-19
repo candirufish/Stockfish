eccfde7f97eb3dcbc77ba4fb0016cc38d6be55ce
candirufish <38038147+candirufish@users.noreply.github.com>
2019-09-19 20:52:07 +0200

    reduce lmr for passpawn

--- src/search.cpp
+++ src/search.cpp
@@ -597,7 +597,7 @@ namespace {
     Depth extension, newDepth;
     Value bestValue, value, ttValue, eval, maxValue;
     bool ttHit, ttPv, inCheck, givesCheck, improving, doLMR;
-    bool captureOrPromotion, doFullDepthSearch, moveCountPruning, ttCapture;
+    bool captureOrPromotion, doFullDepthSearch, moveCountPruning, ttCapture, PassPawn;
     Piece movedPiece;
     int moveCount, captureCount, quietCount, singularLMR;
 
@@ -946,6 +946,7 @@ moves_loop: // When in check, search starts from here
       captureOrPromotion = pos.capture_or_promotion(move);
       movedPiece = pos.moved_piece(move);
       givesCheck = pos.gives_check(move);
+	  PassPawn = pos.pawn_passed(us, to_sq(move));
 
       // Step 13. Extensions (~70 Elo)
 
@@ -1108,6 +1109,9 @@ moves_loop: // When in check, search starts from here
               // Increase reduction for cut nodes (~5 Elo)
               if (cutNode)
                   r += 2 * ONE_PLY;
+			  
+			  if (PassPawn)
+				  r -= ONE_PLY;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
