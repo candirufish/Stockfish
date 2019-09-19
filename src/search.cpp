b8fc526f874347cac17be7811b58456b0db6006b
candirufish <38038147+candirufish@users.noreply.github.com>
2019-09-19 13:29:20 +0200

    double extend if pass pawn is far from king

--- src/search.cpp
+++ src/search.cpp
@@ -1009,7 +1009,7 @@ moves_loop: // When in check, search starts from here
       else if (   move == ss->killers[0]
                && pos.advanced_pawn_push(move)
                && pos.pawn_passed(us, to_sq(move)))
-          extension = ONE_PLY;
+          extension = (distance(pos.square<KING>(pos.side_to_move()), to_sq(move)) > 2 ? 2 : 1) *ONE_PLY;
 
       // Calculate new depth for this move
       newDepth = depth - ONE_PLY + extension;
