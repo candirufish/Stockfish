cacfaa721b261c9639d3ca302a3ce722ad2abc90
candirufish <38038147+candirufish@users.noreply.github.com>
2020-04-18 20:02:09 +0200

    pvnode only

--- src/search.cpp
+++ src/search.cpp
@@ -1129,7 +1129,7 @@ moves_loop: // When in check, search starts from here
       if (   move == ttMove
           && pos.rule50_count() > 80
           && (captureOrPromotion || type_of(movedPiece) == PAWN))
-          extension = 2;
+           extension = PvNode ? 2 : 1;
 
       // Add extension to new depth
       newDepth += extension;
