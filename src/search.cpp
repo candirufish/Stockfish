96181403e807f141bf04fd7d40f8726a050a4a44
candirufish <38038147+candirufish@users.noreply.github.com>
2020-04-19 06:35:49 +0200

    extend only at pv and formerpv nodes

--- src/search.cpp
+++ src/search.cpp
@@ -1118,8 +1118,9 @@ moves_loop: // When in check, search starts from here
 
       // Last captures extension
       else if (   PieceValue[EG][pos.captured_piece()] > PawnValueEg
-               && pos.non_pawn_material() <= 2 * RookValueMg)
-          extension = 1 + (move == ttMove);
+               && pos.non_pawn_material() <= 2 * RookValueMg
+			   && (PvNode || formerPv))
+          extension = 1;
 
       // Castling extension
       if (type_of(move) == CASTLING)
