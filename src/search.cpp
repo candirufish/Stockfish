5cda5b8d0fe654b174c2aea41e4e7a0f7192bf43
candirufish <38038147+candirufish@users.noreply.github.com>
2020-04-19 06:45:52 +0200

    extend more if 50move is low

--- src/search.cpp
+++ src/search.cpp
@@ -1118,9 +1118,8 @@ moves_loop: // When in check, search starts from here
 
       // Last captures extension
       else if (   PieceValue[EG][pos.captured_piece()] > PawnValueEg
-               && pos.non_pawn_material() <= 2 * RookValueMg
-			   && move == ttMove)
-          extension = 1;
+               && pos.non_pawn_material() <= 2 * RookValueMg)
+          extension = 1 + (pos.rule50_count() <= 10);
 
       // Castling extension
       if (type_of(move) == CASTLING)
