adaaed64396e84509eaab10d2f749a611f3ea8be
candirufish <38038147+candirufish@users.noreply.github.com>
2020-04-19 06:39:54 +0200

    add ttmove condition

--- src/search.cpp
+++ src/search.cpp
@@ -1119,7 +1119,7 @@ moves_loop: // When in check, search starts from here
       // Last captures extension
       else if (   PieceValue[EG][pos.captured_piece()] > PawnValueEg
                && pos.non_pawn_material() <= 2 * RookValueMg
-			   && (PvNode || formerPv))
+			   && move == ttMove)
           extension = 1;
 
       // Castling extension
