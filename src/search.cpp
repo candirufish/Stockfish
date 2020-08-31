0ef47bfc73a593cfeff794cf053d1190d502217f
candirufish <38038147+candirufish@users.noreply.github.com>
2020-08-31 23:11:59 +0200

    endgame extension pvnode or formerpv node only

--- src/search.cpp
+++ src/search.cpp
@@ -1129,7 +1129,7 @@ moves_loop: // When in check, search starts from here
 				&&  abs(ss->staticEval) >= Value(10)
        			&&  pos.rule50_count() <= 10
 				&&  depth >= 8
- 				&&  PvNode)
+ 				&&  (PvNode || formerPv))
 			extension = 1;
 
       // Last captures extension
