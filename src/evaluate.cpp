74da674cfb97f9cab95128b7260359763e6bd500
candirufish <38038147+candirufish@users.noreply.github.com>
2020-09-10 13:58:41 +0200

    always use nnue for equal piece count with many pawns take 2

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1021,6 +1021,7 @@ Value Eval::evaluate(const Position& pos) {
   bool useClassical = abs(eg_value(pos.psq_score())) * 16 > NNUEThreshold1 * (16 + pos.rule50_count());
   bool classical = !Eval::useNNUE
                 ||  useClassical
+				|| !(pos.non_pawn_material(WHITE) == pos.non_pawn_material(BLACK) && pos.count<PAWN>() > 9)
                 || (abs(eg_value(pos.psq_score())) > PawnValueMg / 4 && !(pos.this_thread()->nodes & 0xB));
   Value v = classical ? Evaluation<NO_TRACE>(pos).value()
                       : NNUE::evaluate(pos) * 5 / 4 + Tempo;
