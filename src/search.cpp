e2e2b00afc6a09d62addc2f05cc50c10269e29c3
candirufish <38038147+candirufish@users.noreply.github.com>
2021-05-28 04:36:56 +0200

    less reduction for checking moves

--- src/search.cpp
+++ src/search.cpp
@@ -1153,7 +1153,7 @@ moves_loop: // When in check, search starts from here
 
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode)
-              r += 1 + !captureOrPromotion;
+		      r += captureOrPromotion || ss->inCheck || givesCheck ? 1 : 2;
 
           if (!captureOrPromotion)
           {
