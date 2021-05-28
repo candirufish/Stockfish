41270dbf382268348f5f73d257380dfcff929f67
candirufish <38038147+candirufish@users.noreply.github.com>
2021-05-28 15:31:29 +0200

    check and incheck extension

--- src/search.cpp
+++ src/search.cpp
@@ -1098,7 +1098,7 @@ moves_loop: // When in check, search starts from here
           }
       }
 	  
-	  else if (ss->inCheck)
+	  else if (givesCheck || ss->inCheck)
           extension = 1;
 
       // Add extension to new depth
