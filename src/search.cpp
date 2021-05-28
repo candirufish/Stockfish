1af65d58964cdffb7e9e121872c3a74119f01f0c
candirufish <38038147+candirufish@users.noreply.github.com>
2021-05-28 07:15:11 +0200

    decrease cut node on previous 2 moves

--- src/search.cpp
+++ src/search.cpp
@@ -1153,7 +1153,7 @@ moves_loop: // When in check, search starts from here
 
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode)
-		      r += 1 + ((ss-1)->moveCount > 1);
+		      r += 1 + ((ss-1)->moveCount > 2);
 
           if (!captureOrPromotion)
           {
