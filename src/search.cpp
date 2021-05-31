05badc7eea5e1e0706b0e0587638fb7f5fc9d77c
candirufish <38038147+candirufish@users.noreply.github.com>
2021-05-31 15:59:41 +0200

    do not prune captures near king

--- src/search.cpp
+++ src/search.cpp
@@ -1015,7 +1015,7 @@ moves_loop: // When in check, search starts from here
               // Capture history based pruning when the move doesn't give check
               if (   !givesCheck
                   && lmrDepth < 1
-				  && type_of(pos.piece_on(to_sq(move))) == PAWN
+				  && distance(pos.square<KING>(~pos.side_to_move()), to_sq(move)) >= 2
                   && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 0)
                   continue;
 
