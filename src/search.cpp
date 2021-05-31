0a6c09af460a066c9243876bbd6224f6b598b33f
candirufish <38038147+candirufish@users.noreply.github.com>
2021-05-31 22:17:25 +0200

    do not extend checking captures

--- src/search.cpp
+++ src/search.cpp
@@ -1098,6 +1098,7 @@ moves_loop: // When in check, search starts from here
           }
       }
       else if (   givesCheck
+			   && !captureOrPromotion
                && depth > 6
                && abs(ss->staticEval) > Value(100))
           extension = 1;
