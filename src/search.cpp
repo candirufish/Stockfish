8ea3b3047a8893ad987832957b4e3c2a69affc2f
candirufish <38038147+candirufish@users.noreply.github.com>
2021-05-31 15:50:20 +0200

    do not prune major piece captures

--- src/search.cpp
+++ src/search.cpp
@@ -1015,6 +1015,7 @@ moves_loop: // When in check, search starts from here
               // Capture history based pruning when the move doesn't give check
               if (   !givesCheck
                   && lmrDepth < 1
+				  && type_of(pos.piece_on(to_sq(move))) == PAWN
                   && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 0)
                   continue;
 
