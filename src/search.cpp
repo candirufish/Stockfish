0dd6c0f45686b041431eed44b018077d0b08fab1
candirufish <38038147+candirufish@users.noreply.github.com>
2021-05-31 16:58:21 +0200

    do not prune NBQ near king dist 3

--- src/search.cpp
+++ src/search.cpp
@@ -1015,7 +1015,7 @@ moves_loop: // When in check, search starts from here
               // Capture history based pruning when the move doesn't give check
               if (   !givesCheck
                   && lmrDepth < 1
-				  && distance(pos.square<KING>(~pos.side_to_move()), to_sq(move)) >= 2
+				  && distance(pos.square<KING>(~pos.side_to_move()), to_sq(move)) >= 3
 				  && (type_of(pos.piece_on(to_sq(move))) == PAWN || type_of(pos.piece_on(to_sq(move))) == ROOK)
                   && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 0)
                   continue;
