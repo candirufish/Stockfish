ea9bb300b4f0ae3b5f6ef9cbfa801ccc6c918a1d
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-01 18:35:18 +0200

    extend see moves negative history

--- src/search.cpp
+++ src/search.cpp
@@ -1097,10 +1097,17 @@ moves_loop: // When in check, search starts from here
                   return beta;
           }
       }
-      else if (   givesCheck
-               && depth > 6
-               && abs(ss->staticEval) > Value(100))
+      else if (givesCheck)	
+	  {
+		  if (depth > 6  
+			&& abs(ss->staticEval) > Value(100))
           extension = 1;
+
+		  else if ( depth <= 5 
+		    && pos.see_ge(move)
+			&& thisThread->mainHistory[us][from_to(move)] >= -1000)
+          extension = 1;
+	  }
 
       // Add extension to new depth
       newDepth += extension;
