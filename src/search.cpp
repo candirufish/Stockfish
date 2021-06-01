c75b4db730398f6d00df49cd2b18c0ae15d9dd43
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-01 13:58:13 +0200

    extend see moves with history d9

--- src/search.cpp
+++ src/search.cpp
@@ -1103,7 +1103,7 @@ moves_loop: // When in check, search starts from here
 			&& abs(ss->staticEval) > Value(100))
           extension = 1;
 		  
-		  else if ( depth <= 7 
+		  else if ( depth <= 9 
 		    && pos.see_ge(move)
 			&& thisThread->mainHistory[us][from_to(move)] >= 0)
           extension = 1;
