9f59397a6a85fcba6a36b30c4e4828a55426823f
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-01 23:17:43 +0200

    extend see that is not likelyfaillow

--- src/search.cpp
+++ src/search.cpp
@@ -1105,8 +1105,7 @@ moves_loop: // When in check, search starts from here
 			&& abs(ss->staticEval) > Value(100))
           extension = 1;
 
-		  else if ( depth <= 5
-			&& !likelyFailLow
+		  else if (!likelyFailLow
 		    && pos.see_ge(move)
 			&& thisThread->mainHistory[us][from_to(move)] >= 0)
           extension = 1;
