d958500d7acd8d55aec013d44baaa8f8cd77d8a9
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-01 15:52:13 +0200

    extend see moves major piece d6

--- src/search.cpp
+++ src/search.cpp
@@ -1103,7 +1103,7 @@ moves_loop: // When in check, search starts from here
 			&& abs(ss->staticEval) > Value(100))
           extension = 1;
 
-		  else if ( depth <= 5 
+		  else if ( depth <= 6 
 			&& type_of(movedPiece) != PAWN
 		    && pos.see_ge(move)
 			&& thisThread->mainHistory[us][from_to(move)] >= 0)
