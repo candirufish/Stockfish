f2b3b83e132011a66a18b653f378224327efccfe
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-02 10:10:50 +0200

    do not prune queen discovered check

--- src/search.cpp
+++ src/search.cpp
@@ -1016,7 +1016,7 @@ moves_loop: // When in check, search starts from here
               // Capture history based pruning when the move doesn't give check
               if (   !givesCheck
 			  	  && !pos.is_discovered_check_on_king(~us, move)
-				  && type_of(pos.piece_on(to_sq(move))) != KNIGHT
+				  && type_of(pos.piece_on(to_sq(move))) != QUEEN
                   && lmrDepth < 1
                   && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 0)
                   continue;
