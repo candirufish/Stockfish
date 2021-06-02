95f97e63eaecf87868059d93aea6d75995eb3a6f
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-02 07:47:06 +0200

    no prune castle

--- src/search.cpp
+++ src/search.cpp
@@ -1002,6 +1002,7 @@ moves_loop: // When in check, search starts from here
       // Step 13. Pruning at shallow depth (~200 Elo)
       if (  !rootNode
           && pos.non_pawn_material(us)
+		  && type_of(move) != CASTLING
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold
