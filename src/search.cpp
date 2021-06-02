ba3ebff7f306a24e73f784b8ee7baf54a2c45f0d
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-03 00:49:35 +0200

    extend discovered checks at high depths d7

--- src/search.cpp
+++ src/search.cpp
@@ -1098,10 +1098,14 @@ moves_loop: // When in check, search starts from here
                   return beta;
           }
       }
-      else if (   givesCheck
-               && depth > 6
-               && abs(ss->staticEval) > Value(100))
+      else if (givesCheck && depth > 7)	
+	  {
+		  if (abs(ss->staticEval) > Value(100))
           extension = 1;
+
+		  else if (pos.is_discovered_check_on_king(~us, move))
+          extension = 1;
+	  }
 
       // Add extension to new depth
       newDepth += extension;
