b21f80486eb79f9505c096bf0172d288993f8fc0
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-02 09:33:47 +0200

    increase reduction only on non capture

--- src/search.cpp
+++ src/search.cpp
@@ -1142,12 +1142,6 @@ moves_loop: // When in check, search starts from here
               && !likelyFailLow)
               r -= 2;
 
-          // Increase reduction at root and non-PV nodes when the best move does not change frequently
-          if (   (rootNode || !PvNode)
-              && thisThread->rootDepth > 10
-              && thisThread->bestMoveChanges <= 2)
-              r++;
-
           // Decrease reduction if opponent's move count is high (~1 Elo)
           if ((ss-1)->moveCount > 13)
               r--;
@@ -1165,6 +1159,12 @@ moves_loop: // When in check, search starts from here
               // Increase reduction if ttMove is a capture (~3 Elo)
               if (ttCapture)
                   r++;
+			  
+			  // Increase reduction at root and non-PV nodes when the best move does not change frequently
+              if (   (rootNode || !PvNode)
+					&& thisThread->rootDepth > 10
+					&& thisThread->bestMoveChanges <= 2)
+              r++;
 
               ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                              + (*contHist[0])[movedPiece][to_sq(move)]
