434a9b795800be7d1f3a0135566c2c59303abf28
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-03 02:10:36 +0200

    see and dc extension

--- src/search.cpp
+++ src/search.cpp
@@ -1098,10 +1098,14 @@ moves_loop: // When in check, search starts from here
                   return beta;
           }
       }
-      else if (   givesCheck
-               && depth > 6
-               && abs(ss->staticEval) > Value(100))
+      else if (givesCheck && depth > 6)	
+	  {
+		  if (abs(ss->staticEval) > Value(100)
+			 || pos.is_discovered_check_on_king(~us, move)
+			 || pos.see_ge(move))
           extension = 1;
+		  
+	  }
 
       // Add extension to new depth
       newDepth += extension;
