5840e3eaf6f0f86d4a55d41aa90c56141d340714
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-03 16:24:55 +0200

    king check lmr d10 bv100 16

--- src/search.cpp
+++ src/search.cpp
@@ -1167,7 +1167,7 @@ moves_loop: // When in check, search starts from here
                   r++;
 			  
 			  if (ss->inCheck 
-			  && depth >= 6
+			  && depth >= 10
 			  && abs(bestValue) < Value(100)
 			  && pos.rule50_count() >= 16
 			  && type_of(movedPiece) == KING
