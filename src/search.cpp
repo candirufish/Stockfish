18c2ad9a81f8838f95548ef711fe7f3de64db5a5
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-03 05:44:16 +0200

    king check lmr 14

--- src/search.cpp
+++ src/search.cpp
@@ -1167,7 +1167,7 @@ moves_loop: // When in check, search starts from here
                   r++;
 			  
 			  if (ss->inCheck 
-			  && pos.rule50_count() >= 11
+			  && pos.rule50_count() >= 14
 			  && type_of(movedPiece) == KING
 			  )
                   r++;
