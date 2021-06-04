5e80398db7825067dfe3ef75bdbd5f5fa9e9cf47
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-04 02:34:09 +0200

    using bv for extension

--- src/search.cpp
+++ src/search.cpp
@@ -1100,7 +1100,7 @@ moves_loop: // When in check, search starts from here
       }
       else if (   givesCheck
                && depth > 6
-               && abs(ss->staticEval) > Value(100))
+               && abs(bestValue) > Value(100))
           extension = 1;
 
       // Add extension to new depth
