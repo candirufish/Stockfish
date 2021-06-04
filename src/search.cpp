fb88843ce361c68c36ca5c94c1179c544a407e25
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-04 02:54:36 +0200

    bv se extension rookeg

--- src/search.cpp
+++ src/search.cpp
@@ -1100,7 +1100,7 @@ moves_loop: // When in check, search starts from here
       }
       else if (   givesCheck
                && depth > 6
-               && (bestValue - ss->staticEval > -PawnValueMg))
+               && (bestValue - ss->staticEval > -RookValueEg))
           extension = 1;
 
       // Add extension to new depth
