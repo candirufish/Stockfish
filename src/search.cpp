29af6293017395e1701a20fda1aa3c2f477a4b70
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-04 03:24:56 +0200

    bv se extension bishopeg

--- src/search.cpp
+++ src/search.cpp
@@ -1100,7 +1100,7 @@ moves_loop: // When in check, search starts from here
       }
       else if (   givesCheck
                && depth > 6
-               && (bestValue - ss->staticEval > -RookValueEg))
+               && (bestValue - ss->staticEval > -BishopValueEg))
           extension = 1;
 
       // Add extension to new depth
