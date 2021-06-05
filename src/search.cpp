49b0a4f80d94f29b89bc26724eec01ee6c572422
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-05 06:06:12 +0200

    ev lmr captures 4RoEg

--- src/search.cpp
+++ src/search.cpp
@@ -1161,7 +1161,7 @@ moves_loop: // When in check, search starts from here
               r += 1 + !captureOrPromotion;
 		  
 		  if (captureOrPromotion
-		  && bestValue - ss->staticEval > -(3 * BishopValueEg))
+		  && bestValue - ss->staticEval > -(4 * RookValueEg))
 		      r--;
 
           if (!captureOrPromotion)
