a4f5f2a6d4877de1c3fd4c752cdb6771956c2774
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-05 15:04:02 +0200

    ev lmr on captures 4

--- src/search.cpp
+++ src/search.cpp
@@ -1159,6 +1159,12 @@ moves_loop: // When in check, search starts from here
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode)
               r += 1 + !captureOrPromotion;
+		  
+		  if (captureOrPromotion
+		  && !extension
+		  && move == ss->killers[0]
+		  && bestValue - ss->staticEval > -(4 * BishopValueEg))
+		      r--;
 
           if (!captureOrPromotion)
           {
