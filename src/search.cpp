1730a6a4f2e8fff2a28ca3aedd265638ea1ada02
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-05 06:38:29 +0200

    bv se ext 4BeG No Depth

--- src/search.cpp
+++ src/search.cpp
@@ -1098,9 +1098,8 @@ moves_loop: // When in check, search starts from here
                   return beta;
           }
       }
-      else if (   givesCheck
-               && depth > 6
-               && abs(ss->staticEval) > Value(100))
+       else if (   givesCheck
+               && (bestValue - ss->staticEval > -(4 * BishopValueEg)))
           extension = 1;
 
       // Add extension to new depth
