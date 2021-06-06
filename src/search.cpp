cdaf0570fa6a07d22d37e0348e9708a72dd57cca
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-06 07:15:45 +0200

    bv se lmr on checking captures 2

--- src/search.cpp
+++ src/search.cpp
@@ -1134,7 +1134,7 @@ moves_loop: // When in check, search starts from here
 		  
 		  if (captureOrPromotion
 		  && givesCheck
-		  && bestValue - ss->staticEval > -(4 * BishopValueEg))
+		  && bestValue - ss->staticEval > -(2 * BishopValueEg))
 		      r--;
 
           // Decrease reduction if the ttHit running average is large (~0 Elo)
