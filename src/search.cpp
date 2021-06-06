72eb3f65c4e14052c407c4efaf836cad963cfb58
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-06 19:39:50 +0200

    king check lmr no root node

--- src/search.cpp
+++ src/search.cpp
@@ -1165,6 +1165,12 @@ moves_loop: // When in check, search starts from here
               // Increase reduction if ttMove is a capture (~3 Elo)
               if (ttCapture)
                   r++;
+			  
+			  if (ss->inCheck 
+			  && rootNode
+			  && type_of(movedPiece) == KING
+			  )
+				   r++;
 
               ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                              + (*contHist[0])[movedPiece][to_sq(move)]
