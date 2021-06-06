78c9f7a5a26f749e32c62a7f65fbd42dadee2023
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-06 19:00:58 +0200

    check ext no root and excl

--- src/search.cpp
+++ src/search.cpp
@@ -1100,6 +1100,7 @@ moves_loop: // When in check, search starts from here
       }
       else if (   givesCheck
 			   && !rootNode
+			   && !excludedMove 
                && depth > 6
                && abs(ss->staticEval) > Value(100))
           extension = 1;
