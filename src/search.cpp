40880ab7c2f05abc6643f8e4022d973c6fe876ee
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-06 07:13:21 +0200

    bv se lmr on checking captures

--- src/search.cpp
+++ src/search.cpp
@@ -1131,6 +1131,11 @@ moves_loop: // When in check, search starts from here
           && (!PvNode || ss->ply > 1 || thisThread->id() % 4 != 3))
       {
           Depth r = reduction(improving, depth, moveCount);
+		  
+		  if (captureOrPromotion
+		  && givesCheck
+		  && bestValue - ss->staticEval > -(4 * BishopValueEg))
+		      r--;
 
           // Decrease reduction if the ttHit running average is large (~0 Elo)
           if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
