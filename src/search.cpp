a812e44f5511b20e8733b3e5dd791e834081cea0
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-07 12:40:57 +0200

    pv lmr no captures

--- src/search.cpp
+++ src/search.cpp
@@ -1132,8 +1132,8 @@ moves_loop: // When in check, search starts from here
       {
           Depth r = reduction(improving, depth, moveCount);
 
-          if (PvNode)
-              r -= captureOrPromotion ? 1 : 2;
+          if (PvNode && !captureOrPromotion)
+              r--;
 
           // Decrease reduction if the ttHit running average is large (~0 Elo)
           if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
