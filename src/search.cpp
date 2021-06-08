fd0b484fe74c941c75ce37470ea80308976a44da
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-08 02:59:35 +0200

    check ext no root exl bv se 4QeG

--- src/search.cpp
+++ src/search.cpp
@@ -1098,11 +1098,12 @@ moves_loop: // When in check, search starts from here
                   return beta;
           }
       }
-      else if (   givesCheck
+       else if (   givesCheck
                && depth > 6
-               && abs(ss->staticEval) > Value(100))
+			   && !rootNode
+			   && !excludedMove 
+               && bestValue - ss->staticEval > -(4 * QueenValueEg))
           extension = 1;
-
       // Add extension to new depth
       newDepth += extension;
 
