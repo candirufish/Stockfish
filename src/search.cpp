edf47fc400f7c23008611ed483f3a88160ea0d6a
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-09 14:37:02 +0200

    chk promote lmr add subtract

--- src/search.cpp
+++ src/search.cpp
@@ -1162,6 +1162,15 @@ moves_loop: // When in check, search starts from here
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode)
               r += 1 + !captureOrPromotion;
+		  
+		  if (type_of(move) == PROMOTION)
+		  {  	
+			if (promotion_type(move) != QUEEN && !givesCheck)
+              r++;
+			
+			else if (givesCheck)			
+			  r--;
+		  }
 
           if (!captureOrPromotion)
           {
