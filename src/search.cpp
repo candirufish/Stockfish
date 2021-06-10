c03504ac0e42212a6b09043c7cdb7ebe35c3b995
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-10 19:41:04 +0200

    pv eval lmr d8

--- src/search.cpp
+++ src/search.cpp
@@ -1133,7 +1133,7 @@ moves_loop: // When in check, search starts from here
           Depth r = reduction(improving, depth, moveCount);
 
           if (PvNode)
-		      r -= (bestValue - ss->staticEval < -(PawnValueEg)) && depth > 6 ? 2 : 1;
+		      r -= (bestValue - ss->staticEval < -(PawnValueEg)) && depth >= 8 ? 2 : 1;
 
           // Decrease reduction if the ttHit running average is large (~0 Elo)
           if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
