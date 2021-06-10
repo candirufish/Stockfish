efba5e173826a6b37c493db55999cb9a7034ecb0
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-10 14:33:14 +0200

    seldepth lmr pv 2

--- src/search.cpp
+++ src/search.cpp
@@ -1134,6 +1134,8 @@ moves_loop: // When in check, search starts from here
 
           if (PvNode)
               r--;
+		  else if (thisThread->rootDepth > thisThread->selDepth)
+              r--;
 
           // Decrease reduction if the ttHit running average is large (~0 Elo)
           if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
