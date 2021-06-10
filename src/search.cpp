7aec79b29bb7b5c0529a20600f453c51ff9a4bd5
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-10 06:45:19 +0200

    lmr pr cap stat 1x reorder

--- src/search.cpp
+++ src/search.cpp
@@ -1181,7 +1181,7 @@ moves_loop: // When in check, search starts from here
           }
 		  
 		  else if ((to_sq(move) == (type_of(move) == PROMOTION)) == to_sq((ss-1)->currentMove) && (ss-1)->statScore > 0)
-			  r += 2;
+			  r++;
 
           // In general we want to cap the LMR depth search at newDepth. But if
           // reductions are really negative and movecount is low, we allow this move
