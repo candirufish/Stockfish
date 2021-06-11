ee4270f3ac2def831ae9231d21da39cd672502e7
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-11 22:59:10 +0200

    d6 lmr reduce 5 ev pv x2 1

--- src/search.cpp
+++ src/search.cpp
@@ -1137,9 +1137,11 @@ moves_loop: // When in check, search starts from here
           && (!PvNode || ss->ply > 1 || thisThread->id() % 4 != 3))
       {
           Depth r = reduction(improving, depth, moveCount);
-
-          if (PvNode)
-              r--;
+		  
+	 if (depth > 6) {
+			  
+		  if (PvNode)
+              r -= bestValue - ss->staticEval < -(PawnValueEg) ? 2 : 1;
 
           // Decrease reduction if the ttHit running average is large (~0 Elo)
           if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
@@ -1151,12 +1153,6 @@ moves_loop: // When in check, search starts from here
               && !likelyFailLow)
               r -= 2;
 
-          // Increase reduction at root and non-PV nodes when the best move does not change frequently
-          if (   (rootNode || !PvNode)
-              && thisThread->rootDepth > 10
-              && thisThread->bestMoveChanges <= 2)
-              r++;
-
           // Decrease reduction if opponent's move count is high (~1 Elo)
           if ((ss-1)->moveCount > 13)
               r--;
@@ -1164,6 +1160,14 @@ moves_loop: // When in check, search starts from here
           // Decrease reduction if ttMove has been singularly extended (~1 Elo)
           if (singularQuietLMR)
               r--;
+		  
+		  }
+		  
+		  // Increase reduction at root and non-PV nodes when the best move does not change frequently
+          if (   (rootNode || !PvNode)
+              && thisThread->rootDepth > 10
+              && thisThread->bestMoveChanges <= 2)
+              r++;
 
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode)
