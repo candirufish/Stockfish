c1ebf4a86cd630b4fff0d9105d9f6d4ee54382e2
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-11 17:26:26 +0200

    d6 lmr reduce 5 ev pv x2 1

--- src/search.cpp
+++ src/search.cpp
@@ -1135,8 +1135,8 @@ moves_loop: // When in check, search starts from here
 		  
 	 if (depth > 6) {
 			  
-		  if (PvNode && bestValue - ss->staticEval < -(PawnValueEg))
-              r -= 2;
+		  if (PvNode)
+              r -= bestValue - ss->staticEval < -(PawnValueEg) ? 2 : 1;
 
           // Decrease reduction if the ttHit running average is large (~0 Elo)
           if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
