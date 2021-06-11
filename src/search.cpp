5b6312ee3a9c37a5c14cbd9faba121e3ec35822e
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-11 09:11:02 +0200

    pv lmr x3

--- src/search.cpp
+++ src/search.cpp
@@ -1132,8 +1132,8 @@ moves_loop: // When in check, search starts from here
       {
           Depth r = reduction(improving, depth, moveCount);
 
-          if (PvNode && depth > 6)
-              r -= bestValue - ss->staticEval < -(PawnValueEg) ? 2 : 1;
+          if (PvNode)
+		      r -= (bestValue - ss->staticEval < -(PawnValueEg)) && depth > 6 ? 3 : 1;
 		  
           // Decrease reduction if the ttHit running average is large (~0 Elo)
           if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
