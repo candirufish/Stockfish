a3112fb2936bb4e3beadbe3e0708e8d9e9f93061
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-13 16:21:54 +0200

    dbl ext lmr promo low d

--- src/search.cpp
+++ src/search.cpp
@@ -1168,6 +1168,9 @@ moves_loop: // When in check, search starts from here
           // Decrease reduction if ttMove has been singularly extended (~1 Elo)
           if (singularQuietLMR)
               r--;
+		  
+		  if (type_of(move) == PROMOTION && promotion_type(move) != QUEEN && doubleExtension && depth <= 10)
+			  r++;
 
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode)
