66ce49f140bf4acb1b0d7c99283ddb25008465e0
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-15 00:45:49 +0200

    lmr ext pv no reduce

--- src/search.cpp
+++ src/search.cpp
@@ -1142,9 +1142,6 @@ moves_loop: // When in check, search starts from here
       {
           Depth r = reduction(improving, depth, moveCount);
 
-          if (PvNode)
-              r--;
-
           // Decrease reduction if the ttHit running average is large (~0 Elo)
           if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
               r--;
@@ -1193,7 +1190,7 @@ moves_loop: // When in check, search starts from here
           // In general we want to cap the LMR depth search at newDepth. But if
           // reductions are really negative and movecount is low, we allow this move
           // to be searched deeper than the first move, unless ttMove was extended by 2.
-          Depth d = std::clamp(newDepth - r, 1, newDepth + (r < -1 && moveCount <= 5 && !doubleExtension));
+          Depth d = std::clamp(newDepth - r, 1, newDepth + (r < -1 && moveCount <= 5 && !doubleExtension) + (depth >= 6 && PvNode));
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
