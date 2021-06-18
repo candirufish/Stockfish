94f906454a735824f3d9b83619992a4d0a24a73c
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-18 03:01:21 +0200

    bmc no lmrext

--- src/search.cpp
+++ src/search.cpp
@@ -962,6 +962,9 @@ moves_loop: // When in check, search starts from here
                          && ttMove
                          && (tte->bound() & BOUND_UPPER)
                          && tte->depth() >= depth;
+						 
+	bool bmc = (rootNode || !PvNode)
+			&& thisThread->bestMoveChanges <= 2;
 
     // Step 12. Loop through all pseudo-legal moves until no moves remain
     // or a beta cutoff occurs.
@@ -1156,8 +1159,7 @@ moves_loop: // When in check, search starts from here
               r -= 2;
 
           // Increase reduction at root and non-PV nodes when the best move does not change frequently
-          if (   (rootNode || !PvNode)
-              && thisThread->bestMoveChanges <= 2)
+          if (bmc)
               r++;
 
           // Decrease reduction if opponent's move count is high (~1 Elo)
@@ -1192,7 +1194,7 @@ moves_loop: // When in check, search starts from here
           // In general we want to cap the LMR depth search at newDepth. But if
           // reductions are really negative and movecount is low, we allow this move
           // to be searched deeper than the first move, unless ttMove was extended by 2.
-          Depth d = std::clamp(newDepth - r, 1, newDepth + (r < -1 && moveCount <= 5 && !doubleExtension));
+          Depth d = std::clamp(newDepth - r, 1, newDepth + (r < -1 && moveCount <= 5 && !doubleExtension && !bmc));
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
