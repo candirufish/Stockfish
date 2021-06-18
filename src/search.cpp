0b35065e126d805a00acf9427c9e208e217e3b63
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-18 03:04:41 +0200

    bmc no lmrext no reduce

--- src/search.cpp
+++ src/search.cpp
@@ -1158,10 +1158,6 @@ moves_loop: // When in check, search starts from here
               && !likelyFailLow)
               r -= 2;
 
-          // Increase reduction at root and non-PV nodes when the best move does not change frequently
-          if (bmc)
-              r++;
-
           // Decrease reduction if opponent's move count is high (~1 Elo)
           if ((ss-1)->moveCount > 13)
               r--;
