9e1f78d063441097a96b0697ab67f71e0bb6dedf
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-18 03:52:39 +0200

    can check lmrext

--- src/search.cpp
+++ src/search.cpp
@@ -1176,9 +1176,6 @@ moves_loop: // When in check, search starts from here
 
           if (!captureOrPromotion)
           {
-			  		  
-		      if (canCheck)
-				  r--;
 			  
               // Increase reduction if ttMove is a capture (~3 Elo)
               if (ttCapture)
@@ -1198,7 +1195,7 @@ moves_loop: // When in check, search starts from here
           // In general we want to cap the LMR depth search at newDepth. But if
           // reductions are really negative and movecount is low, we allow this move
           // to be searched deeper than the first move, unless ttMove was extended by 2.
-          Depth d = std::clamp(newDepth - r, 1, newDepth + (r < -1 && moveCount <= 5 && !doubleExtension));
+          Depth d = std::clamp(newDepth - r, 1, newDepth + (r < -1 && (moveCount <= 5 || canCheck) && !doubleExtension));
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
