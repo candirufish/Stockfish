6bc2a9075259474e6085ae3f69b653c6751a391a
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-21 21:49:27 +0200

    best move lmr pv3

--- src/search.cpp
+++ src/search.cpp
@@ -1142,8 +1142,8 @@ moves_loop: // When in check, search starts from here
       {
           Depth r = reduction(improving, depth, moveCount);
 
-          if (PvNode || bestMove)
-              r--;
+          if (PvNode)
+              r -= bestMove ? 2 : 1;
 		  
           // Decrease reduction if the ttHit running average is large (~0 Elo)
           if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
