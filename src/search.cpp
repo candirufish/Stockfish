e1352ba4f05f177237cc53efcacae83b6ffaf3b8
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-21 21:34:31 +0200

    best move lmr

--- src/search.cpp
+++ src/search.cpp
@@ -1144,6 +1144,9 @@ moves_loop: // When in check, search starts from here
 
           if (PvNode)
               r--;
+		  
+		  if (bestMove)
+			  r--;
 
           // Decrease reduction if the ttHit running average is large (~0 Elo)
           if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
