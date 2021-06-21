55f4c0dbef9d7f98bd43ca1f9f68ab42f1a37dd4
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-21 06:05:05 +0200

    elmr kd not cn cap

--- src/search.cpp
+++ src/search.cpp
@@ -1145,7 +1145,7 @@ moves_loop: // When in check, search starts from here
           if (PvNode)
               r--;
 		  
-		  if (captureOrPromotion && !extension
+		  if (captureOrPromotion && !cutNode
 		    && distance(to_sq(move), pos.square<KING>(~us)) < 3
 			&& bestValue - ss->staticEval < -(PawnValueEg))
 			  r--;
