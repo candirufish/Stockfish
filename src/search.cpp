3498ab96a3d8e5f08446b079a23862f3493a9e4d
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-21 13:58:56 +0200

    king move lmr ss2

--- src/search.cpp
+++ src/search.cpp
@@ -1182,6 +1182,7 @@ moves_loop: // When in check, search starts from here
 			  
 			  if (ss->kingmove 
 			   && (ss-1)->kingmove
+			   && (ss-2)->kingmove
 			   && Pawns::probe(pos)->passed_count() == 0
 			   && pos.rule50_count() >= 11)
 			      r++;
