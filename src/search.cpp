1fb1849f4534a491cb3c5f7840ff657b2c755121
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-21 23:43:48 +0200

    best move lmr lfl pv 2

--- src/search.cpp
+++ src/search.cpp
@@ -1139,7 +1139,7 @@ moves_loop: // When in check, search starts from here
 
           // Decrease reduction if position is or has been on the PV
           // and node is not likely to fail low. (~3 Elo)
-          if (   ss->ttPv
+          if (   (ss->ttPv || (PvNode && bestMove))
               && !likelyFailLow)
               r -= 2;
 
