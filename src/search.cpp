46e7fde0d2f0414de4b08d2e2adebf90a75145e2
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-22 17:15:32 +0200

    ttmove lmr -

--- src/search.cpp
+++ src/search.cpp
@@ -1132,7 +1132,10 @@ moves_loop: // When in check, search starts from here
 
           if (PvNode)
               r--;
-
+		  
+		  if (ttMove)
+			  r--; 
+			 
           // Decrease reduction if the ttHit running average is large (~0 Elo)
           if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
               r--;
