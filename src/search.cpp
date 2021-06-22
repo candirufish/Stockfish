de92f5d46dc1d47eea74afd585574ab60bb48727
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-22 16:15:28 +0200

    pv increase lmr 1

--- src/search.cpp
+++ src/search.cpp
@@ -901,12 +901,6 @@ namespace {
          ss->ttPv = ttPv;
     }
 
-    // Step 10. If the position is not in TT, decrease depth by 2
-    if (   PvNode
-        && depth >= 6
-        && !ttMove)
-        depth -= 2;
-
 moves_loop: // When in check, search starts from here
 
     ttCapture = ttMove && pos.capture_or_promotion(ttMove);
@@ -1129,6 +1123,11 @@ moves_loop: // When in check, search starts from here
           && (!PvNode || ss->ply > 1 || thisThread->id() % 4 != 3))
       {
           Depth r = reduction(improving, depth, moveCount);
+		  
+		 if (PvNode
+              && depth >= 6
+              && !ttMove)
+               r++;
 
           if (PvNode)
               r--;
