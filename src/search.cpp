dfb31636d28fb195433dbed60608c2fc322c15c5
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-22 16:38:35 +0200

    pv lmr decrease ttm

--- src/search.cpp
+++ src/search.cpp
@@ -1130,7 +1130,7 @@ moves_loop: // When in check, search starts from here
       {
           Depth r = reduction(improving, depth, moveCount);
 
-          if (PvNode)
+          if (PvNode && ttMove)
               r--;
 
           // Decrease reduction if the ttHit running average is large (~0 Elo)
