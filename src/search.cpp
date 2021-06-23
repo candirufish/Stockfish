a776f1e2167645522726930ade4bf6483e31f6ea
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-23 18:48:57 +0200

    pv inc lmr 1 no cap

--- src/search.cpp
+++ src/search.cpp
@@ -1169,7 +1169,7 @@ moves_loop: // When in check, search starts from here
 			  if (PvNode
               && depth >= 6
               && !ttMove)
-               r += 2;
+                  r++;
 
               ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                              + (*contHist[0])[movedPiece][to_sq(move)]
