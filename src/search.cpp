67b7d008dc30e8d2f4dfde693413b778e892e196
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-24 00:04:46 +0200

    check ext <=d5 see

--- src/search.cpp
+++ src/search.cpp
@@ -1096,8 +1096,8 @@ moves_loop: // When in check, search starts from here
           }
       }
       else if (   givesCheck
-               && depth > 6
-               && abs(ss->staticEval) > Value(100))
+               && ((depth >= 6 && abs(ss->staticEval) > Value(100))
+			   || (depth <= 5 && (!pos.see_ge(move, Value(-218) * depth)) )))
           extension = 1;
 
       // Add extension to new depth
