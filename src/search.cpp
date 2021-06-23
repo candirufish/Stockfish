eb903b2295df4f528eaa340aac3514db758e7cbc
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-23 17:40:00 +0200

    ttcap lmr simplify

--- src/search.cpp
+++ src/search.cpp
@@ -1162,10 +1162,6 @@ moves_loop: // When in check, search starts from here
 
           if (!captureOrPromotion)
           {
-              // Increase reduction if ttMove is a capture (~3 Elo)
-              if (ttCapture)
-                  r++;
-
               ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                              + (*contHist[0])[movedPiece][to_sq(move)]
                              + (*contHist[1])[movedPiece][to_sq(move)]
