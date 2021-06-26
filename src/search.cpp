25c9984b912eea85adf3bf17578f13836a7c3087
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-26 16:22:24 +0200

    d5 ttm extension

--- src/search.cpp
+++ src/search.cpp
@@ -1099,6 +1099,12 @@ moves_loop: // When in check, search starts from here
                && depth > 6
                && abs(ss->staticEval) > Value(100))
           extension = 1;
+		  
+	 if (ttMove 
+			&& depth <= 5
+			&& captureOrPromotion 
+			&& captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] > 0)			
+	      extension = 1;
 
       // Add extension to new depth
       newDepth += extension;
