a9e44caa12f7040e979d9b936c21355a809370b9
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-26 19:47:43 +0200

    less clmr for k moves

--- src/search.cpp
+++ src/search.cpp
@@ -1157,8 +1157,8 @@ moves_loop: // When in check, search starts from here
               r--;
 
           // Increase reduction for cut nodes (~3 Elo)
-          if (cutNode && move != ss->killers[0])
-              r += 1 + !captureOrPromotion;
+          if (cutNode)
+              r += 1 + !(captureOrPromotion || move == ss->killers[0]);
 
           if (!captureOrPromotion)
           {
