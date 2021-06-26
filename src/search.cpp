2b18b2fdcd57220a4ce8e96423a185673368c789
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-26 19:10:27 +0200

    no cm pr on pv

--- src/search.cpp
+++ src/search.cpp
@@ -1016,7 +1016,8 @@ moves_loop: // When in check, search starts from here
           else
           {
               // Continuation history based pruning (~20 Elo)
-              if (   lmrDepth < 5
+              if (   !PvNode
+			      && lmrDepth < 5			  
                   && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
                   && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
                   continue;
