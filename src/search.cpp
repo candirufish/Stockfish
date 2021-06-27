40faa8bd20f3bc66b4205fcac28c3be9e09cdb7c
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-27 03:15:33 +0200

    no cm pr on pv with lmr

--- src/search.cpp
+++ src/search.cpp
@@ -1133,6 +1133,11 @@ moves_loop: // When in check, search starts from here
 
           if (PvNode)
               r--;
+		  
+		  if (PvNode
+              && depth >= 6
+              && !ttMove)
+               r += 2;
 
           // Decrease reduction if the ttHit running average is large (~0 Elo)
           if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
