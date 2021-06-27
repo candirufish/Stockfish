b9c9090b1ddb1b20272463f2a242b6545fb2b368
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-27 23:26:35 +0200

    if no pv no imp lmr

--- src/search.cpp
+++ src/search.cpp
@@ -1132,6 +1132,9 @@ moves_loop: // When in check, search starts from here
 
           if (PvNode)
               r--;
+		  
+		  if (!PvNode && !improving )
+			  r++;
 
           // Decrease reduction if the ttHit running average is large (~0 Elo)
           if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
