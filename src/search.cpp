2ac37ce6c06086edb5af7155081df0df5f645880
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-28 23:24:16 +0200

    cap ss1 or 2 km clmr

--- src/search.cpp
+++ src/search.cpp
@@ -1159,7 +1159,7 @@ moves_loop: // When in check, search starts from here
 
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode)
-              r += 1 + !(captureOrPromotion || ((ss-1)->WasCPr && (ss-2)->WasCPr));
+              r += 1 + !((captureOrPromotion || (ss-1)->WasCPr || (ss-2)->WasCPr) && move == ss->killers[0]);
 
           if (!captureOrPromotion)
           {
