3c25d9ff5d2020d5ebbea2259f334705443c370e
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-28 08:44:17 +0200

    less clmr cap or kill1 only

--- src/search.cpp
+++ src/search.cpp
@@ -1158,7 +1158,7 @@ moves_loop: // When in check, search starts from here
 
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode)
-              r += 1 + !(captureOrPromotion || (move == ss->killers[0] || move == ss->killers[1]));
+              r += 1 + !(captureOrPromotion || (move == ss->killers[1]));
 
           if (!captureOrPromotion)
           {
