337a4c41c80c2d4ffa81f8db7b7624ebe1fd3ebd
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-29 01:19:12 +0200

    cap or pos stat km clmr

--- src/search.cpp
+++ src/search.cpp
@@ -1155,22 +1155,22 @@ moves_loop: // When in check, search starts from here
           // Decrease reduction if ttMove has been singularly extended (~1 Elo)
           if (singularQuietLMR)
               r--;
+		  
+		  ss->statScore =  thisThread->mainHistory[us][from_to(move)]
+                        + (*contHist[0])[movedPiece][to_sq(move)]
+                        + (*contHist[1])[movedPiece][to_sq(move)]
+                        + (*contHist[3])[movedPiece][to_sq(move)]
+                          - 4923;
 
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode)
-              r += 1 + !(captureOrPromotion || (givesCheck && move == ss->killers[0]));
+              r += 1 + !(captureOrPromotion || (ss->statScore > 0 && move == ss->killers[0]));
 
           if (!captureOrPromotion)
           {
               // Increase reduction if ttMove is a capture (~3 Elo)
               if (ttCapture)
                   r++;
-
-              ss->statScore =  thisThread->mainHistory[us][from_to(move)]
-                             + (*contHist[0])[movedPiece][to_sq(move)]
-                             + (*contHist[1])[movedPiece][to_sq(move)]
-                             + (*contHist[3])[movedPiece][to_sq(move)]
-                             - 4923;
 
               // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
               if (!ss->inCheck)
