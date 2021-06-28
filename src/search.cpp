aaae2f42ddbacb318cc8611b816200a005f5aa99
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-28 02:02:36 +0200

    pv no imp no ttm quiet

--- src/search.cpp
+++ src/search.cpp
@@ -1165,6 +1165,9 @@ moves_loop: // When in check, search starts from here
               // Increase reduction if ttMove is a capture (~3 Elo)
               if (ttCapture)
                   r++;
+			  
+			  if (PvNode && !improving & !ttMove)
+			      r++;
 
               ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                              + (*contHist[0])[movedPiece][to_sq(move)]
