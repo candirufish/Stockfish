e1432614a40173827bd33a376f7db894e435a563
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-30 03:18:33 +0200

    ss1 passpawn lmr -2

--- src/search.cpp
+++ src/search.cpp
@@ -1136,7 +1136,7 @@ moves_loop: // When in check, search starts from here
 		  
 		  if (captureOrPromotion
 		     &&  (ss-1)->passpawncount < (ss)->passpawncount)
-			  r--;
+			  r -= 2;
 
           // Decrease reduction if the ttHit running average is large (~0 Elo)
           if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
