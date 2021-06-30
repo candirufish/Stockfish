d8ec5e6f3b144f8a94bc7c11735df097fc7effa5
candirufish <38038147+candirufish@users.noreply.github.com>
2021-07-01 00:35:12 +0200

    pass pawn cap lmr no pawn

--- src/search.cpp
+++ src/search.cpp
@@ -1135,7 +1135,7 @@ moves_loop: // When in check, search starts from here
               r--;
 		  
 		  if (captureOrPromotion && improving
-		     && type_of(movedPiece) == PAWN
+		     && type_of(movedPiece) != PAWN
 		     &&  (ss-1)->passpawncount < (ss)->passpawncount)
 			  r -= 2;
 
