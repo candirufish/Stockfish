05bdf425e4455c2f64f08ee6ba1b77f0631d3708
candirufish <38038147+candirufish@users.noreply.github.com>
2021-07-01 02:44:39 +0200

    failing improving ++

--- src/search.cpp
+++ src/search.cpp
@@ -1157,7 +1157,7 @@ moves_loop: // When in check, search starts from here
               r--;
 		  
 		  if (improving && bestValue - ss->staticEval < -(PawnValueEg))
-			  r--;
+			  r++;
 
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode)
