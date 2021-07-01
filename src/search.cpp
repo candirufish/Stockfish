8a2c77f8698142b60a50bb2bb8a2dfc6ff4cb2a8
candirufish <38038147+candirufish@users.noreply.github.com>
2021-07-02 01:30:11 +0200

    failing imp quiet ++

--- src/search.cpp
+++ src/search.cpp
@@ -1167,7 +1167,7 @@ moves_loop: // When in check, search starts from here
                   r++;
 			  
 			  if (improving && bestValue - ss->staticEval < -(PawnValueEg))
-			      r--;
+			      r++;
 
               ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                              + (*contHist[0])[movedPiece][to_sq(move)]
