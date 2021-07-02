f770e0a49680a4c81b94641559e774e8d2b9ffd8
candirufish <38038147+candirufish@users.noreply.github.com>
2021-07-03 00:48:11 +0200

    ss2 imp q ss1 cap ++

--- src/search.cpp
+++ src/search.cpp
@@ -1116,7 +1116,7 @@ moves_loop: // When in check, search starts from here
 
       // Step 15. Make the move
       pos.do_move(move, st, givesCheck);
-	  ss->WasImp = improving;
+	  ss->WasImp = improving && !captureOrPromotion;
 	  ss->WasCPr = captureOrPromotion;
 
       // Step 16. Late moves reduction / extension (LMR, ~200 Elo)
