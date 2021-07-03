39fb6c286ca76afd22653da8c6b11e8985c2ea62
candirufish <38038147+candirufish@users.noreply.github.com>
2021-07-03 02:11:53 +0200

    pp lmr reverse ++

--- src/search.cpp
+++ src/search.cpp
@@ -1137,7 +1137,7 @@ moves_loop: // When in check, search starts from here
 		  if (captureOrPromotion 
 			 && pos.non_pawn_material() <= 5000
 		     &&  (ss-1)->passpawncount > (ss)->passpawncount)
-			  r--;
+			  r++;
 
           // Decrease reduction if the ttHit running average is large (~0 Elo)
           if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
