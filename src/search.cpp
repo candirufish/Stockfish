b3b58221b8aae5c170151f247d1bc5e026b2e96b
candirufish <38038147+candirufish@users.noreply.github.com>
2021-07-04 09:16:20 +0200

    k0 stat <0 no pv r++

--- src/search.cpp
+++ src/search.cpp
@@ -1172,7 +1172,7 @@ moves_loop: // When in check, search starts from here
                              + (*contHist[3])[movedPiece][to_sq(move)]
                              - 4923;
 							 
-			  if (move == ss->killers[0] && ss->statScore < 0)
+			  if (move == ss->killers[0] && ss->statScore < 0 && !PvNode)
 				  r++;
 
               // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
