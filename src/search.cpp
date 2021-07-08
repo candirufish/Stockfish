2cef0a2ee5a1405d389b6f1127eb2547fef12846
candirufish <38038147+candirufish@users.noreply.github.com>
2021-07-08 14:11:05 +0200

    k1 pos stat ttm not pv

--- src/search.cpp
+++ src/search.cpp
@@ -1172,7 +1172,7 @@ moves_loop: // When in check, search starts from here
                              + (*contHist[3])[movedPiece][to_sq(move)]
                              - 4923;
 							 
-			  if (move == ss->killers[1] && ss->statScore > 0 && ttMove)
+			  if (move == ss->killers[1] && ss->statScore > 0 && ttMove && !PvNode)
 				  r--;
 
               // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
