f7e4faa6463efd9213ad3924dc553e11bf436de6
candirufish <38038147+candirufish@users.noreply.github.com>
2021-07-10 11:45:21 +0200

    pv ext 2 1 lmr

--- src/search.cpp
+++ src/search.cpp
@@ -1130,8 +1130,8 @@ moves_loop: // When in check, search starts from here
       {
           Depth r = reduction(improving, depth, moveCount);
 
-          if (PvNode || extension)
-              r--;
+          if (PvNode)
+              r -= extension ? 2 : 1;
 		  
           // Decrease reduction if the ttHit running average is large (~0 Elo)
           if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
