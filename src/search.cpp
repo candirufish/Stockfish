4aafe347c4d10d850c5653600ca6866e357b7aa9
candirufish <38038147+candirufish@users.noreply.github.com>
2021-07-10 03:03:05 +0200

    d6 pv not ttm cap s10 stat <0

--- src/search.cpp
+++ src/search.cpp
@@ -1127,9 +1127,15 @@ moves_loop: // When in check, search starts from here
           if (PvNode)
               r--;
 		  
+		  ss->statScore =  thisThread->mainHistory[us][from_to(move)]
+                    + (*contHist[0])[movedPiece][to_sq(move)]
+                    + (*contHist[1])[movedPiece][to_sq(move)]
+                    + (*contHist[3])[movedPiece][to_sq(move)]
+                            - 4923;		  
 		  if (PvNode
               && depth >= 6
 			  && !captureOrPromotion
+			  && ss->statScore < 0 
               && !ttMove)
                r += 2;
 
@@ -1165,12 +1171,6 @@ moves_loop: // When in check, search starts from here
               // Increase reduction if ttMove is a capture (~3 Elo)
               if (ttCapture)
                   r++;
-
-              ss->statScore =  thisThread->mainHistory[us][from_to(move)]
-                             + (*contHist[0])[movedPiece][to_sq(move)]
-                             + (*contHist[1])[movedPiece][to_sq(move)]
-                             + (*contHist[3])[movedPiece][to_sq(move)]
-                             - 4923;
 
               // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
               if (!ss->inCheck)
