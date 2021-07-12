162bebc3753484a108cfee907772dd6561cfcbc0
candirufish <38038147+candirufish@users.noreply.github.com>
2021-07-12 10:03:06 +0200

    k1 ps ttpv r50 16

--- src/search.cpp
+++ src/search.cpp
@@ -1172,7 +1172,7 @@ moves_loop: // When in check, search starts from here
                              + (*contHist[3])[movedPiece][to_sq(move)]
                              - 4923;
 							 
-			  if (move == ss->killers[1] && ss->statScore > 0 && ss->ttPv && pos.rule50_count() <= 21)
+			  if (move == ss->killers[1] && ss->statScore > 0 && ss->ttPv && pos.rule50_count() <= 16)
 				  r--;
 
               // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
