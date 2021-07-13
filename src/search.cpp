7963db867d336e2d7099fa66c1bed773d9591c3b
candirufish <38038147+candirufish@users.noreply.github.com>
2021-07-13 08:36:28 +0200

    cap ext lmr --

--- src/search.cpp
+++ src/search.cpp
@@ -1133,7 +1133,7 @@ moves_loop: // When in check, search starts from here
           if (PvNode)
               r--;
 		  
-		  if (givesCheck && extension)
+		  if (captureOrPromotion && extension)
 			  r--;
 
           // Decrease reduction if the ttHit running average is large (~0 Elo)
