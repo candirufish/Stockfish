4591c67e4e5176dbe80cfde017aa17a2cb23da3c
candirufish <38038147+candirufish@users.noreply.github.com>
2021-07-13 13:02:36 +0200

    gc ext lmr r50 <=11

--- src/search.cpp
+++ src/search.cpp
@@ -1133,7 +1133,7 @@ moves_loop: // When in check, search starts from here
           if (PvNode)
               r--;
 		  
-		  if (givesCheck && extension)
+		  if (givesCheck && extension && pos.rule50_count() <= 11)
 			  r--;
 
           // Decrease reduction if the ttHit running average is large (~0 Elo)
