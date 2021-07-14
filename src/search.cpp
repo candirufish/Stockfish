260a5849faea4b299b21cbd961adb6ababf11962
candirufish <38038147+candirufish@users.noreply.github.com>
2021-07-14 13:39:55 +0200

    repeat move king r50 16

--- src/search.cpp
+++ src/search.cpp
@@ -1164,6 +1164,9 @@ moves_loop: // When in check, search starts from here
               // Increase reduction if ttMove is a capture (~3 Elo)
               if (ttCapture)
                   r++;
+			  
+			  if (move == reverse_move((ss-2)->currentMove) && (type_of(movedPiece) == KING || pos.rule50_count() >= 16))
+                  r++;
 
               ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                              + (*contHist[0])[movedPiece][to_sq(move)]
