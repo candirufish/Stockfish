7aa3ac6f1c31a3d8595a3a9e3367d6978beeef83
candirufish <38038147+candirufish@users.noreply.github.com>
2021-07-15 10:38:47 +0200

    rm km or r50 > 16 no imp

--- src/search.cpp
+++ src/search.cpp
@@ -1165,7 +1165,8 @@ moves_loop: // When in check, search starts from here
               if (ttCapture)
                   r++;
 			  
-			  if (move == reverse_move((ss-2)->currentMove) && (type_of(movedPiece) == KING || pos.rule50_count() >= 16))
+			  if (move == reverse_move((ss-2)->currentMove)  && !improving 
+			  && (type_of(movedPiece) == KING || pos.rule50_count() >= 16))
                   r++;
 
               ss->statScore =  thisThread->mainHistory[us][from_to(move)]
