be219e06b201492397e6abbf7d4076279fd5db28
candirufish <38038147+candirufish@users.noreply.github.com>
2021-07-15 11:32:49 +0200

    ml > 2 lmr cond

--- src/search.cpp
+++ src/search.cpp
@@ -1122,6 +1122,7 @@ moves_loop: // When in check, search starts from here
       // cases where we extend a son if it has good chances to be "interesting".
       if (    depth >= 3
           &&  moveCount > 1 + 2 * rootNode
+		  &&  MoveList<LEGAL>(pos).size() > 2
           && (  !captureOrPromotion
               || (cutNode && (ss-1)->moveCount > 1)
               || !ss->ttPv)
