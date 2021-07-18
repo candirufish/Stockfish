b64a140e4dab7cef7a9340e3c151af61c2c0b500
candirufish <38038147+candirufish@users.noreply.github.com>
2021-07-18 14:01:35 +0200

    no cpr op king ring if ss-1 inc

--- src/search.cpp
+++ src/search.cpp
@@ -1005,7 +1005,7 @@ moves_loop: // When in check, search starts from here
           {
               // Capture history based pruning when the move doesn't give check
               if (   !givesCheck
-			      && !ss->inCheck
+			      && !((ss-1)->inCheck && distance(pos.square<KING>(~pos.side_to_move()), to_sq(move)) <= 2)
                   && lmrDepth < 1
                   && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 0)
                   continue;
