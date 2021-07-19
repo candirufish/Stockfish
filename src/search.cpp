49df703608eabf10d2440b6d7bd149df67045c0a
candirufish <38038147+candirufish@users.noreply.github.com>
2021-07-19 10:43:37 +0200

    no pr non pawn cap when inc

--- src/search.cpp
+++ src/search.cpp
@@ -1005,7 +1005,7 @@ moves_loop: // When in check, search starts from here
           {
               // Capture history based pruning when the move doesn't give check
               if (   !givesCheck
-			      && !ss->inCheck
+			      && !(ss->inCheck && type_of(pos.piece_on(to_sq(move))) != PAWN)
                   && lmrDepth < 1
                   && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 0)
                   continue;
