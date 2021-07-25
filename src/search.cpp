afd00b57ad4ad0ae29e74d2c75d307669fb3a087
candirufish <38038147+candirufish@users.noreply.github.com>
2021-07-25 16:48:38 +0200

    cm not prcap ttpv r--

--- src/search.cpp
+++ src/search.cpp
@@ -1132,7 +1132,7 @@ moves_loop: // When in check, search starts from here
           if (PvNode)
               r--;
 		  
-		  if (move == countermove && !priorCapture)
+		  if (ss->ttPv && move == countermove && !priorCapture)
 			  r--;
 
           // Decrease reduction if the ttHit running average is large (~0 Elo)
