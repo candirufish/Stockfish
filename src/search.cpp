c952c2504cb03ec0fdc4bcae0d4c8c40c2c8ff76
candirufish <38038147+candirufish@users.noreply.github.com>
2021-07-25 04:41:11 +0200

    lmrd 2 if imp

--- src/search.cpp
+++ src/search.cpp
@@ -1005,7 +1005,7 @@ moves_loop: // When in check, search starts from here
           {
               // Capture history based pruning when the move doesn't give check
               if (   !givesCheck
-                  && lmrDepth < 1 + 2 * improving
+                  && lmrDepth < 1 + improving
                   && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 0)
                   continue;
 
