81890e95b1b95dd8f7f61403940372b006b1a745
candirufish <38038147+candirufish@users.noreply.github.com>
2021-07-27 08:48:22 +0200

    statscore n dblext

--- src/search.cpp
+++ src/search.cpp
@@ -1152,7 +1152,7 @@ moves_loop: // When in check, search starts from here
               r--;
 
           // Decrease reduction if ttMove has been singularly extended (~1 Elo)
-          if (singularQuietLMR  && !doubleExtension)
+          if (singularQuietLMR)
               r--;
 
           // Increase reduction for cut nodes (~3 Elo)
@@ -1170,7 +1170,8 @@ moves_loop: // When in check, search starts from here
                          - 4923;
 
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
-          r -= ss->statScore / 14721;
+		  if (!doubleExtension)
+				r -= ss->statScore / 14721;
 
           // In general we want to cap the LMR depth search at newDepth. But if
           // reductions are really negative and movecount is low, we allow this move
