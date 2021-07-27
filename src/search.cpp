7a9ba4b7f7370ebbd4542a1cf2e89ac023a81101
candirufish <38038147+candirufish@users.noreply.github.com>
2021-07-27 20:30:56 +0200

    mvc and sts ndblext

--- src/search.cpp
+++ src/search.cpp
@@ -1146,11 +1146,7 @@ moves_loop: // When in check, search starts from here
           if (   (rootNode || !PvNode)
               && thisThread->bestMoveChanges <= 2)
               r++;
-
-          // Decrease reduction if opponent's move count is high (~1 Elo)
-          if ((ss-1)->moveCount > 13)
-              r--;
-
+         
           // Decrease reduction if ttMove has been singularly extended (~1 Elo)
           if (singularQuietLMR)
               r--;
@@ -1168,9 +1164,16 @@ moves_loop: // When in check, search starts from here
                          + (*contHist[1])[movedPiece][to_sq(move)]
                          + (*contHist[3])[movedPiece][to_sq(move)]
                          - 4923;
-
-          // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
-          r -= ss->statScore / 14721;
+         
+		  if (!doubleExtension)
+		  {
+			    // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
+				r -= ss->statScore / 14721;
+				
+				// Decrease reduction if opponent's move count is high (~1 Elo)
+		        if ((ss-1)->moveCount > 13)
+					r--;
+		  }
 
           // In general we want to cap the LMR depth search at newDepth. But if
           // reductions are really negative and movecount is low, we allow this move
