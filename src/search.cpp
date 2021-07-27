9b849af0e448f76f70077eff96eb037455d773a5
candirufish <38038147+candirufish@users.noreply.github.com>
2021-07-27 08:19:10 +0200

    tthit n dblext r--

--- src/search.cpp
+++ src/search.cpp
@@ -1129,11 +1129,11 @@ moves_loop: // When in check, search starts from here
       {
           Depth r = reduction(improving, depth, moveCount);
 
-          if (PvNode && !doubleExtension)
+          if (PvNode)
               r--;
 
           // Decrease reduction if the ttHit running average is large (~0 Elo)
-          if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
+          if ((thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)  && !doubleExtension)
               r--;
 
           // Decrease reduction if position is or has been on the PV
