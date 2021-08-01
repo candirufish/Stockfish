d0a069799b4b6da1fed8363c9b5e758d5ffa2006
candirufish <38038147+candirufish@users.noreply.github.com>
2021-08-01 10:48:01 +0200

    no pr inc pv and ttm

--- src/search.cpp
+++ src/search.cpp
@@ -991,7 +991,7 @@ moves_loop: // When in check, search starts here
 
       // Step 13. Pruning at shallow depth (~200 Elo)
       if (  !rootNode
-	      && !(ttMove && ss->inCheck)  
+	      && !(PvNode && ttMove && ss->inCheck)  
           && pos.non_pawn_material(us)
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
       {
