eeaa2c616816c5fafb42fb14c2030485eaccca3c
candirufish <38038147+candirufish@users.noreply.github.com>
2021-08-02 03:09:56 +0200

    no prcap if inc and d1

--- src/search.cpp
+++ src/search.cpp
@@ -1005,7 +1005,7 @@ moves_loop: // When in check, search starts here
           {
               // Capture history based pruning when the move doesn't give check
               if (   !givesCheck
-				  && !(improving && depth == 1)
+				  && !(ss->inCheck && depth == 1)
                   && lmrDepth < 1
                   && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 0)
                   continue;
