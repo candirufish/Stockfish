7b86a3a2a49caa6301407458e9a60c533572faa9
candirufish <38038147+candirufish@users.noreply.github.com>
2021-08-03 12:15:35 +0200

    conthist 0 1 mh

--- src/search.cpp
+++ src/search.cpp
@@ -1018,7 +1018,8 @@ moves_loop: // When in check, search starts here
               // Continuation history based pruning (~20 Elo)
               if (   lmrDepth < 5
                   && (*contHist[0])[movedPiece][to_sq(move)] < (depth == 1 ? 0 : -stat_bonus(depth-1))
-                  && (*contHist[1])[movedPiece][to_sq(move)] < (depth == 1 ? 0 : -stat_bonus(depth-1)))
+                  && (*contHist[1])[movedPiece][to_sq(move)] < (depth == 1 ? 0 : -stat_bonus(depth-1))
+				  && thisThread->mainHistory[pos.side_to_move()][from_to(move)] < (depth == 1 ? 0 : -stat_bonus(depth-1)))
                   continue;
 
               // Futility pruning: parent node (~5 Elo)
