c565abdcc7cf0fde907f194c5851ea39e3d0011e
candirufish <38038147+candirufish@users.noreply.github.com>
2021-08-03 12:43:14 +0200

    ch0 mh -6200 pr

--- src/search.cpp
+++ src/search.cpp
@@ -1520,7 +1520,7 @@ moves_loop: // When in check, search starts here
       if (  !captureOrPromotion
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY
           && (*contHist[0])[pos.moved_piece(move)][to_sq(move)] < CounterMovePruneThreshold
-          && (*contHist[1])[pos.moved_piece(move)][to_sq(move)] < CounterMovePruneThreshold)
+          && thisThread->mainHistory[pos.side_to_move()][from_to(move)] < -6200)
           continue;
 
       // Make and search the move
