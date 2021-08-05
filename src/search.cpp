de50bb18665a670a73e4fe08718a25e4799ef5ce
candirufish <38038147+candirufish@users.noreply.github.com>
2021-08-05 02:33:42 +0200

    ch1 simp

--- src/search.cpp
+++ src/search.cpp
@@ -1519,8 +1519,7 @@ moves_loop: // When in check, search starts here
       // Continuation history based pruning
       if (  !captureOrPromotion
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY
-          && (*contHist[0])[pos.moved_piece(move)][to_sq(move)] < CounterMovePruneThreshold
-          && (*contHist[1])[pos.moved_piece(move)][to_sq(move)] < CounterMovePruneThreshold)
+          && (*contHist[0])[pos.moved_piece(move)][to_sq(move)] < CounterMovePruneThreshold)
           continue;
 
       // Make and search the move
