2d1711e3eb7d17a0339d0acba48e66f8fbad36ce
candirufish <38038147+candirufish@users.noreply.github.com>
2021-08-07 14:48:15 +0200

    k1 and cm quiet not prcap pv 2 1

--- src/search.cpp
+++ src/search.cpp
@@ -1129,7 +1129,7 @@ moves_loop: // When in check, search starts here
           Depth r = reduction(improving, depth, moveCount);
 
           if (PvNode)
-              r -= (move == countermove && move == ss->killers[1]) ? 3 : 1;		  
+              r -= (move == countermove && move == ss->killers[1] && !captureOrPromotion && !priorCapture) ? 2 : 1;		  
 
           // Decrease reduction if the ttHit running average is large (~0 Elo)
           if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
