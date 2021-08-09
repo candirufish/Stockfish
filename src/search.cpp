ce00156bad442120967bdc8fc74c16d564a45bfa
candirufish <38038147+candirufish@users.noreply.github.com>
2021-08-09 23:27:24 +0200

    cmpt cp and gc d6 ch01 r++

--- src/search.cpp
+++ src/search.cpp
@@ -1170,6 +1170,12 @@ moves_loop: // When in check, search starts here
 
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           r -= ss->statScore / 14721;
+		  
+		  if (!captureOrPromotion && !givesCheck
+		   && depth < 6
+		   && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
+           && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
+		      r++;
 
           // In general we want to cap the LMR depth search at newDepth. But if
           // reductions are really negative and movecount is low, we allow this move
