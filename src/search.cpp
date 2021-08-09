60a8d5729cec1a1e89abf4f4f0897517c8626b9d
candirufish <38038147+candirufish@users.noreply.github.com>
2021-08-09 03:54:25 +0200

    no cnlmr k0 and q less for cap pos stat

--- src/search.cpp
+++ src/search.cpp
@@ -1154,10 +1154,6 @@ moves_loop: // When in check, search starts here
           if (singularQuietLMR)
               r--;
 
-          // Increase reduction for cut nodes (~3 Elo)
-          if (cutNode && !(move == ss->killers[0] && !captureOrPromotion))
-              r += 1 + !captureOrPromotion;
-
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
               r++;
@@ -1170,6 +1166,10 @@ moves_loop: // When in check, search starts here
 
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           r -= ss->statScore / 14721;
+		  
+		  // Increase reduction for cut nodes (~3 Elo)
+          if (cutNode && !(move == ss->killers[0] && !captureOrPromotion))
+              r += 1 + (!captureOrPromotion && ss->statScore > 0 );
 
           // In general we want to cap the LMR depth search at newDepth. But if
           // reductions are really negative and movecount is low, we allow this move
