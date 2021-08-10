552d240a5d46608cf754ed47efcbb815e7ee8b5a
candirufish <38038147+candirufish@users.noreply.github.com>
2021-08-10 10:52:00 +0200

    k1 mv <=5

--- src/search.cpp
+++ src/search.cpp
@@ -1170,6 +1170,9 @@ moves_loop: // When in check, search starts here
 
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           r -= ss->statScore / 14721;
+		  
+		  if (move == ss->killers[1] && moveCount <= 5)
+			  r--;
 
           // In general we want to cap the LMR depth search at newDepth. But if
           // reductions are really negative and movecount is low, we allow this move
