3668dea842df49be61d564ee92bdc3db001b08bb
candirufish <38038147+candirufish@users.noreply.github.com>
2021-08-14 00:23:52 +0200

    no dblext cap neg see no gc

--- src/search.cpp
+++ src/search.cpp
@@ -1066,7 +1066,7 @@ moves_loop: // When in check, search starts here
               // Avoid search explosion by limiting the number of double extensions to at most 3
               if (   !PvNode
                   && value < singularBeta - 93
-				  && !(captureOrPromotion && !pos.see_ge(move))
+				  && !(captureOrPromotion && !pos.see_ge(move) && !givesCheck)
                   && ss->doubleExtensions < 3)
               {
                   extension = 2;
