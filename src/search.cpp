ce80c80574699e8ed7b69a2aee47dc064826367f
candirufish <38038147+candirufish@users.noreply.github.com>
2021-08-14 00:13:30 +0200

    dblext r50 <= 6

--- src/search.cpp
+++ src/search.cpp
@@ -1066,7 +1066,7 @@ moves_loop: // When in check, search starts here
               // Avoid search explosion by limiting the number of double extensions to at most 3
               if (   !PvNode
                   && value < singularBeta - 93
-				  && !(captureOrPromotion && !pos.see_ge(move))
+				  && pos.rule50_count() <= 6
                   && ss->doubleExtensions < 3)
               {
                   extension = 2;
