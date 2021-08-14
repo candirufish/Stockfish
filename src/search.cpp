b9b11e821a21ce67f3afb698f20f9c1d21866b82
candirufish <38038147+candirufish@users.noreply.github.com>
2021-08-14 10:17:50 +0200

    d + cn not ttpv

--- src/search.cpp
+++ src/search.cpp
@@ -1316,7 +1316,7 @@ moves_loop: // When in check, search starts here
     // Bonus for prior countermove that caused the fail low
     else if (   (depth >= 3 || PvNode)
              && !priorCapture)
-        update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, stat_bonus(depth + (cutNode && ss->ttPv)));
+        update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, stat_bonus(depth + (cutNode && !ss->ttPv)));
 
     if (PvNode)
         bestValue = std::min(bestValue, maxValue);
