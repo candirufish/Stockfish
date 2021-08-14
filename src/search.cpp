478747bab8a7ae55ed72ab584bdc0957bac3d1d6
candirufish <38038147+candirufish@users.noreply.github.com>
2021-08-14 06:59:58 +0200

    no cnlmr for k1 and cm

--- src/search.cpp
+++ src/search.cpp
@@ -1155,8 +1155,8 @@ moves_loop: // When in check, search starts here
               r--;
 
           // Increase reduction for cut nodes (~3 Elo)
-          if (cutNode && move != ss->killers[0])
-              r += 2 - (move == ss->killers[1] && move == countermove);
+          if (cutNode && move != ss->killers[0] && !(move == ss->killers[1] && move == countermove))
+              r += 2;
 
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
