215834bdf1aa053e6851770afb7785e60a898a80
candirufish <38038147+candirufish@users.noreply.github.com>
2021-08-15 05:05:52 +0200

    -1 cnlmr k1 ttpv and quiet ss1 ns

--- src/search.cpp
+++ src/search.cpp
@@ -1166,7 +1166,8 @@ moves_loop: // When in check, search starts here
 						 
 		  // Increase reduction for cut nodes (~3 Elo)
           if (cutNode && move != ss->killers[0])
-		      r += (move == ss->killers[1] && ss->ttPv && !captureOrPromotion) ? -1 : 2;
+		      r += (move == ss->killers[1] && ss->ttPv 
+		       && (ss-1)->statScore < 0 && !captureOrPromotion) ? -1 : 2;
 
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           r -= ss->statScore / 14721;
