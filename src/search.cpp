713d3b3e5975b76965bd239b8aeaebe1c9420cae
candirufish <38038147+candirufish@users.noreply.github.com>
2021-08-18 23:54:57 +0200

    k2 and cm r--

--- src/search.cpp
+++ src/search.cpp
@@ -1170,6 +1170,9 @@ moves_loop: // When in check, search starts here
 
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           r -= ss->statScore / 14721;
+		  
+		  if (move == ss->killers[2] && move == countermove)
+			  r--;
 
           // In general we want to cap the LMR depth search at newDepth. But if
           // reductions are really negative and movecount is low, we allow this move
@@ -1198,9 +1201,6 @@ moves_loop: // When in check, search starts here
           {
               int bonus = value > alpha ?  stat_bonus(newDepth)
                                         : -stat_bonus(newDepth);
-										
-		      if (move == ss->killers[2])
-                    bonus += bonus * 3 / 4;
 
               update_continuation_histories(ss, movedPiece, to_sq(move), bonus);
           }
