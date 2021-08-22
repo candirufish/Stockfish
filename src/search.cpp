6fe998be3d0b88e73380d02faff0015d04073ed5
candirufish <38038147+candirufish@users.noreply.github.com>
2021-08-22 12:58:05 +0200

    r neg cap bonus 4 fix

--- src/search.cpp
+++ src/search.cpp
@@ -1173,6 +1173,8 @@ moves_loop: // When in check, search starts here
 		  
 		  if (r < - 1)
 			  rneg = true;
+		  else 
+			  rneg = false;
 
           // In general we want to cap the LMR depth search at newDepth. But if
           // reductions are really negative and movecount is low, we allow this move
