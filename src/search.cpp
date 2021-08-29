c7f8d3ec0434c9a6561a9471ab465b08199493a4
candirufish <38038147+candirufish@users.noreply.github.com>
2021-08-29 19:31:09 +0200

    cm lmr r-- nb

--- src/search.cpp
+++ src/search.cpp
@@ -1162,6 +1162,9 @@ moves_loop: // When in check, search starts here
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
               r++;
+		  
+		  if (move == countermove)
+			  r--;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
