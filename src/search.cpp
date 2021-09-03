6d15f386ca8d757b8ede56baba94ad97628a210f
candirufish <38038147+candirufish@users.noreply.github.com>
2021-09-03 08:19:52 +0200

    rc lfl 2 1

--- src/search.cpp
+++ src/search.cpp
@@ -1140,9 +1140,8 @@ moves_loop: // When in check, search starts here
           // Decrease reduction if position is or has been on the PV
           // and node is not likely to fail low. (~3 Elo)
           if (   ss->ttPv
-		      && us == rootColor
               && !likelyFailLow)
-              r -= 2;
+              r -= us == rootColor ? 2 : 1;
 
           // Increase reduction at root and non-PV nodes when the best move does not change frequently
           if (   (rootNode || !PvNode)
