ee6441bd70ff1beefcacd697687308aa7d243f8f
candirufish <38038147+candirufish@users.noreply.github.com>
2021-09-03 08:12:12 +0200

    us rc bmc r++

--- src/search.cpp
+++ src/search.cpp
@@ -71,6 +71,7 @@ namespace {
 
   // Reductions lookup table, initialized at startup
   int Reductions[MAX_MOVES]; // [depth or moveNumber]
+  Color rootColor;
 
   Depth reduction(bool i, Depth d, int mn) {
     int r = Reductions[d] * Reductions[mn];
@@ -261,7 +262,7 @@ void Thread::search() {
   Depth lastBestMoveDepth = 0;
   MainThread* mainThread = (this == Threads.main() ? Threads.main() : nullptr);
   double timeReduction = 1, totBestMoveChanges = 0;
-  Color us = rootPos.side_to_move();
+  Color us = rootColor = rootPos.side_to_move();
   int iterIdx = 0;
 
   std::memset(ss-7, 0, 10 * sizeof(Stack));
@@ -1144,6 +1145,7 @@ moves_loop: // When in check, search starts here
 
           // Increase reduction at root and non-PV nodes when the best move does not change frequently
           if (   (rootNode || !PvNode)
+			  && us == rootColor
               && thisThread->bestMoveChanges <= 2)
               r++;
 
