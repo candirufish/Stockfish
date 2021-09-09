1b8f7cccf12b9a161febad6859a7ac57c4e3a22e
candirufish <38038147+candirufish@users.noreply.github.com>
2021-09-09 21:18:49 +0200

    cp ext cn k1

--- src/search.cpp
+++ src/search.cpp
@@ -1096,7 +1096,7 @@ moves_loop: // When in check, search starts here
       }
 
       // Capture extensions for PvNodes and cutNodes
-      else if (   (PvNode || (cutNode && move == ss->killers[0])) 
+      else if (   (PvNode || (cutNode && move == ss->killers[1])) 
                && captureOrPromotion 
                && moveCount != 1)
           extension = 1;
