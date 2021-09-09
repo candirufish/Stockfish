7357788a543d16d6d82504432ddf618bf361d00c
candirufish <38038147+candirufish@users.noreply.github.com>
2021-09-09 21:10:42 +0200

    cp ext type not pawn

--- src/search.cpp
+++ src/search.cpp
@@ -1098,6 +1098,7 @@ moves_loop: // When in check, search starts here
       // Capture extensions for PvNodes and cutNodes
       else if (   (PvNode || cutNode) 
                && captureOrPromotion 
+		       && type_of(pos.piece_on(to_sq(move))) != PAWN
                && moveCount != 1)
           extension = 1;
 
