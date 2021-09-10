fa23ef223b83ab95fd8e936f7f967dfdaa73b308
candirufish <38038147+candirufish@users.noreply.github.com>
2021-09-10 12:34:17 +0200

    adv pwn k0 k2 ext

--- src/search.cpp
+++ src/search.cpp
@@ -1106,7 +1106,7 @@ moves_loop: // When in check, search starts here
                && moveCount != 1)
           extension = 1;
 		  
-      else if (   move == ss->killers[2]
+      else if (   move == ss->killers[2] && move == ss->killers[0]
                && pos.advanced_pawn_push(move)
                && pos.pawn_passed(us, to_sq(move)))
           extension = 1;
