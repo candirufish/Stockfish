76cb49a251f4938d95d9c3abfac21cefe97f60fa
candirufish <38038147+candirufish@users.noreply.github.com>
2021-09-11 10:40:22 +0200

    2 ttm cn lmr

--- src/search.cpp
+++ src/search.cpp
@@ -1170,7 +1170,7 @@ moves_loop: // When in check, search starts here
 
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode && move != ss->killers[0])
-              r += 2 + !ttMove;
+              r += 1 + !ttMove;
 
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
