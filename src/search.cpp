d94bc5674ed39b322476a16b3d08fd1aa9276bc9
candirufish <38038147+candirufish@users.noreply.github.com>
2022-01-05 16:05:57 +0100

    k2 extension

--- src/search.cpp
+++ src/search.cpp
@@ -634,7 +634,7 @@ namespace {
 
     (ss+1)->ttPv         = false;
     (ss+1)->excludedMove = bestMove = MOVE_NONE;
-    (ss+2)->killers[0]   = (ss+2)->killers[1] = MOVE_NONE;
+    (ss+2)->killers[0]   = (ss+2)->killers[1] = (ss + 2)->killers[2] = MOVE_NONE;
     ss->doubleExtensions = (ss-1)->doubleExtensions;
     ss->depth            = depth;
     Square prevSq        = to_sq((ss-1)->currentMove);
@@ -1124,7 +1124,7 @@ moves_loop: // When in check, search starts here
       // Quiet ttMove extensions (~0 Elo)
       else if (   PvNode
                && move == ttMove
-               && move == ss->killers[0]
+               && move == ss->killers[2]
                && (*contHist[0])[movedPiece][to_sq(move)] >= 10000)
           extension = 1;
 
@@ -1734,11 +1734,13 @@ moves_loop: // When in check, search starts here
   void update_quiet_stats(const Position& pos, Stack* ss, Move move, int bonus) {
 
     // Update killers
-    if (ss->killers[0] != move)
-    {
-        ss->killers[1] = ss->killers[0];
-        ss->killers[0] = move;
-    }
+      if (ss->killers[0] != move)
+      {
+          ss->killers[1] = ss->killers[0];
+          ss->killers[0] = move;
+      }
+      else
+          ss->killers[2] = move;
 
     Color us = pos.side_to_move();
     Thread* thisThread = pos.this_thread();
