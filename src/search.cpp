174c496a1a3b6b4a66784734ef655b0a39bd9949
candirufish <38038147+candirufish@users.noreply.github.com>
2022-01-13 10:55:21 +0100

    less cn lmr inc r50 > 11

--- src/search.cpp
+++ src/search.cpp
@@ -1176,7 +1176,7 @@ moves_loop: // When in check, search starts here
 
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode && move != ss->killers[0])
-              r += 2 - (ss->inCheck && pos.rule50_count() <= 11);
+              r += 2 - (ss->inCheck && pos.rule50_count() > 11);
 
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
