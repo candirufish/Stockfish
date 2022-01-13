6a2ca064bb58f07c63f19ba88e4110a8cb9cdbe8
candirufish <38038147+candirufish@users.noreply.github.com>
2022-01-13 11:27:12 +0100

    less cn lmr np capt

--- src/search.cpp
+++ src/search.cpp
@@ -1176,7 +1176,7 @@ moves_loop: // When in check, search starts here
 
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode && move != ss->killers[0])
-              r += 2 - (captureOrPromotion && type_of(pos.piece_on(to_sq(move))) == QUEEN);
+              r += 2 - (captureOrPromotion && type_of(pos.piece_on(to_sq(move))) != PAWN);
 
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
