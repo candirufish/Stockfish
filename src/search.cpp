9e79b3fd2009aef06adeea5d65b35ba9129d5956
candirufish <38038147+candirufish@users.noreply.github.com>
2022-01-14 11:27:46 +0100

    quiet and not inc lmr

--- src/search.cpp
+++ src/search.cpp
@@ -1179,19 +1179,22 @@ moves_loop: // When in check, search starts here
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode && move != ss->killers[0])
               r += 2;
+		  
+		  if (!captureOrPromotion && !ss->inCheck)
+          {
+				// Increase reduction if ttMove is a capture (~3 Elo)
+				if (ttCapture)
+					r++;
 
-          // Increase reduction if ttMove is a capture (~3 Elo)
-          if (ttCapture)
-              r++;
-
-          ss->statScore =  thisThread->mainHistory[us][from_to(move)]
+				ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
                          + (*contHist[1])[movedPiece][to_sq(move)]
                          + (*contHist[3])[movedPiece][to_sq(move)]
                          - 4923;
 
-          // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
-          r -= ss->statScore / 14721;
+				// Decrease/increase reduction for moves with a good/bad history (~30 Elo)
+				r -= ss->statScore / 14721;
+		  }
 
           // In general we want to cap the LMR depth search at newDepth. But if reductions
           // are really negative and movecount is low, we allow this move to be searched
