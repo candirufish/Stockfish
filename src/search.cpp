cd9004cde1ac6b6a99b21fad15ad50cb76bd0371
candirufish <38038147+candirufish@users.noreply.github.com>
2022-01-26 21:52:20 +0100

    ! d <= 6 cp cnlmr

--- src/search.cpp
+++ src/search.cpp
@@ -1148,8 +1148,8 @@ moves_loop: // When in check, search starts here
               r--;
 
           // Increase reduction for cut nodes (~3 Elo)
-          if (cutNode && move != ss->killers[0])
-              r += captureOrPromotion && depth >= 6 ? 1 : 2;
+          if (cutNode && move != ss->killers[0] && !(captureOrPromotion && depth <= 6))
+			  r += 2;
 
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
