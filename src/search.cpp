1cee11bacdc0e19ef8f91165d7643f4dad2bf3c8
candirufish <38038147+candirufish@users.noreply.github.com>
2022-01-27 21:45:52 +0100

    rn bv se -400 r--

--- src/search.cpp
+++ src/search.cpp
@@ -1147,7 +1147,7 @@ moves_loop: // When in check, search starts here
           if ((ss-1)->moveCount > 13)
               r--;
 		  
-		  if (rootNode && ss->staticEval != VALUE_NONE && bestValue - ss->staticEval < -300)
+		  if (rootNode && ss->staticEval != VALUE_NONE && bestValue - ss->staticEval < -400)
               r--;
 
           // Increase reduction for cut nodes (~3 Elo)
