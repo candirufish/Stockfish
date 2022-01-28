15ba61e53e3001617ce4e42523d27a2b7e40815b
candirufish <38038147+candirufish@users.noreply.github.com>
2022-01-28 07:04:03 +0100

    ! ttpv and cp or inc cnlmr

--- src/search.cpp
+++ src/search.cpp
@@ -1148,8 +1148,8 @@ moves_loop: // When in check, search starts here
               r--;
 
           // Increase reduction for cut nodes (~3 Elo)
-          if (cutNode && move != ss->killers[0])
-              r += ss->ttPv && (captureOrPromotion || ss->inCheck) ? 1 : 2;
+          if (cutNode && move != ss->killers[0] && !(ss->ttPv && (captureOrPromotion || ss->inCheck)))
+              r += 2;
 
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
