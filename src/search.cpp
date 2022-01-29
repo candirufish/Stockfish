64d06169a29778f633096742833ded0b21e5efc3
candirufish <38038147+candirufish@users.noreply.github.com>
2022-01-29 22:14:20 +0100

    pv cp 50r <= 10

--- src/search.cpp
+++ src/search.cpp
@@ -1089,8 +1089,7 @@ moves_loop: // When in check, search starts here
 		  
 		  else if (  PvNode 
                && captureOrPromotion
-			   &&  abs(ss->staticEval) <= Value(200)
-			   &&  abs(ss->staticEval) >= Value(10)
+			   && pos.rule50_count() <= 10
 		       && complexity > 1000)
               extension = 1;
 
