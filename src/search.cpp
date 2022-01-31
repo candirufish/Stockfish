81fb43884f64c91a029cfe29a02e64ccd279c0fa
candirufish <38038147+candirufish@users.noreply.github.com>
2022-01-31 23:06:26 +0100

    abs ev simplify

--- src/search.cpp
+++ src/search.cpp
@@ -1089,8 +1089,7 @@ moves_loop: // When in check, search starts here
 
           // Check extensions (~1 Elo)
           else if (   givesCheck
-                   && depth > 6
-                   && abs(ss->staticEval) > 100)
+                   && depth > 6)
               extension = 1;
 
           // Quiet ttMove extensions (~0 Elo)
