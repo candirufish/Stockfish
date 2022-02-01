396a4a142176dc225d693e789e7edb8e8917f26f
candirufish <38038147+candirufish@users.noreply.github.com>
2022-02-01 16:47:36 +0100

    ! rn cpx > 1000 lfl

--- src/search.cpp
+++ src/search.cpp
@@ -1140,7 +1140,7 @@ moves_loop: // When in check, search starts here
           // Decrease reduction if position is or has been on the PV
           // and node is not likely to fail low. (~3 Elo)
           if (   ss->ttPv
-		      && !rootNode
+		      && !(rootNode && complexity > 1000)
               && !likelyFailLow)
               r -= 2;
 
