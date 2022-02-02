c922dd68ed09a6a388f403caf1f29c2e0d79f116
candirufish <38038147+candirufish@users.noreply.github.com>
2022-02-02 23:38:21 +0100

    q res r++

--- src/search.cpp
+++ src/search.cpp
@@ -1155,7 +1155,7 @@ moves_loop: // When in check, search starts here
           if (ttCapture)
               r++;
 		  
-		  if (!captureOrPromotion && (ss-1)->research && complexity < 50)
+		  if (!captureOrPromotion && (ss-1)->research)
               r++;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
