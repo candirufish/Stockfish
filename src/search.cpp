25e279ce08cc7de3964f42620faf36718ea31ef5
candirufish <38038147+candirufish@users.noreply.github.com>
2022-02-03 11:11:25 +0100

    400k ext tuning

--- src/search.cpp
+++ src/search.cpp
@@ -1069,7 +1069,7 @@ moves_loop: // When in check, search starts here
 
                   // Avoid search explosion by limiting the number of double extensions
                   if (  !PvNode
-                      && value < singularBeta - 75
+                      && value < singularBeta - 74
                       && ss->doubleExtensions <= 6)
                       extension = 2;
               }
@@ -1097,7 +1097,7 @@ moves_loop: // When in check, search starts here
           else if (   PvNode
                    && move == ttMove
                    && move == ss->killers[0]
-                   && (*contHist[0])[movedPiece][to_sq(move)] >= 10000)
+                   && (*contHist[0])[movedPiece][to_sq(move)] >= 10079)
               extension = 1;
       }
 
