79d46e57f2d37ac8cdca981af6cb4bf6dc7c0c8b
candirufish <38038147+candirufish@users.noreply.github.com>
2022-03-21 00:18:37 +0100

    ncp lmr stats

--- src/search.cpp
+++ src/search.cpp
@@ -1164,14 +1164,16 @@ moves_loop: // When in check, search starts here
               r += 2;
 
           // Increase reduction if ttMove is a capture (~3 Elo)
-          if (ttCapture && !capture)
+          if (ttCapture)
               r++;
 
           // Decrease reduction at PvNodes if bestvalue
           // is vastly different from static evaluation
           if (PvNode && !ss->inCheck && abs(ss->staticEval - bestValue) > 250)
               r--;
-
+          
+		  if (!capture)
+		  {
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
                          + (*contHist[1])[movedPiece][to_sq(move)]
@@ -1180,7 +1182,7 @@ moves_loop: // When in check, search starts here
 
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           r -= ss->statScore / 15914;
-
+          }
           // In general we want to cap the LMR depth search at newDepth. But if reductions
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions).
