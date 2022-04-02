c7c4309d5e3c5a2f77cae92243caee43c7f861b4
candirufish <38038147+candirufish@users.noreply.github.com>
2022-04-02 19:53:20 +0200

    queen promote ext quiet ext

--- src/search.cpp
+++ src/search.cpp
@@ -1115,7 +1115,7 @@ moves_loop: // When in check, search starts here
 	      // Promotion extension
           if ( type_of(move) == PROMOTION 
 				   && promotion_type(move) == QUEEN
-				   && complexity > 200)
+				   && !capture)
               extension = 1;
 
       }
