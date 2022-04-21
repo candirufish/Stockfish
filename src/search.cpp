6161640ab705fe5550e82930a1a732b3b0c6ecec
candirufish <38038147+candirufish@users.noreply.github.com>
2022-04-21 03:20:31 +0200

    ttv beta ext -3 ttv <= value

--- src/search.cpp
+++ src/search.cpp
@@ -1097,7 +1097,7 @@ moves_loop: // When in check, search starts here
 
               // If the eval of ttMove is greater than beta, we reduce it (negative extension)
               else if (ttValue >= beta)
-                  extension = -2;
+                  extension = -(2 + (ttValue <= value));
           }
 
           // Check extensions (~1 Elo)
