76a1e05ff2e873f57619a7caa9faa919a5ce44c7
candirufish <38038147+candirufish@users.noreply.github.com>
2022-04-25 15:01:37 +0200

    fcapt cnlmr 1

--- src/search.cpp
+++ src/search.cpp
@@ -1163,8 +1163,8 @@ moves_loop: // When in check, search starts here
               r--;
 
           // Increase reduction for cut nodes (~3 Elo)
-          if (cutNode && move != ss->killers[0] && !firstCapture)
-              r += 2;
+          if (cutNode && move != ss->killers[0])
+              r += firstCapture ? 1 : 2;
 
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
