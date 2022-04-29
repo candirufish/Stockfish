90cee9d49c5973868e0deb028e8011b0cf3062d3
candirufish <38038147+candirufish@users.noreply.github.com>
2022-04-29 07:37:26 +0200

    cut cnt <= 3 cm bonus

--- src/search.cpp
+++ src/search.cpp
@@ -1177,9 +1177,6 @@ moves_loop: // When in check, search starts here
           if (PvNode)
               r -= 1 + 15 / ( 3 + depth );
 		  
-		  if ((ss+1)->cutoffCnt > 3 && !PvNode && move != ss->killers[0])
-              r++;
-
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
                          + (*contHist[1])[movedPiece][to_sq(move)]
@@ -1348,7 +1345,8 @@ moves_loop: // When in check, search starts here
 
     // Bonus for prior countermove that caused the fail low
     else if (   (depth >= 4 || PvNode)
-             && !priorCapture)
+             && !priorCapture
+		     && (ss+1)->cutoffCnt <= 3)
     {
         //Assign extra bonus if current node is PvNode or cutNode
         //or fail low was really bad
