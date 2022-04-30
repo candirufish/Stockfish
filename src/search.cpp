84f74d4c2a9c222194be4d23cfebd084c6ce04c0
candirufish <38038147+candirufish@users.noreply.github.com>
2022-04-30 08:59:58 +0200

    not ttcap bonus

--- src/search.cpp
+++ src/search.cpp
@@ -1189,7 +1189,6 @@ moves_loop: // When in check, search starts here
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions).
           int deeper =   r >= -1                   ? 0
-					   : ttCapture				   ? 0
                        : moveCount <= 4            ? 2
                        : PvNode && depth > 4       ? 1
                        : cutNode && moveCount <= 8 ? 1
@@ -1221,7 +1220,7 @@ moves_loop: // When in check, search starts here
               int bonus = value > alpha ?  stat_bonus(newDepth)
                                         : -stat_bonus(newDepth);
 
-              if (capture)
+              if (capture && !ttCapture)
                   bonus /= 6;
 
               update_continuation_histories(ss, movedPiece, to_sq(move), bonus);
