ace8c89d6de97a4709857a032b0956d4bf4c90bc
candirufish <38038147+candirufish@users.noreply.github.com>
2022-05-04 00:29:10 +0200

    no pradvpwn r5 near 50 20

--- src/position.h
+++ src/position.h
@@ -127,6 +127,7 @@ public:
   bool pseudo_legal(const Move m) const;
   bool capture(Move m) const;
   bool gives_check(Move m) const;
+  bool advanced_pawn_push(Move m) const;
   Piece moved_piece(Move m) const;
   Piece captured_piece() const;
 
@@ -319,6 +320,11 @@ inline Bitboard Position::check_squares(PieceType pt) const {
 
 inline bool Position::pawn_passed(Color c, Square s) const {
   return !(pieces(~c, PAWN) & passed_pawn_span(c, s));
+}
+
+inline bool Position::advanced_pawn_push(Move m) const {
+  return   type_of(moved_piece(m)) == PAWN
+        && relative_rank(sideToMove, to_sq(m)) > RANK_5;
 }
 
 inline int Position::pawns_on_same_color_squares(Color c, Square s) const {
