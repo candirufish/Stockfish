3c32c12e5daba4d8678d67f6e8338dcdd9ee5fc1
candirufish <38038147+candirufish@users.noreply.github.com>
2022-05-06 01:05:17 +0200

    fpd + 2 * imp

--- src/search.cpp
+++ src/search.cpp
@@ -789,7 +789,7 @@ namespace {
     // Step 8. Futility pruning: child node (~25 Elo).
     // The depth condition is important for mate finding.
     if (   !ss->ttPv
-        &&  depth < 8 + improving
+        &&  depth < 8 + 2 * improving
         &&  eval - futility_margin(depth, improving) - (ss-1)->statScore / 256 >= beta
         &&  eval >= beta
         &&  eval < 26305) // larger than VALUE_KNOWN_WIN, but smaller than TB wins.
