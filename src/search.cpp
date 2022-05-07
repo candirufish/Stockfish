494bfbe7075c044254cbca7047443c667d0d0180
candirufish <38038147+candirufish@users.noreply.github.com>
2022-05-07 10:25:39 +0200

    rp nttpv r50 > 80 r++

--- src/search.cpp
+++ src/search.cpp
@@ -1183,7 +1183,7 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
-          if (repeating && !ss->ttPv)
+          if (repeating && !ss->ttPv && pos.rule50_count() > 80)
               r++;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
