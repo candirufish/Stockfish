82a1a5eb657ce25a7ed1d5c013890d06c815ce6c
candirufish <38038147+candirufish@users.noreply.github.com>
2022-05-08 07:32:49 +0200

    rp nttpv ninc r++

--- src/search.cpp
+++ src/search.cpp
@@ -1183,7 +1183,7 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
-          if (repeating && !ss->ttPv)
+          if (repeating && !ss->ttPv && !ss->inCheck)
               r++;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
