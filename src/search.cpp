c4b230560ef368be66169ecd9ad2c26a2a39484c
candirufish <38038147+candirufish@users.noreply.github.com>
2022-05-08 08:17:21 +0200

    r-3 ss1 mcp lmrext2

--- src/search.cpp
+++ src/search.cpp
@@ -950,6 +950,7 @@ moves_loop: // When in check, search starts here
 
     value = bestValue;
     moveCountPruning = false;
+	ss->mcp = false;
 
     // Indicate PvNodes that will probably fail low if the node was searched
     // at a depth equal or greater than the current depth, and the result of this search was a fail low.
@@ -1004,7 +1005,7 @@ moves_loop: // When in check, search starts here
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold (~7 Elo)
-          moveCountPruning = moveCount >= futility_move_count(improving, depth);
+          ss->mcp = moveCountPruning = moveCount >= futility_move_count(improving, depth);
 
           // Reduced depth of the next LMR search
           int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount, delta, thisThread->rootDelta), 0);
@@ -1192,11 +1193,12 @@ moves_loop: // When in check, search starts here
           // In general we want to cap the LMR depth search at newDepth. But if reductions
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions).
-          int deeper =   r >= -1                   ? 0
-                       : moveCount <= 4            ? 2
-                       : PvNode && depth > 4       ? 1
-                       : cutNode && moveCount <= 8 ? 1
-                       :                             0;
+          int deeper =   r >= -1                        ? 0
+                       : (ss-1)->mcp && r < -3          ? 2
+                       : moveCount <= 4                 ? 2
+                       : PvNode && depth > 4            ? 1
+                       : cutNode && moveCount <= 8      ? 1
+                       :                                  0;
 
           Depth d = std::clamp(newDepth - r, 1, newDepth + deeper);
 
