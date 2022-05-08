f42f73f1c8e3746d42a40e76c725f6123e235903
candirufish <38038147+candirufish@users.noreply.github.com>
2022-05-08 07:43:01 +0200

    50r > 80 cp or pwn pv r--

--- src/search.cpp
+++ src/search.cpp
@@ -1176,6 +1176,10 @@ moves_loop: // When in check, search starts here
           if (PvNode)
               r -= 1 + 15 / ( 3 + depth );
 
+          if (PvNode && pos.rule50_count() > 80
+             && (capture || type_of(movedPiece) == PAWN))
+              r--;
+
           // Increase reduction if next ply has a lot of fail high else reset count to 0
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
