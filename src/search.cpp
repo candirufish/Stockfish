bf12ec6d15db82d1b42c8c54b8b5fcff6efebd86
candirufish <38038147+candirufish@users.noreply.github.com>
2022-05-08 08:18:44 +0200

    r-2 ss1 mcp lmrext2

--- src/search.cpp
+++ src/search.cpp
@@ -1194,7 +1194,7 @@ moves_loop: // When in check, search starts here
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions).
           int deeper =   r >= -1                        ? 0
-                       : (ss-1)->mcp && r < -3          ? 2
+                       : (ss-1)->mcp && r < -2          ? 2
                        : moveCount <= 4                 ? 2
                        : PvNode && depth > 4            ? 1
                        : cutNode && moveCount <= 8      ? 1
