863ec45fd33c4d5ae0284afad67a72e1e13fb685
candirufish <38038147+candirufish@users.noreply.github.com>
2022-05-18 23:25:58 +0200

    qt stat fh lmr

--- src/search.cpp
+++ src/search.cpp
@@ -1179,12 +1179,12 @@ moves_loop: // When in check, search starts here
           if (PvNode)
               r -= 1 + 15 / ( 3 + depth );
 
+          if (!capture)
+         {
           // Increase reduction if next ply has a lot of fail high else reset count to 0
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
-          if (!capture)
-		  {
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
                          + (*contHist[1])[movedPiece][to_sq(move)]
@@ -1193,7 +1193,7 @@ moves_loop: // When in check, search starts here
 
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           r -= ss->statScore / 15914;
-		  }
+         }
 
           // In general we want to cap the LMR depth search at newDepth. But if reductions
           // are really negative and movecount is low, we allow this move to be searched
