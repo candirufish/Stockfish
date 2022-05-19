ce511824fd352a4b843acd849e3b1f5dcf86aebc
candirufish <38038147+candirufish@users.noreply.github.com>
2022-05-19 02:56:31 +0200

    ninc fh lmr

--- src/search.cpp
+++ src/search.cpp
@@ -1176,11 +1176,11 @@ moves_loop: // When in check, search starts here
               r--;
 
           // Decrease reduction for PvNodes based on depth
-          if (PvNode && !ss->inCheck)
+          if (PvNode)
               r -= 1 + 15 / ( 3 + depth );
 
           // Increase reduction if next ply has a lot of fail high else reset count to 0
-          if ((ss+1)->cutoffCnt > 3 && !PvNode)
+          if ((ss+1)->cutoffCnt > 3 && !PvNode && !ss->inCheck)
               r++;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
