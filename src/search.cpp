42363f6f0e4901fc5e069f0c7ff36a5d9b1d0fa1
candirufish <38038147+candirufish@users.noreply.github.com>
2022-05-19 02:47:02 +0200

    ninc mv lmr

--- src/search.cpp
+++ src/search.cpp
@@ -1155,12 +1155,12 @@ moves_loop: // When in check, search starts here
           // Decrease reduction if position is or has been on the PV
           // and node is not likely to fail low. (~3 Elo)
           if (   ss->ttPv
-              && !likelyFailLow
-			  && !ss->inCheck)
+              && !likelyFailLow)
               r -= 2;
 
           // Decrease reduction if opponent's move count is high (~1 Elo)
-          if ((ss-1)->moveCount > 7)
+          if ((ss-1)->moveCount > 7 
+			  && !ss->inCheck)
               r--;
 
           // Increase reduction for cut nodes (~3 Elo)
