456c52c7b4f9d267fb3dd7631347c5aa75b79534
candirufish <38038147+candirufish@users.noreply.github.com>
2022-05-19 05:01:04 +0200

    mpc stat = 0

--- src/search.cpp
+++ src/search.cpp
@@ -1192,6 +1192,9 @@ moves_loop: // When in check, search starts here
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           r -= ss->statScore / 15914;
 
+         if (moveCountPruning)
+                  ss->statScore = 0;
+
           // In general we want to cap the LMR depth search at newDepth. But if reductions
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions).
