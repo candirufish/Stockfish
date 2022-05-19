006cb1183f696cb9b4ecd740e2f35693bc59ed31
candirufish <38038147+candirufish@users.noreply.github.com>
2022-05-19 03:41:00 +0200

    ss ply <=4 pv lext 2

--- src/search.cpp
+++ src/search.cpp
@@ -1197,7 +1197,7 @@ moves_loop: // When in check, search starts here
           // deeper than the first move (this may lead to hidden double extensions).
           int deeper =   r >= -1                   ? 0
                        : moveCount <= 4            ? 2
-                       : ss->ply <= 4 && PvNode    ? 1
+                       : ss->ply <= 4 && PvNode    ? 2
                        : PvNode && depth > 4       ? 1
                        : cutNode && moveCount <= 8 ? 1
                        :                             0;
