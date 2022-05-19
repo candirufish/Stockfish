de2c9fec0b06fef378485415d2bd17cf39feda39
candirufish <38038147+candirufish@users.noreply.github.com>
2022-05-19 02:58:22 +0200

    ninc stat lmr

--- src/search.cpp
+++ src/search.cpp
@@ -1180,9 +1180,11 @@ moves_loop: // When in check, search starts here
               r -= 1 + 15 / ( 3 + depth );
 
           // Increase reduction if next ply has a lot of fail high else reset count to 0
-          if ((ss+1)->cutoffCnt > 3 && !PvNode && !ss->inCheck)
+          if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
+          if (!ss->inCheck)
+          {
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
                          + (*contHist[1])[movedPiece][to_sq(move)]
@@ -1191,7 +1193,7 @@ moves_loop: // When in check, search starts here
 
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           r -= ss->statScore / 15914;
-
+          }
           // In general we want to cap the LMR depth search at newDepth. But if reductions
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions).
