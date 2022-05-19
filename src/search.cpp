fa90196620be3b13a42f4ad425f5618da49daeb5
candirufish <38038147+candirufish@users.noreply.github.com>
2022-05-19 23:30:25 +0200

    nttpv nk0 full razor

--- src/search.cpp
+++ src/search.cpp
@@ -783,7 +783,7 @@ namespace {
     if (   !PvNode
         && eval < alpha - 348 - 258 * depth * depth)
     {
-        if (!ss->ttPv && ttMove != MOVE_NONE)
+        if (!ss->ttPv && move != ss->killers[0])
         {
         value = search<NonPV>(pos, ss, alpha - 1, alpha, depth - 7, cutNode);
         if (value < alpha)
