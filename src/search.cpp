e7efc4166aeda9d257e16cb5409e96126690f5ad
candirufish <38038147+candirufish@users.noreply.github.com>
2022-05-20 02:56:14 +0200

    nttpv d-5 full raz

--- src/search.cpp
+++ src/search.cpp
@@ -785,7 +785,7 @@ namespace {
     {
         if (!ss->ttPv)
         {
-        value = search<NonPV>(pos, ss, alpha - 1, alpha, depth - 3, cutNode);
+        value = search<NonPV>(pos, ss, alpha - 1, alpha, depth - 5, cutNode);
         if (value < alpha)
             return value;
         }
