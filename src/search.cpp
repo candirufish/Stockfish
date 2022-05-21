60755ae819b850e058b6c0f1440d85e2d6131548
candirufish <38038147+candirufish@users.noreply.github.com>
2022-05-22 01:32:12 +0200

    gcExt ssply & 1 && ss-1 mvc > 1 lmrext 0

--- src/search.cpp
+++ src/search.cpp
@@ -1197,9 +1197,9 @@ moves_loop: // When in check, search starts here
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions).
           int deeper =   r >= -1                   ? 0
-                       : gcExt                     ? 0
+                       : gcExt && ((ss->ply & 1) && (ss-1)->moveCount > 1)                   ? 0
                        : moveCount <= 4            ? 2
-                       : PvNode && depth > 4       ? 1
+                       : PvNode                    ? 1
                        : cutNode && moveCount <= 8 ? 1
                        :                             0;
 
