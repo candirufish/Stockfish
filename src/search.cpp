0c58de5c5e9d95ff267d4404dd9989718590d92b
candirufish <38038147+candirufish@users.noreply.github.com>
2022-05-21 11:49:48 +0200

    research lmr r++

--- src/search.cpp
+++ src/search.cpp
@@ -1183,6 +1183,9 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
+          if ((ss-1)->research)
+              r++;
+
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
                          + (*contHist[1])[movedPiece][to_sq(move)]
@@ -1219,7 +1222,9 @@ moves_loop: // When in check, search starts here
       // Step 18. Full depth search when LMR is skipped or fails high
       if (doFullDepthSearch)
       {
+          ss->research = didLMR;
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth + doDeeperSearch, !cutNode);
+          ss->research = false;
 
           // If the move passed LMR update its stats
           if (didLMR)
