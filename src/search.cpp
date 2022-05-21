388f03fa8a9f817959dee1b1af3f15f1dae4e72e
candirufish <38038147+candirufish@users.noreply.github.com>
2022-05-22 01:35:15 +0200

    gcext ssp 1 ss-1-3-5 mvc > 1 lmrext 0

--- src/search.cpp
+++ src/search.cpp
@@ -1193,11 +1193,16 @@ moves_loop: // When in check, search starts here
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           r -= ss->statScore / 15914;
 
+          bool notImportant =    (ss->ply & 1)
+                              && (   (ss-1)->moveCount > 1
+                                  || (ss-3)->moveCount > 1
+                                  || (ss-5)->moveCount > 1);
+
           // In general we want to cap the LMR depth search at newDepth. But if reductions
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions).
           int deeper =   r >= -1                   ? 0
-                       : gcExt && ((ss->ply & 1) && (ss-1)->moveCount > 1)                   ? 0
+                       : gcExt && notImportant     ? 0
                        : moveCount <= 4            ? 2
                        : PvNode                    ? 1
                        : cutNode && moveCount <= 8 ? 1
