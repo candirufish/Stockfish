b233bb0bfd0654a1755aadf1e7e603da14fef37b
candirufish <38038147+candirufish@users.noreply.github.com>
2022-05-21 18:16:31 +0200

    no negext on 8th th

--- src/search.cpp
+++ src/search.cpp
@@ -1098,13 +1098,16 @@ moves_loop: // When in check, search starts here
               else if (singularBeta >= beta)
                   return singularBeta;
 
+              else if ((thisThread->id() & 7) != 7)
+             {
               // If the eval of ttMove is greater than beta, we reduce it (negative extension)
-              else if (ttValue >= beta)
+              if (ttValue >= beta)
                   extension = -2;
 
               // If the eval of ttMove is less than alpha and value, we reduce it (negative extension)
               else if (ttValue <= alpha && ttValue <= value)
                   extension = -1;
+              }
           }
 
           // Check extensions (~1 Elo)
