b2bd47920442a80ae7527857ebc97177f1ff92f2
candirufish <38038147+candirufish@users.noreply.github.com>
2022-05-22 08:27:38 +0200

    mp insert -4250

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -239,7 +239,7 @@ top:
           endMoves = generate<QUIETS>(pos, cur);
 
           score<QUIETS>();
-          partial_insertion_sort(cur, endMoves, -3750 * depth);
+          partial_insertion_sort(cur, endMoves, -4250 * depth);
       }
 
       ++stage;
