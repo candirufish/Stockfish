fb8e7eb43976091437d6f237ff99fe42fdd9ca38
candirufish <38038147+candirufish@users.noreply.github.com>
2022-05-25 01:01:52 +0200

    dbl ext lmrext 0

--- src/search.cpp
+++ src/search.cpp
@@ -953,6 +953,7 @@ moves_loop: // When in check, search starts here
 
     value = bestValue;
     moveCountPruning = false;
+    bool dblExt = false;
 
     // Indicate PvNodes that will probably fail low if the node was searched
     // at a depth equal or greater than the current depth, and the result of this search was a fail low.
@@ -1087,7 +1088,7 @@ moves_loop: // When in check, search starts here
                   if (  !PvNode
                       && value < singularBeta - 26
                       && ss->doubleExtensions <= 8)
-                      extension = 2;
+                      extension = 2, dblExt = true;
               }
 
               // Multi-cut pruning
@@ -1196,6 +1197,7 @@ moves_loop: // When in check, search starts here
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions).
           int deeper =   r >= -1                   ? 0
+                       : dblExt                    ? 0
                        : moveCount <= 4            ? 2
                        : PvNode                    ? 1
                        : cutNode && moveCount <= 8 ? 1
