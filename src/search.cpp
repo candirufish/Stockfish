e8cd018a0bb938c01fc8ad3d85c2792b4358fb3f
candirufish <38038147+candirufish@users.noreply.github.com>
2022-05-26 03:14:13 +0200

    ss2 lmrext deeper r-2

--- src/search.cpp
+++ src/search.cpp
@@ -1184,7 +1184,7 @@ moves_loop: // When in check, search starts here
               r++;
 
           if (move == (ss-2)->didLmrExt)
-              r--;
+              r -= 2;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
