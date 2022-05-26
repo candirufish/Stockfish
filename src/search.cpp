a8e142963e0b4c43c70547781b43a26b007025bf
candirufish <38038147+candirufish@users.noreply.github.com>
2022-05-26 15:57:45 +0200

    nttpv v >= b frz

--- src/search.cpp
+++ src/search.cpp
@@ -786,7 +786,7 @@ namespace {
         if (!ss->ttPv)
         {
         value = search<NonPV>(pos, ss, alpha - 1, alpha, depth - 7, cutNode);
-        if (value < alpha)
+        if (value >= beta)
             return value;
         }
        else if (depth <= 7)
@@ -1205,7 +1205,7 @@ moves_loop: // When in check, search starts here
           // deeper than the first move (this may lead to hidden double extensions).
           int deeper =   r >= -1                   ? 0
                        : moveCount <= 4            ? 2
-                       : PvNode && depth > 4       ? 1
+                       : PvNode                    ? 1
                        : cutNode && moveCount <= 8 ? 1
                        :                             0;
 
