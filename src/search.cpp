303c8142d3f7f32d0bcffec010f1346a17659f4b
candirufish <38038147+candirufish@users.noreply.github.com>
2022-05-26 02:30:23 +0200

    cext chkcnt < 4

--- src/search.cpp
+++ src/search.cpp
@@ -558,7 +558,7 @@ namespace {
     bool givesCheck, improving, didLMR, priorCapture;
     bool capture, doFullDepthSearch, moveCountPruning, ttCapture;
     Piece movedPiece;
-    int moveCount, captureCount, quietCount, improvement, complexity;
+    int moveCount, captureCount, quietCount, improvement, complexity, checkCount;
 
     // Step 1. Initialize node
     Thread* thisThread = pos.this_thread();
@@ -569,6 +569,7 @@ namespace {
     moveCount          = captureCount = quietCount = ss->moveCount = 0;
     bestValue          = -VALUE_INFINITE;
     maxValue           = VALUE_INFINITE;
+    checkCount         = 0;
 
     // Check for the available remaining time
     if (thisThread == Threads.main())
@@ -995,6 +996,8 @@ moves_loop: // When in check, search starts here
       capture = pos.capture(move);
       movedPiece = pos.moved_piece(move);
       givesCheck = pos.gives_check(move);
+      if (givesCheck) 
+          checkCount++;
 
       // Calculate new depth for this move
       newDepth = depth - 1;
@@ -1110,6 +1113,7 @@ moves_loop: // When in check, search starts here
           // Check extensions (~1 Elo)
           else if (   givesCheck
                    && depth > 9
+                   && checkCount < 4
                    && abs(ss->staticEval) > 71)
               extension = 1;
 
