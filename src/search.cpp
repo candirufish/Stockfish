bb85277e3a11c9ec279946ecdc4f12c29f50fda8
candirufish <38038147+candirufish@users.noreply.github.com>
2022-05-26 03:04:21 +0200

    ss1 lmrext deeper npv

--- src/search.cpp
+++ src/search.cpp
@@ -1183,7 +1183,7 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
-          if ((ss-1)->didLmrExt)
+          if ((ss-1)->didLmrExt && !PvNode)
               r++;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
