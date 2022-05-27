6a7adf9d3a549a3c22b9142eba1b4030a439bddb
candirufish <38038147+candirufish@users.noreply.github.com>
2022-05-27 17:27:51 +0200

    se > vk npv r++

--- src/search.cpp
+++ src/search.cpp
@@ -1192,7 +1192,7 @@ moves_loop: // When in check, search starts here
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           r -= ss->statScore / 15914;
 
-        if (abs(ss->staticEval) > VALUE_KNOWN_WIN)
+        if (abs(ss->staticEval) > VALUE_KNOWN_WIN && !PvNode)
           r++;
 
           // In general we want to cap the LMR depth search at newDepth. But if reductions
