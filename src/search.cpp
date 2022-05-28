b8940c2abd1fceeb99f3a32c8287cc1202751eb0
candirufish <38038147+candirufish@users.noreply.github.com>
2022-05-28 04:26:39 +0200

    cutoffcnt and stat reset

--- src/search.cpp
+++ src/search.cpp
@@ -1324,8 +1324,10 @@ moves_loop: // When in check, search starts here
           }
       }
       else
+      {
+         ss->statScore = 0;
          ss->cutoffCnt = 0;
-
+      }
 
       // If the move is worse than some previously searched move, remember it to update its stats later
       if (move != bestMove)
