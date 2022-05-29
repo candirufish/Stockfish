df2643049d4afb08233272e310a0ec1fcdd41233
candirufish <38038147+candirufish@users.noreply.github.com>
2022-05-29 10:31:58 +0200

    lmrext 2 8th thread

--- src/search.cpp
+++ src/search.cpp
@@ -1195,11 +1195,12 @@ moves_loop: // When in check, search starts here
           // In general we want to cap the LMR depth search at newDepth. But if reductions
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions).
-          int deeper =   r >= -1                   ? 0
-                       : moveCount <= 4            ? 2
-                       : PvNode                    ? 1
-                       : cutNode && moveCount <= 8 ? 1
-                       :                             0;
+          int deeper =   r >= -1                    ? 0
+                       : (thisThread->id() & 7) == 7 ? 2
+                       : moveCount <= 4             ? 2
+                       : PvNode                     ? 1
+                       : cutNode && moveCount <= 8  ? 1
+                       :                              0;
 
           Depth d = std::clamp(newDepth - r, 1, newDepth + deeper);
 
