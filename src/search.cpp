c0fecb741637e07e6cefdf665f1d0ec4905200af
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-03 21:05:38 +0200

    ttv >= b + 25 npv -2

--- src/search.cpp
+++ src/search.cpp
@@ -1097,7 +1097,12 @@ moves_loop: // When in check, search starts here
 
               // If the eval of ttMove is greater than beta, we reduce it (negative extension)
               else if (ttValue >= beta)
-                  extension = -2;
+              {
+                  extension = -1;
+                  if (  !PvNode
+                     && ttValue >= beta + 25)
+                     extension = -2;
+              }
 
               // If the eval of ttMove is less than alpha and value, we reduce it (negative extension)
               else if (ttValue <= alpha && ttValue <= value)
