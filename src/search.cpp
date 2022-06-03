d9d5214fca3acea18c97336bc3b1548461e1e7ea
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-04 01:07:39 +0200

    frz nttpv d-7 true

--- src/search.cpp
+++ src/search.cpp
@@ -781,12 +781,20 @@ namespace {
     // If eval is really low check with qsearch if it can exceed alpha, if it can't,
     // return a fail low.
     if (   !PvNode
-        && depth <= 7
         && eval < alpha - 348 - 258 * depth * depth)
     {
+        if (!ss->ttPv)
+        {
+        value = search<NonPV>(pos, ss, alpha - 1, alpha, depth - 7, true);
+        if (value < alpha)
+            return value;
+        }
+       else if (depth <= 7)
+        {
         value = qsearch<NonPV>(pos, ss, alpha - 1, alpha);
         if (value < alpha)
             return value;
+        }
     }
 
     // Step 8. Futility pruning: child node (~25 Elo).
