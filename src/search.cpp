fbc7df3e77026624bec2af3b0eb62ea9db344ad6
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-05 00:04:11 +0200

    flb  v < ap - 150 cnt 0

--- src/search.cpp
+++ src/search.cpp
@@ -1364,7 +1364,7 @@ moves_loop: // When in check, search starts here
                           || cutNode
                           || bestValue < alpha - 70 * depth;
 
-        if (bestValue < alpha - 70 * depth)
+        if (value < alpha - 150)
             ss->cutoffCnt = 0;
 
         update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, stat_bonus(depth) * (1 + extraBonus));
