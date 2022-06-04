b7102bda4ddd9029e45105d401c39f4aa13bc437
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-04 23:39:33 +0200

    v < ap - 25 cnt 0

--- src/search.cpp
+++ src/search.cpp
@@ -787,7 +787,7 @@ namespace {
         value = qsearch<NonPV>(pos, ss, alpha - 1, alpha);
         if (value < alpha)
         {
-            if (value < alpha - 10)
+            if (value < alpha - 25)
                  ss->cutoffCnt = 0;
 
             return value;
