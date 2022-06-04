05f9285a8eb31206fbedaf4434310c85b2166449
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-04 23:58:23 +0200

    fl bonux extra cnt 0

--- src/search.cpp
+++ src/search.cpp
@@ -1364,7 +1364,9 @@ moves_loop: // When in check, search starts here
                           || cutNode
                           || bestValue < alpha - 70 * depth;
 
-        ss->cutoffCnt = 0;
+        if (extraBonus)
+            ss->cutoffCnt = 0;
+
         update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, stat_bonus(depth) * (1 + extraBonus));
     }
 
