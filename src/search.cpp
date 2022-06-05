d9656836764922297469c2b8f4fa57fa815743b4
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-05 13:26:49 +0200

    margin ap -64

--- src/search.cpp
+++ src/search.cpp
@@ -1196,7 +1196,7 @@ moves_loop: // When in check, search starts here
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
-          if ((value > alpha + 128) && d + 6 < newDepth)
+          if ((value > alpha - 64) && d + 6 < newDepth)
               value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, (d + newDepth)/2, true);
 
           // If the son is reduced and fails high it will be re-searched at full depth
