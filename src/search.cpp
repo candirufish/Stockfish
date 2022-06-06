239913c6289ba2b9352157eb7960f4a380fcdb45
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-06 02:05:59 +0200

    apsearch each 2nd th

--- src/search.cpp
+++ src/search.cpp
@@ -1196,7 +1196,7 @@ moves_loop: // When in check, search starts here
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
-          if (value > alpha && d + 6 < newDepth && (thisThread->id() & 7) == 7)
+          if (value > alpha && d + 6 < newDepth && !(thisThread->id() & 1))
               value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, (d + newDepth)/2, true);
 
           // If the son is reduced and fails high it will be re-searched at full depth
