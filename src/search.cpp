0ae788dc5bf493f204a513ff67e4f8824722b7e1
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-08 08:11:03 +0200

    if pv nttm d >= 3 d-2

--- src/search.cpp
+++ src/search.cpp
@@ -915,7 +915,13 @@ namespace {
         depth -= 3;
 
     if (depth <= 0)
+
         return qsearch<PV>(pos, ss, alpha, beta);
+
+    else if (PvNode
+         && !ttMove
+         && depth >= 3)
+        depth -= 2;
 
     if (    cutNode
         &&  depth >= 8
