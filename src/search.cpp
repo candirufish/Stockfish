0ede87bb370c5c450d41832172b92a22a125d2c1
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-10 01:29:42 +0200

    v > ap d+6 nd nlfl

--- src/search.cpp
+++ src/search.cpp
@@ -1199,7 +1199,7 @@ moves_loop: // When in check, search starts here
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
-          if (value > alpha && d + 6 < newDepth && !ttMove)
+          if (value > alpha && d + 6 < newDepth && !likelyFailLow)
               value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, (d + newDepth)/2, true);
 
           // If the son is reduced and fails high it will be re-searched at full depth
