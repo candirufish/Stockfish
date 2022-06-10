77b468f496469ebb4cce9796ad0c23d249a240c9
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-10 07:42:01 +0200

    -1 cnlmr k1 ttpv and quiet ss1 ns

--- src/search.cpp
+++ src/search.cpp
@@ -1164,7 +1164,8 @@ moves_loop: // When in check, search starts here
 
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode && move != ss->killers[0])
-              r += 2;
+             r += (move == ss->killers[1] && ss->ttPv 
+              && (ss-1)->statScore < 0 && !capture) ? -1 : 2;
 
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
