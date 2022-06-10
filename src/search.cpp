6b0529366998cf891deabe7a987689ef01b0ecd8
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-11 00:50:00 +0200

    cnt > 3 npvttm r++

--- src/search.cpp
+++ src/search.cpp
@@ -1175,8 +1175,8 @@ moves_loop: // When in check, search starts here
               r -= 1 + 15 / ( 3 + depth );
 
           // Increase reduction if next ply has a lot of fail high else reset count to 0
-          if ((ss+1)->cutoffCnt > 3 && !PvNode)
-              r += ttMove ? 1 : 2;
+          if ((ss+1)->cutoffCnt > 3 && !(PvNode && ttMove))
+              r++;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
