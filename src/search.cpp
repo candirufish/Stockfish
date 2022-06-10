39dda90779285c24c38af240071256fe9fb4fe26
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-11 01:37:39 +0200

    ssp 1 ss-135 mv > 1 -1 lmrext 1st

--- src/search.cpp
+++ src/search.cpp
@@ -1196,9 +1196,9 @@ moves_loop: // When in check, search starts here
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions).
           int deeper =   r >= -1                   ? 0
+                       : nglext                    ? -1
                        : moveCount <= 4            ? 2
                        : PvNode || cutNode         ? 1
-                       : nglext                    ? -1
                        :                             0;
 
           Depth d = std::clamp(newDepth - r, 1, newDepth + deeper);
