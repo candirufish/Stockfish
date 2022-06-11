e212cb9f0756ce5710576658101eb5fb5ae968ed
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-12 00:08:49 +0200

    ttb upper fh lmr r+2

--- src/search.cpp
+++ src/search.cpp
@@ -1175,8 +1175,8 @@ moves_loop: // When in check, search starts here
               r -= 1 + 15 / ( 3 + depth );
 
           // Increase reduction if next ply has a lot of fail high else reset count to 0
-          if ((ss+1)->cutoffCnt > 3 && !PvNode)
-              r++;
+          if ((ss+1)->cutoffCnt > 3 && !PvNode )
+              r += (tte->bound() & BOUND_UPPER) && tte->depth() >= depth ? 2 : 1;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
