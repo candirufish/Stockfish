fc282a2ae2354fa84069d56cc6f53edcd6103180
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-12 11:26:06 +0200

    v < ap - 32 * d

--- src/search.cpp
+++ src/search.cpp
@@ -1106,7 +1106,7 @@ moves_loop: // When in check, search starts here
               else if (ttValue <= alpha && ttValue <= value)
                   extension = -1;
 
-              else if (value < alpha - 16 * depth)
+              else if (value < alpha - 32 * depth)
                   extension = -1;
           }
 
