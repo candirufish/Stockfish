92c50cf0e5ae0dcc23883c67b8d6a8572fcd2484
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-12 04:05:28 +0200

    v < ap - 32 cnt <= 3 -1 ext

--- src/search.cpp
+++ src/search.cpp
@@ -1106,7 +1106,7 @@ moves_loop: // When in check, search starts here
               else if (ttValue <= alpha && ttValue <= value)
                   extension = -1;
 
-              else if (value < alpha && (ss+1)->cutoffCnt <= 3)
+              else if (value < alpha - 32 && (ss+1)->cutoffCnt <= 3)
                   extension = -1;
           }
 
