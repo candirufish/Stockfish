54ed9a9dcd332e120b6edfe410dfd783bec4ec85
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-13 20:06:46 +0200

    inc king pv r--

--- src/search.cpp
+++ src/search.cpp
@@ -1178,7 +1178,7 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
-          if (ss->inCheck && type_of(movedPiece) == KING)
+          if (ss->inCheck && type_of(movedPiece) == KING && PvNode)
               r--;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
