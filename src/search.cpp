23e3d8c88cce3cb1329879146bebed98b1bba90c
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-14 03:21:01 +0200

    ttgc ngcext npv  r++

--- src/search.cpp
+++ src/search.cpp
@@ -1182,7 +1182,7 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
-          if (ttGivesCheck && !flExt)
+          if (ttGivesCheck && !flExt && !PvNode)
               r++;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
