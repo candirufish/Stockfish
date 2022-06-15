a57e915394a7b812f20a7e0d707afb58b999c6aa
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-15 06:12:27 +0200

    cp ml == 1 ext 1

--- src/search.cpp
+++ src/search.cpp
@@ -1119,6 +1119,10 @@ moves_loop: // When in check, search starts here
                    && move == ss->killers[0]
                    && (*contHist[0])[movedPiece][to_sq(move)] >= 5491)
               extension = 1;
+
+          else if (capture
+                   && MoveList<LEGAL>(pos).size() == 1)
+                extension = 1;
       }
 
       // Add extension to new depth
