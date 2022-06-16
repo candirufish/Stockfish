0f0c67e50ef06952d779d75b3d729a3be6eb3684
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-16 20:29:55 +0200

    pv cpx ss -512 ss1 r--

--- src/search.cpp
+++ src/search.cpp
@@ -1173,7 +1173,7 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
-          if ((ss->cplxtrack - 512) > (ss-1)->cplxtrack)
+          if (PvNode && (ss->cplxtrack - 512) > (ss-1)->cplxtrack)
               r--;
 
 
