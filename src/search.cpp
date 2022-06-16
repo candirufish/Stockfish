ba8732d996c731e877ff7aeb9e7d17b165fa96ab
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-16 23:32:12 +0200

    sebv > 256 ss2 ss pv r--

--- src/search.cpp
+++ src/search.cpp
@@ -1173,7 +1173,7 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
-          if (ss->volatileEv && (ss-2)->volatileEv)
+          if (ss->volatileEv && (ss-2)->volatileEv && PvNode)
               r--;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
