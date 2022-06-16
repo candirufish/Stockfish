3a6d82516ebdfceadd12e11809df6b0f66232c9d
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-16 19:16:23 +0200

    mv lmrext ngcext 1 + ext

--- src/search.cpp
+++ src/search.cpp
@@ -557,7 +557,7 @@ namespace {
     Depth extension, newDepth;
     Value bestValue, value, ttValue, eval, maxValue, probCutBeta;
     bool givesCheck, improving, didLMR, priorCapture;
-    bool capture, doFullDepthSearch, moveCountPruning, ttCapture;
+    bool capture, doFullDepthSearch, moveCountPruning, ttCapture, flExt;
     Piece movedPiece;
     int moveCount, captureCount, quietCount, improvement, complexity;
 
@@ -989,6 +989,7 @@ moves_loop: // When in check, search starts here
       capture = pos.capture(move);
       movedPiece = pos.moved_piece(move);
       givesCheck = pos.gives_check(move);
+      flExt = false;
 
       // Calculate new depth for this move
       newDepth = depth - 1;
@@ -1105,7 +1106,7 @@ moves_loop: // When in check, search starts here
           else if (   givesCheck
                    && depth > 9
                    && abs(ss->staticEval) > 71)
-              extension = 1;
+              extension = 1, flExt = true;
 
           // Quiet ttMove extensions (~0 Elo)
           else if (   PvNode
@@ -1185,7 +1186,7 @@ moves_loop: // When in check, search starts here
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions).
           int deeper =   r >= -1                   ? 0
-                       : moveCount <= 4            ? 1 + extension
+                       : moveCount <= 4 && !flExt  ? 1 + extension
                        : PvNode || cutNode         ? 1
                        :                             0;
 
