605366877d870ebaefba07d43570dd7630d372ad
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-18 00:57:40 +0200

    ttgc pv or cn r++

--- src/search.cpp
+++ src/search.cpp
@@ -1171,7 +1171,7 @@ moves_loop: // When in check, search starts here
           if (PvNode)
               r -= 1 + 15 / (3 + depth);
 
-          if (PvNode && ttGivesCheck)
+          if ((PvNode || cutNode) && ttGivesCheck)
               r++;
 
           // Increase reduction if next ply has a lot of fail high else reset count to 0
