0b3ae243da2ec33b88bd2eb50de03878362789a8
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-18 00:04:17 +0200

    ttcap 1 + ttgc

--- src/search.cpp
+++ src/search.cpp
@@ -1165,14 +1165,11 @@ moves_loop: // When in check, search starts here
 
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
-              r++;
+              r += 1 + ttGivesCheck;
 
           // Decrease reduction for PvNodes based on depth
           if (PvNode)
               r -= 1 + 15 / (3 + depth);
-
-          if (PvNode && ttGivesCheck)
-              r--;
 
           // Increase reduction if next ply has a lot of fail high else reset count to 0
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
