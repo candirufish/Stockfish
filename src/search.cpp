0a6ee54782e169c106a931e650d70cc44dfd8759
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-18 11:31:20 +0200

    cn nttm d-1 + ttpv

--- src/search.cpp
+++ src/search.cpp
@@ -914,7 +914,7 @@ namespace {
     if (    cutNode
         &&  depth >= 8
         && !ttMove)
-        depth -= 1 + !ss->ttPv;
+        depth -= 1 + ss->ttPv;
 
 moves_loop: // When in check, search starts here
 
