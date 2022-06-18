69894a2792c09cf86fa7aee33d243705c7b8f64a
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-18 13:36:24 +0200

    s11 ttpv with qs

--- src/search.cpp
+++ src/search.cpp
@@ -908,20 +908,19 @@ namespace {
         && !ttMove)
         depth -= 3;
 
+    if (ss->ttPv
+        &&  !cutNode
+        &&  !PvNode
+        &&  !ttMove)
+        depth--;
+
     if (depth <= 0)
-        return qsearch<PV>(pos, ss, alpha, beta);
+        return qsearch<PvNode ? PV : NonPV>(pos, ss, alpha, beta);
 
     if (    cutNode
         &&  depth >= 8
         && !ttMove)
         depth--;
-
-    if (ss->ttPv
-        &&  !cutNode
-        &&  !PvNode
-        &&  !ttMove
-        && depth >= 8)
-        depth -= bool(complexity > 512);
 
 moves_loop: // When in check, search starts here
 
