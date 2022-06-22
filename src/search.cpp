3ebb0a7750ffc4f9bf0f38ca3eae6d555f1ea75c
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-22 07:27:47 +0200

    pv nttm prcap cp r-2

--- src/search.cpp
+++ src/search.cpp
@@ -1172,7 +1172,7 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
-          if (PvNode && !ttMove && priorCapture)
+          if (PvNode && !ttMove && priorCapture && capture)
               r += 2;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
