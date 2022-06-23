4502854e91689bf4a99e884672bc6be891be9a40
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-24 00:55:00 +0200

    s11 pv n ssp & 1

--- src/search.cpp
+++ src/search.cpp
@@ -902,7 +902,8 @@ namespace {
     // Step 11. If the position is not in TT, decrease depth by 3.
     // Use qsearch if depth is equal or below zero (~4 Elo)
     if (    PvNode
-        && !ttMove)
+        && !ttMove
+        && !(ss->ply & 1))
         depth -= 3;
 
     if (depth <= 0)
