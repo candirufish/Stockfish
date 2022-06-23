86c7756f54c651c400691aa3b850fc3196777d6e
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-24 00:40:18 +0200

    pv se-bv cpx > 512 r--

--- src/search.cpp
+++ src/search.cpp
@@ -1165,6 +1165,9 @@ moves_loop: // When in check, search starts here
           if (PvNode)
               r -= 1 + 15 / (3 + depth);
 
+          if (PvNode && !ss->inCheck && abs(ss->staticEval - bestValue) > 250 && complexity > 512)
+              r--;
+
           // Increase reduction if next ply has a lot of fail high else reset count to 0
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
