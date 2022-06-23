ab77ad3c24f563a1f637b6cef002bf98273c06f5
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-23 12:45:40 +0200

    pv cpx ss-512 > ss1 r 1 + cpx / 625

--- src/search.cpp
+++ src/search.cpp
@@ -1171,7 +1171,7 @@ moves_loop: // When in check, search starts here
               r++;
 
           if (PvNode && (ss->cplxtrack - 512) > (ss-1)->cplxtrack)
-              r -= complexity / 625;
+              r -= 1 + complexity / 625;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
