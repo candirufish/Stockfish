1d189cac8b9c360970725251b84758073665c221
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-26 10:32:29 +0200

    s11 d - 3+ss0123 * ntthit

--- src/search.cpp
+++ src/search.cpp
@@ -904,9 +904,7 @@ namespace {
     if (    PvNode
         && !ttMove)
         depth -= 3   + ((ss->complexity / 625) - ((ss-1)->complexity / 625)
-                     + ((ss-2)->complexity / 625) - ((ss-3)->complexity / 625)
-                     + ((ss-4)->complexity / 625) - ((ss-5)->complexity / 625)
-                     + ((ss-6)->complexity / 625) - ((ss-7)->complexity / 625)) * !ss->ttHit;
+                     + ((ss-2)->complexity / 625) - ((ss-3)->complexity / 625)) * !ss->ttHit;
 
     if (depth <= 0)
         return qsearch<PV>(pos, ss, alpha, beta);
