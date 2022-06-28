657744918d6b2fb16753d10dc9c321da0c25e057
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-28 02:34:36 +0200

    v < ap cpx < 64 ngext-1

--- src/search.cpp
+++ src/search.cpp
@@ -1096,6 +1096,9 @@ moves_loop: // When in check, search starts here
               // If the eval of ttMove is less than alpha and value, we reduce it (negative extension)
               else if (ttValue <= alpha && ttValue <= value)
                   extension = -1;
+
+              else if (value < alpha && !ss->inCheck && complexity < 64)
+                  extension = -1;
           }
 
           // Check extensions (~1 Elo)
