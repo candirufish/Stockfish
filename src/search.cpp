0d46b5bb1b9f41459ed38e905922e7cf48da71dd
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-29 00:07:52 +0200

    s11 beta d-3 qs

--- src/search.cpp
+++ src/search.cpp
@@ -907,22 +907,22 @@ namespace {
         && !ttMove)
         depth -= 3;
 
-    if (depth <= 0)
-        return qsearch<PV>(pos, ss, alpha, beta);
-
-    if (    cutNode
-        &&  depth >= 8
-        && !ttMove)
-        depth--;
-
     betaMargin = beta + 1111 - 211 * improving;
 
     if (!PvNode && !ss->ttHit && depth >= 3 && !excludedMove && ss->staticEval >= betaMargin)
     {
         value = search<NonPV>(pos, ss, betaMargin, betaMargin + 1, depth - 2, cutNode);
         if (value > betaMargin)
-            depth -= 2;
+           depth -= 3;
     }
+
+    if (depth <= 0)
+        return qsearch<PvNode ? PV : NonPV>(pos, ss, alpha, beta);
+
+    if (    cutNode
+        &&  depth >= 8
+        && !ttMove)
+        depth--;
 
 moves_loop: // When in check, search starts here
 
