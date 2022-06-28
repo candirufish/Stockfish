e342914db471d0de7ab2fc01494af9ebd9722a98
candirufish <38038147+candirufish@users.noreply.github.com>
2022-06-28 02:41:17 +0200

    deeper pv cpx > 1000 mv <= 5

--- src/search.cpp
+++ src/search.cpp
@@ -1193,7 +1193,7 @@ moves_loop: // When in check, search starts here
       else
       {
           doFullDepthSearch = !PvNode || moveCount > 1;
-          doDeeperSearch = PvNode && complexity > 1000;
+          doDeeperSearch = PvNode && complexity > 1000 && moveCount <= 5;
           didLMR = false;
       }
 
