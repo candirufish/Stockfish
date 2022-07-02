58a6d0af7d4c39ecd36d401b58081d709c310f88
candirufish <38038147+candirufish@users.noreply.github.com>
2022-07-02 08:43:55 +0200

    ttgc cp dc r--

--- src/search.cpp
+++ src/search.cpp
@@ -1172,7 +1172,7 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
-          if (!ttGivesCheck && givesCheck && capture && more_than_one(pos.checkers()))
+          if (ttGivesCheck && capture && more_than_one(pos.checkers()))
               r--;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
