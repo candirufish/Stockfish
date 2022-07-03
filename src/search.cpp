a31097d28a96121ec3cd20b414af45126af6b779
candirufish <38038147+candirufish@users.noreply.github.com>
2022-07-03 18:34:33 +0200

    s20 d / 2

--- src/search.cpp
+++ src/search.cpp
@@ -1291,12 +1291,7 @@ moves_loop: // When in check, search starts here
                       && depth < 7
                       && beta  <  VALUE_KNOWN_WIN
                       && alpha > -VALUE_KNOWN_WIN)
-                          {
-                           if (improving)
-                                depth /= 2;
-                           else
-                                depth -= 1;
-                          }
+                          depth /= 2;
 
                   assert(depth > 0);
               }
