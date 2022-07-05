8162116fd4dfa8a71434dd500fb99c7611bbd97a
candirufish <38038147+candirufish@users.noreply.github.com>
2022-07-05 16:19:57 +0200

    reduce none t2

--- src/search.cpp
+++ src/search.cpp
@@ -1289,6 +1289,7 @@ moves_loop: // When in check, search starts here
                   // Reduce other moves if we have found at least one score improvement
                   if (   depth > 2
                       && depth < 7
+                      && thisThread->id() % 2 != 1
                       && beta  <  VALUE_KNOWN_WIN
                       && alpha > -VALUE_KNOWN_WIN)
                      depth -= 1;
