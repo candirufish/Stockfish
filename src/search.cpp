67c430205cc07d1b27d64decb971f15c84c074e0
candirufish <38038147+candirufish@users.noreply.github.com>
2022-07-07 03:01:21 +0200

    ninc beta ngext

--- src/search.cpp
+++ src/search.cpp
@@ -1090,7 +1090,7 @@ moves_loop: // When in check, search starts here
                   return singularBeta;
 
               // If the eval of ttMove is greater than beta, we reduce it (negative extension)
-              else if (ttValue >= beta)
+              else if (ttValue >= beta && !ss->inCheck)
                   extension = -2;
 
               // If the eval of ttMove is less than alpha and value, we reduce it (negative extension)
