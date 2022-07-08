f04fa64b05a701f6166a2cf89707234a37738067
candirufish <38038147+candirufish@users.noreply.github.com>
2022-07-08 05:43:36 +0200

    pvdist <= 4 cplx > 1000 r--

--- src/search.cpp
+++ src/search.cpp
@@ -1167,7 +1167,7 @@ moves_loop: // When in check, search starts here
           if (PvNode)
               r -= 1 + 15 / (3 + depth);
 
-          if ((ss+1)->distanceFromPv <= 4 && !ss->inCheck && abs(ss->staticEval - bestValue) > 250)
+          if ((ss+1)->distanceFromPv <= 4 && complexity > 1000)
               r--;
 
           // Increase reduction if next ply has a lot of fail high else reset count to 0
