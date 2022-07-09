cb0251443f20768cfee251f5dd75d8aa0436600f
candirufish <38038147+candirufish@users.noreply.github.com>
2022-07-09 09:28:46 +0200

    pv pvdist <= 4 r<-1 lmrext

--- src/search.cpp
+++ src/search.cpp
@@ -1180,10 +1180,12 @@ moves_loop: // When in check, search starts here
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           r -= ss->statScore / 15914;
 
+          bool deeper = PvNode && r < -1 && (ss+1)->distanceFromPv <= 4;
+
           // In general we want to cap the LMR depth search at newDepth, but when
           // reduction is negative, we allow this move a limited search extension
           // beyond the first move depth. This may lead to hidden double extensions.
-          Depth d = std::clamp(newDepth - r, 1, newDepth + bool((ss+1)->distanceFromPv <= 4));
+          Depth d = std::clamp(newDepth - r, 1, newDepth + deeper);
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
