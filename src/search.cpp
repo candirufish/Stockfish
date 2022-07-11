f76f3752dcde4dd2785458be8a85e614ed908fea
candirufish <38038147+candirufish@users.noreply.github.com>
2022-07-11 02:17:53 +0200

    cl ttpv cp k0 2 3

--- src/search.cpp
+++ src/search.cpp
@@ -1156,8 +1156,10 @@ moves_loop: // When in check, search starts here
               r--;
 
           // Increase reduction for cut nodes (~3 Elo)
-          if (cutNode && move != ss->killers[0])
-              r += 2;
+          if (cutNode)
+              r += std::clamp(!ss->ttPv - ss->ttPv 
+                + !capture - capture 
+                + (move != ss->killers[0]) - (move == ss->killers[0]), 2, 3);
 
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
