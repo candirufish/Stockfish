173ca7740732dbd0558f457619e759dbc5362057
candirufish <38038147+candirufish@users.noreply.github.com>
2022-07-16 05:05:11 +0200

    cl ttpv k0 - tthit 1 3

--- src/search.cpp
+++ src/search.cpp
@@ -1160,7 +1160,7 @@ moves_loop: // When in check, search starts here
               r += std::clamp(!ss->ttPv - ss->ttPv 
                 + !capture - capture 
                 + (move != ss->killers[0]) - (move == ss->killers[0])
-                - improving, 1, 3);
+                - ss->ttHit, 1, 3);
 
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
