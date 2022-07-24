8a3832a4b4cdf492a2786635221a6e9838597540
candirufish <38038147+candirufish@users.noreply.github.com>
2022-07-24 12:21:16 +0200

    cnlmr +1 ttpv

--- src/search.cpp
+++ src/search.cpp
@@ -1156,8 +1156,8 @@ moves_loop: // When in check, search starts here
               r--;
 
           // Increase reduction for cut nodes (~3 Elo)
-          if (cutNode && !ss->ttPv)
-              r += 2;
+          if (cutNode)
+              r += 2 - ss->ttPv;
 
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
