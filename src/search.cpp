dff9c078784764e66c914b803207b2dd0213bacf
candirufish <38038147+candirufish@users.noreply.github.com>
2022-07-24 12:18:29 +0200

    cnlmr +1 cap

--- src/search.cpp
+++ src/search.cpp
@@ -1156,8 +1156,8 @@ moves_loop: // When in check, search starts here
               r--;
 
           // Increase reduction for cut nodes (~3 Elo)
-          if (cutNode && !capture)
-              r += 2;
+          if (cutNode)
+              r += 2 - capture;
 
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
