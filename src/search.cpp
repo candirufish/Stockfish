ff4379210c9bf4e3eaeeaa7a7038a36ffa80654c
candirufish <38038147+candirufish@users.noreply.github.com>
2022-07-28 12:48:03 +0200

    nk0ttpv cl ttpv cp k0ttpv +1 1 cpx > 650 and d <= 6 2 3

--- src/search.cpp
+++ src/search.cpp
@@ -1156,10 +1156,9 @@ moves_loop: // When in check, search starts here
               r--;
 
           // Increase reduction for cut nodes (~3 Elo)
-          if (cutNode)
+          if (cutNode && !(move == ss->killers[0] && ss->ttPv))
               r += std::clamp(!ss->ttPv - ss->ttPv 
                 + !capture - capture 
-                - (move == ss->killers[0] && ss->ttPv)
                 + 1, 1, complexity > 650 && depth <= 6 ? 2 : 3);
 
           // Increase reduction if ttMove is a capture (~3 Elo)
