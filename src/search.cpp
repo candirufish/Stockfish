8e9d45284deb080164575990bdaa8294b84e06ae
candirufish <38038147+candirufish@users.noreply.github.com>
2022-10-15 14:25:27 +0200

    pv mcp r--

--- src/search.cpp
+++ src/search.cpp
@@ -1158,6 +1158,9 @@ moves_loop: // When in check, search starts here
           if (PvNode)
               r -= 1 + 11 / (3 + depth);
 
+          if (PvNode && moveCountPruning)
+              r--;
+
           // Decrease reduction if ttMove has been singularly extended (~1 Elo)
           if (singularQuietLMR)
               r--;
