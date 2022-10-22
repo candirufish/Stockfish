42ffa8e3e366639ba4f3068db943484623b6818f
candirufish <38038147+candirufish@users.noreply.github.com>
2022-10-22 14:23:14 +0200

    s20 pv ss ss1 d-2 mx 1

--- src/search.cpp
+++ src/search.cpp
@@ -1281,14 +1281,12 @@ moves_loop: // When in check, search starts here
                   alpha = value;
 
                   // Reduce other moves if we have found at least one score improvement
-                  if (   depth > 1
-                      && depth < 6
+                  if (   depth < 6
                       && beta  <  VALUE_KNOWN_WIN
                       && alpha > -VALUE_KNOWN_WIN)
-                     depth -= ss->capture && !(ss-1)->capture ? 2 : 1;
+                      depth -= ss->capture && (ss-1)->capture ? 2 : 1;
 
-                  if (depth <= 0)
-                      return qsearch<PV>(pos, ss, alpha, beta);
+                  depth = std::max(depth, 1);
 
                   assert(depth > 0);
               }
