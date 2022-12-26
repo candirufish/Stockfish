4cb283af3fce8056121d51b094ad8bade0dafd27
candirufish <38038147+candirufish@users.noreply.github.com>
2022-12-26 02:47:32 +0100

    qtt ext d < 12 only

--- src/search.cpp
+++ src/search.cpp
@@ -1100,10 +1100,7 @@ moves_loop: // When in check, search starts here
                    && move == ttMove
                    && move == ss->killers[0]
                    && (*contHist[0])[movedPiece][to_sq(move)] >= 5177)
-            {
-              extension = 1;
               depth += depth < 12;
-            }
       }
 
       // Add extension to new depth
