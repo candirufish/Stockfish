3f35fd6e4e6666bd04b558d5de7c2864e818be85
candirufish <38038147+candirufish@users.noreply.github.com>
2022-12-29 01:51:23 +0100

    1 + r50 > 3 cnlmr

--- src/search.cpp
+++ src/search.cpp
@@ -1134,7 +1134,7 @@ moves_loop: // When in check, search starts here
 
       // Increase reduction for cut nodes (~3 Elo)
       if (cutNode)
-          r += 1 + bool(pos.rule50_count() >= 18);
+          r += 1 + bool(pos.rule50_count() > 3);
 
       // Increase reduction if ttMove is a capture (~3 Elo)
       if (ttCapture)
