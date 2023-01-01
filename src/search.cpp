f55d64dbe01104afd8927a850c534571f45a1c11
candirufish <38038147+candirufish@users.noreply.github.com>
2023-01-01 02:44:35 +0100

    s11 pv d-3 + ss1 r >= 1

--- src/search.cpp
+++ src/search.cpp
@@ -891,7 +891,7 @@ namespace {
     // Use qsearch if depth is equal or below zero (~9 Elo)
     if (    PvNode
         && !ttMove)
-        depth -= 3;
+        depth -= 3 + bool((ss-1)->r >=-1);
 
     if (depth <= 0)
         return qsearch<PV>(pos, ss, alpha, beta);
@@ -1165,6 +1165,7 @@ moves_loop: // When in check, search starts here
 
       // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
       r -= ss->statScore / (13000 + 4152 * (depth > 7 && depth < 19));
+      ss->r = r;
 
       // Step 17. Late moves reduction / extension (LMR, ~117 Elo)
       // We use various heuristics for the sons of a node after the first son has
