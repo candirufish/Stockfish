54371590cda799bae00204de423f1b105ad02b2b
candirufish <38038147+candirufish@users.noreply.github.com>
2023-01-02 20:11:12 +0100

    s11 ttcap d >= 2 d-- no lmr

--- src/search.cpp
+++ src/search.cpp
@@ -1140,10 +1140,6 @@ moves_loop: // When in check, search starts here
       if (cutNode)
           r += 2;
 
-      // Increase reduction if ttMove is a capture (~3 Elo)
-      if (ttCapture)
-          r++;
-
       // Decrease reduction for PvNodes based on depth
       if (PvNode)
           r -= 1 + 11 / (3 + depth);
