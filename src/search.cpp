1c9294988edb235cfe8c224ffc084b142a9d3400
candirufish <38038147+candirufish@users.noreply.github.com>
2023-01-03 01:44:47 +0100

    ss r >=1 ss-1 r <= -1 r++

--- src/search.cpp
+++ src/search.cpp
@@ -1165,6 +1165,10 @@ moves_loop: // When in check, search starts here
 
       // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
       r -= ss->statScore / (12800 + 4410 * (depth > 7 && depth < 19));
+      ss->r = r;
+
+      if (ss->r >= 1 && (ss-1)->r <= -1)
+          r++;
 
       // Step 17. Late moves reduction / extension (LMR, ~117 Elo)
       // We use various heuristics for the sons of a node after the first son has
