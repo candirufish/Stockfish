9dddd9a515780106d785c9041f39b52cdebb2ab9
candirufish <38038147+candirufish@users.noreply.github.com>
2023-01-03 14:27:47 +0100

    cnlmr 2 3 nttm d >= 9

--- src/search.cpp
+++ src/search.cpp
@@ -1134,7 +1134,7 @@ moves_loop: // When in check, search starts here
 
       // Increase reduction for cut nodes (~3 Elo)
       if (cutNode)
-          r += 2;
+          r += depth >= 9 && !ttMove ? 3 : 2;
 
       // Increase reduction if ttMove is a capture (~3 Elo)
       if (ttCapture)
