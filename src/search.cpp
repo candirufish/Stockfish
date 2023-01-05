7d80d69acb2df042114fcaf9ffe3d0d3f5df1f54
candirufish <38038147+candirufish@users.noreply.github.com>
2023-01-05 16:48:13 +0100

    s11 pv d - 50r == 0

--- src/search.cpp
+++ src/search.cpp
@@ -894,7 +894,7 @@ namespace {
     // Use qsearch if depth is equal or below zero (~9 Elo)
     if (    PvNode
         && !ttMove)
-        depth -= 3 + bool(pos.rule50_count() > 21);
+        depth -= 3 - bool(pos.rule50_count() == 0);
 
     if (depth <= 0)
         return qsearch<PV>(pos, ss, alpha, beta);
