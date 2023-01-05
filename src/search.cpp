6ec191e89e50fcf0d3fbef84f1837d27db54393c
candirufish <38038147+candirufish@users.noreply.github.com>
2023-01-05 14:14:22 +0100

    s11 pv nss1 mcp

--- src/search.cpp
+++ src/search.cpp
@@ -893,6 +893,7 @@ namespace {
     // Step 11. If the position is not in TT, decrease depth by 3.
     // Use qsearch if depth is equal or below zero (~9 Elo)
     if (    PvNode
+        && !(ss-1)->mcp
         && !ttMove)
         depth -= 3;
 
@@ -990,7 +991,7 @@ moves_loop: // When in check, search starts here
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold (~8 Elo)
-          ss->mcp = moveCountPruning = !(PvNode && (ss-1)->mcp) && moveCount >= futility_move_count(improving, depth);
+          ss->mcp = moveCountPruning = moveCount >= futility_move_count(improving, depth);
 
           // Reduced depth of the next LMR search
           int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount, delta, thisThread->rootDelta), 0);
