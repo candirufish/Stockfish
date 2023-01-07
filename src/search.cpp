a3d4badb980c30ee1e35578318face357dfe7a9c
candirufish <38038147+candirufish@users.noreply.github.com>
2023-01-07 03:45:24 +0100

    pvxt lmrext

--- src/search.cpp
+++ src/search.cpp
@@ -1175,7 +1175,7 @@ moves_loop: // When in check, search starts here
       // been searched. In general we would like to reduce them, but there are many
       // cases where we extend a son if it has good chances to be "interesting".
       if (    depth >= 2
-          &&  moveCount > 1 + ((PvNode && ss->ply <= 1) || pvExact)
+          &&  moveCount > 1 + (PvNode && ss->ply <= 1)
           && (   !ss->ttPv
               || !capture
               || (cutNode && (ss-1)->moveCount > 1)))
@@ -1183,7 +1183,7 @@ moves_loop: // When in check, search starts here
           // In general we want to cap the LMR depth search at newDepth, but when
           // reduction is negative, we allow this move a limited search extension
           // beyond the first move depth. This may lead to hidden double extensions.
-          Depth d = std::clamp(newDepth - r, 1, newDepth + 1);
+          Depth d = std::clamp(newDepth - r, 1, newDepth + 1 + pvExact);
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
