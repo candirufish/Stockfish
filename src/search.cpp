9dab9a4002ff2464ef5a3c38526bc2fe2a385b2f
candirufish <38038147+candirufish@users.noreply.github.com>
2023-01-07 18:32:39 +0100

    s18 ttpv r--

--- src/search.cpp
+++ src/search.cpp
@@ -1215,7 +1215,7 @@ moves_loop: // When in check, search starts here
       // Step 18. Full depth search when LMR is skipped. If expected reduction is high, reduce its depth by 1.
       else if (!PvNode || moveCount > 1)
       {
-               if (singularQuietLMR)
+               if (ss->ttPv)
                     r--;
                value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth - (r > 4), !cutNode);
       }
