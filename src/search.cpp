9961b50640ef39e80c991c456f3323e3288388e6
candirufish <38038147+candirufish@users.noreply.github.com>
2023-01-07 14:08:28 +0100

    64 nttm + ap razor

--- src/search.cpp
+++ src/search.cpp
@@ -772,7 +772,7 @@ namespace {
     // Step 7. Razoring (~1 Elo).
     // If eval is really low check with qsearch if it can exceed alpha, if it can't,
     // return a fail low.
-    if (eval < alpha - 394 - 255 * depth * depth)
+    if (eval < alpha + 64 * bool(!ttMove) - 394 - 255 * depth * depth)
     {
         value = qsearch<NonPV>(pos, ss, alpha - 1, alpha);
         if (value < alpha)
