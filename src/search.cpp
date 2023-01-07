c0818c0f02ceecc95a147067863b32336f97705a
candirufish <38038147+candirufish@users.noreply.github.com>
2023-01-07 15:03:35 +0100

    s20 ss-1 r++

--- src/search.cpp
+++ src/search.cpp
@@ -1171,7 +1171,7 @@ moves_loop: // When in check, search starts here
       r -= ss->statScore / (12800 + 4410 * (depth > 7 && depth < 19));
 
       if ((ss-1)->goodMove) 
-          r--;
+          r++;
 
       // Step 17. Late moves reduction / extension (LMR, ~117 Elo)
       // We use various heuristics for the sons of a node after the first son has
