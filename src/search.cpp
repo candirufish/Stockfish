a116ce515865d6690ce28aaa724d79bcbd9a2e1c
candirufish <38038147+candirufish@users.noreply.github.com>
2023-01-08 18:54:55 +0100

    cnt < 1 and imp r--

--- src/search.cpp
+++ src/search.cpp
@@ -1160,7 +1160,7 @@ moves_loop: // When in check, search starts here
       if ((ss+1)->cutoffCnt > 3)
           r++;
 
-      if ((ss+1)->cutoffCnt < 3 && improving)
+      if ((ss+1)->cutoffCnt < 1 && improving)
           r--;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
