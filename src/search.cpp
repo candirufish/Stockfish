c7b8fa22152207bae6d6aa3e77af8fd056c623d7
candirufish <38038147+candirufish@users.noreply.github.com>
2023-01-10 11:59:04 +0100

    s18 nttm cn r+2 +d>=9

--- src/search.cpp
+++ src/search.cpp
@@ -1217,7 +1217,7 @@ moves_loop: // When in check, search starts here
       {
                // Increase reduction for cut nodes and not ttMove (~1 Elo)
                if (!ttMove && cutNode)
-                         r += 2 - ss->ttPv;
+                         r += 2 + bool(depth >= 9);
 
                value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth - (r > 4), !cutNode);
       }
