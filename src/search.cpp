6ad2fdbc8ee4d534c78c35e379d1e07984abbb4f
candirufish <38038147+candirufish@users.noreply.github.com>
2023-01-17 08:21:12 +0100

    no fppr rank 27

--- src/search.cpp
+++ src/search.cpp
@@ -1000,6 +1000,7 @@ moves_loop: // When in check, search starts here
               // Futility pruning for captures (~2 Elo)
               if (   !givesCheck
                   && !PvNode
+                  && !(to_sq(move) & (Rank2BB | Rank7BB))
                   && lmrDepth < 7
                   && !ss->inCheck
                   && ss->staticEval + 185 + 203 * lmrDepth + PieceValue[EG][pos.piece_on(to_sq(move))]
