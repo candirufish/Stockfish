db6d0901012416a0d4a2a410dafb611c211fec27
candirufish <38038147+candirufish@users.noreply.github.com>
2023-01-18 14:14:13 +0100

    ss < avg abs ss24 tr 32 nimp r++

--- src/search.cpp
+++ src/search.cpp
@@ -1170,7 +1170,7 @@ moves_loop: // When in check, search starts here
       if ((ss+1)->cutoffCnt > 3)
           r++;
 
-      if (evalDown)
+      if (evalDown && !improving)
           r++;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
