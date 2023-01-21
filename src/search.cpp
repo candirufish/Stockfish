8145ec233ce537af00aef2d6b83c92eecfb2e718
candirufish <38038147+candirufish@users.noreply.github.com>
2023-01-21 17:22:14 +0100

    pv nttm d >= 6 mvc < 3 r++

--- src/search.cpp
+++ src/search.cpp
@@ -1147,6 +1147,9 @@ moves_loop: // When in check, search starts here
       if (PvNode)
           r -= 1 + 11 / (3 + depth);
 
+      if (PvNode && depth >= 6 && !ttMove && moveCount < 3)
+          r++;
+
       // Decrease reduction if ttMove has been singularly extended (~1 Elo)
       if (singularQuietLMR)
           r--;
