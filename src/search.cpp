baa5f170542a60c276cea9dff76bd6a5cd6b931e
candirufish <38038147+candirufish@users.noreply.github.com>
2023-01-21 06:36:27 +0100

    mpr nttmpv

--- src/search.cpp
+++ src/search.cpp
@@ -989,7 +989,7 @@ moves_loop: // When in check, search starts here
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold (~8 Elo)
-          moveCountPruning = !ttMove && moveCount >= futility_move_count(improving, depth);
+          moveCountPruning = !(ttMove && PvNode) && moveCount >= futility_move_count(improving, depth);
 
           // Reduced depth of the next LMR search
           int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount, delta, thisThread->rootDelta), 0);
