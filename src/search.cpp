0d516e9c2c0b24f0515015309554fd9dc450ccee
candirufish <38038147+candirufish@users.noreply.github.com>
2023-01-21 02:12:58 +0100

    r++ cn k0 mv > 2

--- src/search.cpp
+++ src/search.cpp
@@ -1136,7 +1136,7 @@ moves_loop: // When in check, search starts here
           r--;
 
       // Increase reduction for cut nodes (~3 Elo)
-      if (cutNode && !(move == ss->killers[0] && moveCount == 1))
+      if (cutNode && !(move == ss->killers[0] && moveCount > 2))
           r += 2;
 
       // Increase reduction if ttMove is a capture (~3 Elo)
