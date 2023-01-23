32aa6c5b000badb13bf1951e0d3dd694dc8bb9a9
candirufish <38038147+candirufish@users.noreply.github.com>
2023-01-23 12:21:35 +0100

    mvc > 7- 2 * pv

--- src/search.cpp
+++ src/search.cpp
@@ -1135,7 +1135,7 @@ moves_loop: // When in check, search starts here
           r -= 2;
 
       // Decrease reduction if opponent's move count is high (~1 Elo)
-      if ((ss-1)->moveCount > 7)
+      if ((ss-1)->moveCount > 7 - 2 * PvNode)
           r--;
 
       // Increase reduction for cut nodes (~3 Elo)
