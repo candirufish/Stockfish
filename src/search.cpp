2f39c5c8b439327169acee4682ce9d498051ea4b
candirufish <38038147+candirufish@users.noreply.github.com>
2023-01-23 12:18:34 +0100

    nttm cn +2 + r50 > 80

--- src/search.cpp
+++ src/search.cpp
@@ -1220,7 +1220,7 @@ moves_loop: // When in check, search starts here
       {
                // Increase reduction for cut nodes and not ttMove (~1 Elo)
                if (!ttMove && cutNode)
-                         r += !capture && pos.rule50_count() > 32 ? 3 : 2;
+                         r += pos.rule50_count() > 80 ? 3 : 2;
 
                value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth - (r > 4), !cutNode);
       }
