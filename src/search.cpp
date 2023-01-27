09b60869cc305735ab6c95c88bb05562b0920f16
candirufish <38038147+candirufish@users.noreply.github.com>
2023-01-27 15:50:45 +0100

    s20 nrn d-=1

--- src/search.cpp
+++ src/search.cpp
@@ -1315,6 +1315,7 @@ moves_loop: // When in check, search starts here
                   // Reduce other moves if we have found at least one score improvement
                   if (   depth > 1
                       && depth < 6
+                      && !rootNode
                       && beta  <  VALUE_KNOWN_WIN
                       && alpha > -VALUE_KNOWN_WIN)
                       depth -= 1;
