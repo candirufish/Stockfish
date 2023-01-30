852a6e74ff72dc0d83a40c39e565dc49a561702a
candirufish <38038147+candirufish@users.noreply.github.com>
2023-01-30 13:52:59 +0100

    v > sb + 25 sb < b ttv <= v ext -1

--- src/search.cpp
+++ src/search.cpp
@@ -1095,7 +1095,7 @@ moves_loop: // When in check, search starts here
               else if (ttValue <= alpha && ttValue <= value)
                   extension = -1;
 
-              else if (value > singularBeta + 25 && singularBeta < beta)
+              else if (value > singularBeta + 25 && singularBeta < beta && ttValue <= value)
                   extension = -1;
           }
 
