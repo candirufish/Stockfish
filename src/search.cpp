50a7f9ba5df2989e49a66d182f860c72f7e58ab3
candirufish <38038147+candirufish@users.noreply.github.com>
2023-02-08 01:41:39 +0100

    s20 d < 6 d >= 6 cpx > 1024  ave > 128

--- src/search.cpp
+++ src/search.cpp
@@ -1319,8 +1319,7 @@ moves_loop: // When in check, search starts here
 
                   // Reduce other moves if we have found at least one score improvement
                   if (   depth > 1
-                      && depth < 6
-                      && !ss->inCheck
+                      && ((depth >= 6 && thisThread->complexityAverage.value() > 128 && complexity > 1024) || depth < 6)
                       && beta  <  VALUE_KNOWN_WIN
                       && alpha > -VALUE_KNOWN_WIN)
                       depth -= 1;
