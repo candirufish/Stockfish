9a063eaf89b43ba113a9c2ea147ec7c2d74f7f75
candirufish <38038147+candirufish@users.noreply.github.com>
2023-02-08 01:29:17 +0100

    s20 ninc

--- src/search.cpp
+++ src/search.cpp
@@ -1320,6 +1320,7 @@ moves_loop: // When in check, search starts here
                   // Reduce other moves if we have found at least one score improvement
                   if (   depth > 1
                       && depth < 6
+                      && !ss->inCheck
                       && beta  <  VALUE_KNOWN_WIN
                       && alpha > -VALUE_KNOWN_WIN)
                       depth -= 1;
