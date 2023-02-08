1a5f4aafb3d9b27b91c8854ccb297431a7be5dd9
candirufish <38038147+candirufish@users.noreply.github.com>
2023-02-08 03:03:02 +0100

    s18 nttm npv ncp cn r+3 2 - ttpv

--- src/search.cpp
+++ src/search.cpp
@@ -1231,7 +1231,7 @@ moves_loop: // When in check, search starts here
       {
                // Increase reduction for cut nodes and not ttMove (~1 Elo)
                if (!ttMove && !PvNode)
-                         r += 2 + !capture + cutNode - ss->ttPv;
+                         r += !capture && cutNode ? 3 : 2 - ss->ttPv;
 
                value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth - (r > 4), !cutNode);
       }
