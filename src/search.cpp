b707aaebf6cfcdd2beec0a7ab7c6fcc87523bfa3
candirufish <38038147+candirufish@users.noreply.github.com>
2023-02-14 04:24:16 +0100

    tte pv r--

--- src/search.cpp
+++ src/search.cpp
@@ -1157,6 +1157,9 @@ moves_loop: // When in check, search starts here
       if (PvNode)
           r -= 1 + 11 / (3 + depth);
 
+      if (PvNode && tte->is_pv())
+          r--;
+
       // Decrease reduction if ttMove has been singularly extended (~1 Elo)
       if (singularQuietLMR)
           r--;
