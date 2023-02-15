2c2674dd01124527d9360322c6b10f1b57b316ad
candirufish <38038147+candirufish@users.noreply.github.com>
2023-02-15 08:04:09 +0100

    s18 mc > 1 + pv ssp <= 1

--- src/search.cpp
+++ src/search.cpp
@@ -1225,7 +1225,7 @@ moves_loop: // When in check, search starts here
       }
 
       // Step 18. Full depth search when LMR is skipped. If expected reduction is high, reduce its depth by 1.
-      else if (!PvNode || moveCount > 1)
+      else if (!PvNode || moveCount > 1 + (PvNode && ss->ply <= 1))
       {
                // Increase reduction for cut nodes and not ttMove (~1 Elo)
                if (!ttMove && cutNode)
