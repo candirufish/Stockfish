ca6fb6ff179cf0a8d335bf68502b656bf59ec624
candirufish <38038147+candirufish@users.noreply.github.com>
2023-02-22 14:54:56 +0100

    r-- mc 7 - 3 ss1 mcpr

--- src/search.cpp
+++ src/search.cpp
@@ -939,6 +939,7 @@ moves_loop: // When in check, search starts here
 
     value = bestValue;
     moveCountPruning = singularQuietLMR = false;
+    ss->mcp = false;
 
     // Indicate PvNodes that will probably fail low if the node was searched
     // at a depth equal or greater than the current depth, and the result of this search was a fail low.
@@ -995,7 +996,7 @@ moves_loop: // When in check, search starts here
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold (~8 Elo)
-          moveCountPruning = moveCount >= futility_move_count(improving, depth);
+          ss->mcp = moveCountPruning = moveCount >= futility_move_count(improving, depth);
 
           // Reduced depth of the next LMR search
           int lmrDepth = std::max(newDepth - r, 0);
@@ -1142,7 +1143,7 @@ moves_loop: // When in check, search starts here
           r -= 2;
 
       // Decrease reduction if opponent's move count is high (~1 Elo)
-      if ((ss-1)->moveCount > 7)
+      if ((ss-1)->moveCount > 7 - 3 * (ss-1)->mcp)
           r--;
 
       // Increase reduction for cut nodes (~3 Elo)
