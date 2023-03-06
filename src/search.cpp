0a8e182bc8d773c131bb1d9830231c0b9b525a94
candirufish <38038147+candirufish@users.noreply.github.com>
2023-03-06 01:32:55 +0100

    ss1 mv > npv or prcap 4 7

--- src/search.cpp
+++ src/search.cpp
@@ -1149,7 +1149,7 @@ moves_loop: // When in check, search starts here
           r -= 2;
 
       // Decrease reduction if opponent's move count is high (~1 Elo)
-      if ((ss-1)->moveCount > 7)
+      if ((ss-1)->moveCount > (!PvNode || priorCapture ? 4 : 7))
           r--;
 
       // Increase reduction for cut nodes (~3 Elo)
