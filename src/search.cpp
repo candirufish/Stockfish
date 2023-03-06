5524cc719b4893a6baa7de7cc18f6057d929eae3
candirufish <38038147+candirufish@users.noreply.github.com>
2023-03-06 01:47:05 +0100

    negext margin +64 -64

--- src/search.cpp
+++ src/search.cpp
@@ -1104,11 +1104,11 @@ moves_loop: // When in check, search starts here
 
               // If the eval of ttMove is greater than beta, we reduce it (negative extension)
               else if (ttValue >= beta)
-                  extension = (ttValue >= (beta + 32) ? -3 : -2);
+                  extension = (ttValue >= (beta + 64) ? -3 : -2);
 
               // If the eval of ttMove is less than value, we reduce it (negative extension)
               else if (ttValue <= value)
-                  extension = (ttValue <= (value - 32) ? -2 : -1);
+                  extension = (ttValue <= (value - 64) ? -2 : -1);
           }
 
           // Check extensions (~1 Elo)
