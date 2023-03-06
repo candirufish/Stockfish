1858fa4debad06bf09910a7334923fb048d54f5d
candirufish <38038147+candirufish@users.noreply.github.com>
2023-03-06 02:12:17 +0100

    dblext ss1 mcp

--- src/search.cpp
+++ src/search.cpp
@@ -946,6 +946,7 @@ moves_loop: // When in check, search starts here
 
     value = bestValue;
     moveCountPruning = singularQuietLMR = false;
+    ss->mcp = false;
 
     // Indicate PvNodes that will probably fail low if the node was searched
     // at a depth equal or greater than the current depth, and the result of this search was a fail low.
@@ -1002,7 +1003,7 @@ moves_loop: // When in check, search starts here
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold (~8 Elo)
-          moveCountPruning = moveCount >= futility_move_count(improving, depth);
+          ss->mcp = moveCountPruning = moveCount >= futility_move_count(improving, depth);
 
           // Reduced depth of the next LMR search
           int lmrDepth = std::max(newDepth - r, 0);
@@ -1086,7 +1087,7 @@ moves_loop: // When in check, search starts here
 
                   // Avoid search explosion by limiting the number of double extensions
                   if (  !PvNode
-                      && value < singularBeta - 25
+                      && (value < singularBeta - 25 || (ss-1)->mcp)
                       && ss->doubleExtensions <= 10)
                   {
                       extension = 2;
