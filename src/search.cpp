035fc6948d160b12b3ce0b6706afb290ae41e388
candirufish <38038147+candirufish@users.noreply.github.com>
2023-03-15 19:06:35 +0100

    q ext pv or d > 10

--- src/search.cpp
+++ src/search.cpp
@@ -1120,7 +1120,7 @@ moves_loop: // When in check, search starts here
               extension = 1;
 
           // Quiet ttMove extensions (~1 Elo)
-          else if (   (PvNode || depth > 6)
+          else if (   (PvNode || depth > 10)
                    && move == ttMove
                    && move == ss->killers[0]
                    && (*contHist[0])[movedPiece][to_sq(move)] >= 5705)
