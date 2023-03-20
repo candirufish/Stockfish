1d798f367e6d114d4d9d53bb302bc849491a4bbe
candirufish <38038147+candirufish@users.noreply.github.com>
2023-03-20 05:10:06 +0100

    ext -2 ttv v-32

--- src/search.cpp
+++ src/search.cpp
@@ -1107,7 +1107,7 @@ moves_loop: // When in check, search starts here
 
               // If the eval of ttMove is less than value, we reduce it (negative extension)
               else if (ttValue <= value)
-                  extension = -1;
+                  extension = (ttValue <= (value - 32) ? -2 : -1);
 
               // If the eval of ttMove is less than alpha, we reduce it (negative extension)
               else if (ttValue <= alpha)
