9b14de93dece15df152498f0315d43ad98d33c48
candirufish <38038147+candirufish@users.noreply.github.com>
2023-03-22 18:24:32 +0100

    abs se ss2 se > vkw ext -1

--- src/search.cpp
+++ src/search.cpp
@@ -1113,7 +1113,7 @@ moves_loop: // When in check, search starts here
               else if (ttValue <= alpha)
                   extension = -1;
 
-              else if (abs(ss->staticEval) > VALUE_KNOWN_WIN)
+              else if (abs(ss->staticEval) > VALUE_KNOWN_WIN && abs((ss-2)->staticEval) > VALUE_KNOWN_WIN)
                   extension = -1;
           }
 
