8f4c770850d8e23c93bc0c4fb5906d6a3d88c09e
candirufish <38038147+candirufish@users.noreply.github.com>
2023-03-23 10:28:14 +0100

    afpr ss2 se cnlmr +3

--- src/search.cpp
+++ src/search.cpp
@@ -1046,7 +1046,7 @@ moves_loop: // When in check, search starts here
               {
                   if (ss->staticEval + 103 + 138 * lmrDepth <= alpha)
                       continue;
-                  else if (ss->staticEval + 103 + 138 * lmrDepth <= alpha + 16)
+                  else if ((ss-2)->staticEval + 103 + 138 * lmrDepth <= alpha)
                       almostFutPruned = true;
               }
 
