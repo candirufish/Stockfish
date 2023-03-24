ba43075e18179ccac0eecf785a6863cd4109615c
candirufish <38038147+candirufish@users.noreply.github.com>
2023-03-24 08:24:03 +0100

    npv afpr mcpr r++

--- src/search.cpp
+++ src/search.cpp
@@ -1168,7 +1168,7 @@ moves_loop: // When in check, search starts here
       if (ttCapture)
           r++;
 
-      if (almostFutPruned && moveCountPruning)
+      if (!PvNode && almostFutPruned && moveCountPruning)
           r++;
 
       // Decrease reduction for PvNodes based on depth
