497456a3582ee53f7b5f5ec1eb77f428b3767ad3
candirufish <38038147+candirufish@users.noreply.github.com>
2023-03-28 22:57:33 +0200

    flraz fs d-3 v ap 3%

--- src/search.cpp
+++ src/search.cpp
@@ -555,7 +555,7 @@ namespace {
     Move ttMove, move, excludedMove, bestMove;
     Depth extension, newDepth;
     Value bestValue, value, ttValue, eval, maxValue, probCutBeta;
-    bool givesCheck, improving, priorCapture, singularQuietLMR, almostRazor;
+    bool givesCheck, improving, priorCapture, singularQuietLMR;
     bool capture, moveCountPruning, ttCapture;
     Piece movedPiece;
     int moveCount, captureCount, quietCount, improvement, complexity;
@@ -719,7 +719,6 @@ namespace {
         // Skip early pruning when in check
         ss->staticEval = eval = VALUE_NONE;
         improving = false;
-        almostRazor = false;
         improvement = 0;
         complexity = 0;
         goto moves_loop;
@@ -773,7 +772,6 @@ namespace {
                   : (ss-4)->staticEval != VALUE_NONE ? ss->staticEval - (ss-4)->staticEval
                   :                                    156;
     improving = improvement > 0;
-    almostRazor = false;
 
     // Step 7. Razoring (~1 Elo).
     // If eval is really low check with qsearch if it can exceed alpha, if it can't,
@@ -785,7 +783,12 @@ namespace {
             return value;
 
         int max_raz = std::max(value, alpha);
-        almostRazor = (std::abs(value - alpha) * 100 <= max_raz * 3);
+        if (std::abs(value - alpha) * 100 <= max_raz * 3)
+        {
+        value = search<NonPV>(pos, ss, alpha - 1, alpha, depth - 3, cutNode);
+        if (value < alpha)
+            return value;
+        }
 
     }
 
@@ -904,12 +907,12 @@ namespace {
 
     // Step 11. If the position is not in TT, decrease depth by 3.
     // Use qsearch if depth is equal or below zero (~9 Elo)
-    if (    (PvNode || almostRazor)
+    if (    PvNode
         && !ttMove)
         depth -= 3;
 
     if (depth <= 0)
-        return qsearch<PvNode ? PV : NonPV>(pos, ss, alpha, beta);
+        return qsearch<PV>(pos, ss, alpha, beta);
 
 
     if (    cutNode
