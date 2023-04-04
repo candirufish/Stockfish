1e4f13fbc38a88c8a95b01fbccc9546f93d75337
candirufish <38038147+candirufish@users.noreply.github.com>
2023-04-05 00:30:22 +0200

    d-5 ss-2 nss-1 tteD

--- src/search.cpp
+++ src/search.cpp
@@ -904,7 +904,7 @@ namespace {
     ss->tteD = ss->ttHit && tte->depth() >= depth;
     if (    PvNode
         && !ttMove)
-        depth -= ss->tteD ? (ss-2)->tteD ? 5 : 4 : 2;
+        depth -= ss->tteD ? (ss-2)->tteD && !(ss-1)->tteD ? 5 : 4 : 2;
 
     if (depth <= 0)
         return qsearch<PV>(pos, ss, alpha, beta);
