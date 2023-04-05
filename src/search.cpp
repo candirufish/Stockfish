53f5d7007c37c5a1202aa5dfd5bd3361d377834a
candirufish <38038147+candirufish@users.noreply.github.com>
2023-04-05 17:47:31 +0200

    d-5 ss-2 nttm tteD

--- src/search.cpp
+++ src/search.cpp
@@ -897,10 +897,10 @@ namespace {
 
     // Step 11. If the position is not in TT, decrease depth by 2 (or by 4 if the TT entry for the current position was hit and the stored depth is greater than or equal to the current depth).
     // Use qsearch if depth is equal or below zero (~9 Elo)
-    ss->tteD = ss->ttHit && tte->depth() >= depth;
+    ss->tteD = ss->ttHit && tte->depth() >= depth && !ttMove;
     if (    PvNode
         && !ttMove)
-        depth -= ss->tteD ? (ss-2)->tteD && !(ss-1)->tteD ? 5 : 4 : 2;
+        depth -= ss->tteD ? (ss-2)->tteD ? 5 : 4 : 2;
 
     if (depth <= 0)
         return qsearch<PV>(pos, ss, alpha, beta);
