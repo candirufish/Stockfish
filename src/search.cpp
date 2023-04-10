cf78c837fab75d1bec6b8dfdac10415f78303e73
candirufish <38038147+candirufish@users.noreply.github.com>
2023-04-10 05:36:43 +0200

    ft pr r50 >= 1 ss-1 r50 < 40

--- src/search.cpp
+++ src/search.cpp
@@ -1009,7 +1009,8 @@ moves_loop: // When in check, search starts here
               if (   !givesCheck
                   && lmrDepth < 6
                   && !ss->inCheck
-                  && ss->staticEval + 182 + 230 * lmrDepth + PieceValue[EG][pos.piece_on(to_sq(move))] + (3 * (ss-1)->rule50) * PvNode
+                  && ((ss-1)->rule50 < 40 || pos.rule50_count() >= 1)
+                  && ss->staticEval + 182 + 230 * lmrDepth + PieceValue[EG][pos.piece_on(to_sq(move))]
                    + captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] / 7 < alpha)
                   continue;
 
