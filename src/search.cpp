0e93f3e1d23635926b02cb01c579a205294f2257
candirufish <38038147+candirufish@users.noreply.github.com>
2023-04-13 14:44:48 +0200

    ft cap se + 50 * pv tte

--- src/search.cpp
+++ src/search.cpp
@@ -1007,7 +1007,7 @@ moves_loop: // When in check, search starts here
               if (   !givesCheck
                   && lmrDepth < 6
                   && !ss->inCheck
-                  && ss->staticEval + 182 + 230 * lmrDepth + PieceValue[EG][pos.piece_on(to_sq(move))]
+                  && ss->staticEval + 182 + 230 * lmrDepth + PieceValue[EG][pos.piece_on(to_sq(move))] + 50 * (PvNode && tte->is_pv())
                    + captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] / 7 < alpha)
                   continue;
 
