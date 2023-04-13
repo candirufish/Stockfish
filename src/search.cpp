c93836d39f5bd0a3dbe7bcefa8905e6142650a94
candirufish <38038147+candirufish@users.noreply.github.com>
2023-04-13 06:56:18 +0200

    ttv b -2 -npv -cn ss-1 r>0

--- src/search.cpp
+++ src/search.cpp
@@ -1115,7 +1115,7 @@ moves_loop: // When in check, search starts here
 
               // If the eval of ttMove is greater than beta, we reduce it (negative extension) (~7 Elo)
               else if (ttValue >= beta)
-                  extension = -2 - !PvNode - (cutNode && (ss-1)->r < -1);
+                  extension = -2 - !PvNode - (cutNode && (ss-1)->r > 0);
 
               // If the eval of ttMove is less than value, we reduce it (negative extension) (~1 Elo)
               else if (ttValue <= value)
