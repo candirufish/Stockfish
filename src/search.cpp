540215fb6f562baa395c8e850501f9720a0618ed
candirufish <38038147+candirufish@users.noreply.github.com>
2023-04-15 00:16:34 +0200

    ft cap se + 50 * pv np cap

--- src/search.cpp
+++ src/search.cpp
@@ -1011,9 +1011,8 @@ moves_loop: // When in check, search starts here
               // Futility pruning for captures (~2 Elo)
               if (   !givesCheck
                   && lmrDepth < 6
-                  && !cpType
                   && !ss->inCheck
-                  && ss->staticEval + 182 + 230 * lmrDepth + PieceValue[EG][pos.piece_on(to_sq(move))]
+                  && ss->staticEval + 182 + 230 * lmrDepth + PieceValue[EG][pos.piece_on(to_sq(move))] + 50 * cpType
                    + captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] / 7 < alpha)
                   continue;
 
