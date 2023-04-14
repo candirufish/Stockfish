9387d231d368db7101ede09fc7a1c1c93c696b41
candirufish <38038147+candirufish@users.noreply.github.com>
2023-04-14 02:24:12 +0200

    s20 pv v < b r50 > 80

--- src/search.cpp
+++ src/search.cpp
@@ -1332,6 +1332,7 @@ moves_loop: // When in check, search starts here
                   // Reduce other moves if we have found at least one score improvement (~1 Elo)
                   if (   depth > 1
                       && (   (improving && complexity > 971)
+                          || pos.rule50_count() > 80
                           || value < (5 * alpha + 75 * beta) / 87
                           || depth < 6)
                       && beta  <  12535
