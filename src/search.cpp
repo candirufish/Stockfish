ff0a7b6b588e92721e65d1507639504c6d133de5
candirufish <38038147+candirufish@users.noreply.github.com>
2023-04-15 13:02:14 +0200

    no ftpr pv nttm cp not pawn

--- src/search.cpp
+++ src/search.cpp
@@ -1005,7 +1005,7 @@ moves_loop: // When in check, search starts here
           {
               bool cpType = false;
               if (   PvNode
-                  && ttMove
+                  && !ttMove
                   && type_of(pos.piece_on(to_sq(move))) != PAWN)
                   cpType = true;
 
