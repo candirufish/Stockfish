27cd2f00fa0cea97c1f67be88bfef920aae13a33
candirufish <38038147+candirufish@users.noreply.github.com>
2023-04-16 23:35:28 +0200

    cnlmr 1 cp qc < 2

--- src/search.cpp
+++ src/search.cpp
@@ -1168,8 +1168,8 @@ moves_loop: // When in check, search starts here
           r--;
 
       // Increase reduction for cut nodes (~3 Elo)
-      if (cutNode && !(capture && quietCount < 2))
-          r += 2;
+      if (cutNode)
+          r += capture && quietCount < 2 ? 1 : 2;
 
       // Increase reduction if ttMove is a capture (~3 Elo)
       if (ttCapture)
