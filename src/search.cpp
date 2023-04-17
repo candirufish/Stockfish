23e4f5fcdf1557b0f031ce224de3f9f2a808c42d
candirufish <38038147+candirufish@users.noreply.github.com>
2023-04-17 03:59:40 +0200

    raz fm >= beta s11 d--

--- src/search.cpp
+++ src/search.cpp
@@ -908,7 +908,7 @@ namespace {
         depth -= 2 + 2 * (ss->ttHit &&  tte->depth() >= depth);
 
     if (    almostRazor)
-        depth -= 3;
+        depth--;
 
     if (depth <= 0)
         return qsearch<PvNode ? PV : NonPV>(pos, ss, alpha, beta);
