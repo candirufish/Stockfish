840864436c316b48da843a97dc4132898f024765
candirufish <38038147+candirufish@users.noreply.github.com>
2023-04-19 03:47:47 +0200

    no ftpr pv cp not pawn qc < 2 ss-1 mv <=1

--- src/search.cpp
+++ src/search.cpp
@@ -1006,7 +1006,7 @@ moves_loop: // When in check, search starts here
               bool cpType = false;
               if (   PvNode
                   && quietCount < 2
-                  && moveCount > 2
+                  && (ss-1)->moveCount <= 1
                   && type_of(pos.piece_on(to_sq(move))) != PAWN)
                   cpType = true;
 
