86c6a8b3ceab1a50e81dc342c95b36924de67a02
candirufish <38038147+candirufish@users.noreply.github.com>
2023-04-24 11:47:51 +0200

    no else mv ttm lmr

--- src/search.cpp
+++ src/search.cpp
@@ -1169,7 +1169,7 @@ moves_loop: // When in check, search starts here
       if ((ss+1)->cutoffCnt > 3)
           r++;
 
-      else if (move == ttMove)
+      if (move == ttMove)
           r--;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
