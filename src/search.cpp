be855cb98d8293ff87ccf2b0e076e8dff2ec75ba
candirufish <38038147+candirufish@users.noreply.github.com>
2023-04-25 04:41:06 +0200

    prb + ss-1 mcp 2 * pv

--- src/search.cpp
+++ src/search.cpp
@@ -1373,7 +1373,7 @@ moves_loop: // When in check, search starts here
     // Bonus for prior countermove that caused the fail low
     else if (!priorCapture && prevSq != SQ_NONE)
     {
-        int bonus = (depth > 5) + (PvNode || cutNode) + (bestValue < alpha - 97 * depth) + ((ss-1)->moveCount > 10) + ((ss-1)->mcp && PvNode);
+        int bonus = (depth > 5) + (PvNode || cutNode) + (bestValue < alpha - 97 * depth) + ((ss-1)->moveCount > 10) + 2 * ((ss-1)->mcp && PvNode);
         update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, stat_bonus(depth) * bonus);
     }
 
