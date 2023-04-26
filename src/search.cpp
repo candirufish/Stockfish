c6e58091fe8f66026d94abfe8f0ba88d434bcb1c
candirufish <38038147+candirufish@users.noreply.github.com>
2023-04-26 16:44:20 +0200

    s11 pv not ss-1 prcap

--- src/search.cpp
+++ src/search.cpp
@@ -883,6 +883,8 @@ namespace {
     // Step 11. If the position is not in TT, decrease depth by 2 (or by 4 if the TT entry for the current position was hit and the stored depth is greater than or equal to the current depth).
     // Use qsearch if depth is equal or below zero (~9 Elo)
     if (    PvNode
+        && !(ss-1)->inCheck 
+        && !priorCapture
         && !ttMove)
         depth -= 2 + 2 * (ss->ttHit &&  tte->depth() >= depth);
 
