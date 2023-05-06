4875aaf627645382803612d28c9a5dcfdfe689e2
candirufish <38038147+candirufish@users.noreply.github.com>
2023-05-06 16:44:42 +0200

    s20 cnt + 1 nttm or v > 12535

--- src/search.cpp
+++ src/search.cpp
@@ -1326,7 +1326,7 @@ moves_loop: // When in check, search starts here
               }
               else
               {
-                  ss->cutoffCnt += 1 + (!ttMove || (value > VALUE_KNOWN_WIN));
+                  ss->cutoffCnt += 1 + (!ttMove || (value > 12535));
                   assert(value >= beta); // Fail high
                   break;
               }
