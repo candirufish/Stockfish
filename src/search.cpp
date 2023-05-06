7db771df73aaba3cadf7aa4d3d49867c13880ac6
candirufish <38038147+candirufish@users.noreply.github.com>
2023-05-06 15:20:52 +0200

    cnlmr ttd >= d+3 r+1

--- src/search.cpp
+++ src/search.cpp
@@ -1144,7 +1144,7 @@ moves_loop: // When in check, search starts here
       // Decrease further on cutNodes. (~1 Elo)
       if (   ss->ttPv
           && !likelyFailLow)
-          r -= cutNode && tte->depth() >= depth + 3 ? 3 : 2;
+          r -= 2;
 
       // Decrease reduction if opponent's move count is high (~1 Elo)
       if ((ss-1)->moveCount > 7)
@@ -1152,7 +1152,7 @@ moves_loop: // When in check, search starts here
 
       // Increase reduction for cut nodes (~3 Elo)
       if (cutNode)
-          r += 2;
+          r += tte->depth() >= depth + 3 ? 1 : 2;
 
       // Increase reduction if ttMove is a capture (~3 Elo)
       if (ttCapture)
