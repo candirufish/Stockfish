ef66555da5d2a3a00d455e278173d3c4396022b4
candirufish <38038147+candirufish@users.noreply.github.com>
2023-05-09 10:21:42 +0200

    npv ttd >= d+3 r-2

--- src/search.cpp
+++ src/search.cpp
@@ -1144,7 +1144,7 @@ moves_loop: // When in check, search starts here
           r -= 2;
 
       if (!PvNode && tte->depth() >= depth + 3)
-          r--;
+          r -= 2;
 
       // Decrease reduction if opponent's move count is high (~1 Elo)
       if ((ss-1)->moveCount > 7)
