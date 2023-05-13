9332bad87e525666c7cb0a0d6e9c1d14977d89ca
candirufish <38038147+candirufish@users.noreply.github.com>
2023-05-13 03:19:04 +0200

    nttm npv ttd >= d+3 r--

--- src/search.cpp
+++ src/search.cpp
@@ -1141,7 +1141,10 @@ moves_loop: // When in check, search starts here
       // Decrease further on cutNodes. (~1 Elo)
       if (   ss->ttPv
           && !likelyFailLow)
-          r -= cutNode && tte->depth() >= depth + 3 ? 3 : 2;
+          r -= 2;
+
+      if (!ttMove && !PvNode && tte->depth() >= depth + 3)
+          r--;
 
       // Decrease reduction if opponent's move count is high (~1 Elo)
       if ((ss-1)->moveCount > 7)
