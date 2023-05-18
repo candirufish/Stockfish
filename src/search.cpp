a9041b4115ae418e5579a255d11ef40561e77de5
candirufish <38038147+candirufish@users.noreply.github.com>
2023-05-18 03:24:04 +0200

    ttv v -1 - mv < 1

--- src/search.cpp
+++ src/search.cpp
@@ -1094,11 +1094,11 @@ moves_loop: // When in check, search starts here
 
               // If the eval of ttMove is greater than beta, we reduce it (negative extension) (~7 Elo)
               else if (ttValue >= beta)
-                  extension = -2 - !PvNode - (ss->moveCount < 1);
+                  extension = -2 - !PvNode;
 
               // If the eval of ttMove is less than value, we reduce it (negative extension) (~1 Elo)
               else if (ttValue <= value)
-                  extension = -1;
+                  extension = -1 - (ss->moveCount < 1);
 
               // If the eval of ttMove is less than alpha, we reduce it (negative extension) (~1 Elo)
               else if (ttValue <= alpha)
