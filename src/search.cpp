1b8bdc3d57ba835b85d2c7342ce17113d786eb9e
candirufish <38038147+candirufish@users.noreply.github.com>
2023-05-18 03:31:01 +0200

    dblext d + < 7 ttpv

--- src/search.cpp
+++ src/search.cpp
@@ -1080,7 +1080,7 @@ moves_loop: // When in check, search starts here
                       && ss->doubleExtensions <= 10)
                   {
                       extension = 2;
-                      depth += depth < 13;
+                      depth += depth < (ss->ttPv ? 7 : 13);
                   }
               }
 
