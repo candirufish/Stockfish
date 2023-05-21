2aacefa93f4379c836fa2dea0531e41580999730
candirufish <38038147+candirufish@users.noreply.github.com>
2023-05-22 00:16:53 +0200

    s20 and depth < 10

--- src/search.cpp
+++ src/search.cpp
@@ -1318,9 +1318,10 @@ moves_loop: // When in check, search starts here
               {
                   // Reduce other moves if we have found at least one score improvement (~1 Elo)
                   if (   depth > 1
+                      && depth < 10
                       && beta  <  14001
                       && value > -12754)
-                      depth -= depth > 3 && depth < 10 ? 2 : 1;
+                      depth -= 1;
 
                   assert(depth > 0);
                   alpha = value; // Update alpha! Always alpha < beta
