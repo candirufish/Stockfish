c4fa2e8f0a0173fea288d9051ace20c63ef69a3e
candirufish <38038147+candirufish@users.noreply.github.com>
2023-05-22 08:32:39 +0200

    cnlmr ncap

--- src/search.cpp
+++ src/search.cpp
@@ -1142,11 +1142,11 @@ moves_loop: // When in check, search starts here
           r -= cutNode && tte->depth() >= depth + 3 ? 3 : 2;
 
       // Decrease reduction if opponent's move count is high (~1 Elo)
-      if ((ss-1)->moveCount > 8 && !capture)
+      if ((ss-1)->moveCount > 8)
           r--;
 
       // Increase reduction for cut nodes (~3 Elo)
-      if (cutNode)
+      if (cutNode && !capture)
           r += 2;
 
       // Increase reduction if ttMove is a capture (~3 Elo)
