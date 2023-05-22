7bcde24713c67c758a00972427987a04fd6304e2
candirufish <38038147+candirufish@users.noreply.github.com>
2023-05-22 08:34:07 +0200

    ttc ncap

--- src/search.cpp
+++ src/search.cpp
@@ -1146,11 +1146,11 @@ moves_loop: // When in check, search starts here
           r--;
 
       // Increase reduction for cut nodes (~3 Elo)
-      if (cutNode && !capture)
+      if (cutNode)
           r += 2;
 
       // Increase reduction if ttMove is a capture (~3 Elo)
-      if (ttCapture)
+      if (ttCapture && !capture)
           r++;
 
       // Decrease reduction for PvNodes based on depth (~2 Elo)
