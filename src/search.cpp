9c48b3dca0ee552bd08181fc4faf0b9219f6f571
candirufish <38038147+candirufish@users.noreply.github.com>
2023-05-23 17:06:09 +0200

    cnt + nttm cn 3 2 1

--- src/search.cpp
+++ src/search.cpp
@@ -1310,7 +1310,7 @@ moves_loop: // When in check, search starts here
 
               if (value >= beta)
               {
-                  ss->cutoffCnt += 1 + (!ttMove || cutNode);
+                  ss->cutoffCnt += (!ttMove ? (cutNode ? 3 : 2) : 1);
                   assert(value >= beta); // Fail high
                   break;
               }
