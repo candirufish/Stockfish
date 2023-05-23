fc159bc68b01eea03ac3cfe8a56efd553c5582f2
candirufish <38038147+candirufish@users.noreply.github.com>
2023-05-23 17:18:43 +0200

    s20 pv ttm cnt--

--- src/search.cpp
+++ src/search.cpp
@@ -1328,7 +1328,7 @@ moves_loop: // When in check, search starts here
           }
       }
       else if (ttMove && PvNode)
-          ss->cutoffCnt = 0;
+          ss->cutoffCnt--;
 
       // If the move is worse than some previously searched move, remember it to update its stats later
       if (move != bestMove)
