35088da26872e4896fdf0027238fa91216f0fc7c
candirufish <38038147+candirufish@users.noreply.github.com>
2023-05-23 16:39:09 +0200

    dess cn inc

--- src/search.cpp
+++ src/search.cpp
@@ -1202,10 +1202,11 @@ moves_loop: // When in check, search starts here
               const bool doDeeperSearch = value > (bestValue + 63 + 11 * (newDepth - d));
               const bool doEvenDeeperSearch = value > alpha + 662 && ss->doubleExtensions <= 6;
               const bool doShallowerSearch = value < bestValue + newDepth;
+              const bool doEvenShallowerSearch = cutNode && ss->inCheck;
 
               ss->doubleExtensions = ss->doubleExtensions + doEvenDeeperSearch;
 
-              newDepth += doDeeperSearch - doShallowerSearch + doEvenDeeperSearch;
+              newDepth += doDeeperSearch - doShallowerSearch + doEvenDeeperSearch - doEvenShallowerSearch;
 
               if (newDepth > d)
                   value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth, !cutNode);
