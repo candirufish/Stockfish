19f14366fac16a40c7a97361a70e9b67bf4dd35c
candirufish <38038147+candirufish@users.noreply.github.com>
2023-05-24 14:14:22 +0200

    r++ cnt cn > 1

--- src/search.cpp
+++ src/search.cpp
@@ -1162,7 +1162,7 @@ moves_loop: // When in check, search starts here
           r--;
 
       // Increase reduction if next ply has a lot of fail high (~5 Elo)
-      if ((ss+1)->cutoffCnt > 3)
+      if ((ss+1)->cutoffCnt > (cutNode ? 1 : 3))
           r++;
 
       else if (move == ttMove)
