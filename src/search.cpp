e5c5bc8b2b20ada250b2abcfc6c8cf965554842a
candirufish <38038147+candirufish@users.noreply.github.com>
2023-05-25 00:41:43 +0200

    nd - dess cn inc

--- src/search.cpp
+++ src/search.cpp
@@ -1194,6 +1194,9 @@ moves_loop: // When in check, search starts here
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
+          const bool doEvenShallowerSearch = cutNode && ss->inCheck;
+          newDepth -= doEvenShallowerSearch;
+
           // Do full depth search when reduced LMR search fails high
           if (value > alpha && d < newDepth)
           {
