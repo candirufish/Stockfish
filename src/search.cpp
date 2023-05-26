7ed6fa09afd3f2093fb88f5732d83043e9279600
candirufish <38038147+candirufish@users.noreply.github.com>
2023-05-26 10:19:15 +0200

    ext 2 ap<=12 ap-p<=6

--- src/search.cpp
+++ src/search.cpp
@@ -1105,7 +1105,7 @@ moves_loop: // When in check, search starts here
           else if (   givesCheck
                    && depth > 9
                    && abs(ss->staticEval) > 87)
-              extension = 1;
+              extension = (pos.count<ALL_PIECES>() <= 12 && (pos.count<ALL_PIECES>() - pos.count<PAWN>() <= 6)) ? 2 : 1;
 
           // Quiet ttMove extensions (~1 Elo)
           else if (   PvNode
