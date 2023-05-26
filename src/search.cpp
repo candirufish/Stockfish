1603f40d5d4ae4faafaa05830110e2ef4e0c4ec7
candirufish <38038147+candirufish@users.noreply.github.com>
2023-05-26 10:46:20 +0200

    ss-1 null lmrext 0

--- src/search.cpp
+++ src/search.cpp
@@ -1185,10 +1185,11 @@ moves_loop: // When in check, search starts here
               || !capture
               || (cutNode && (ss-1)->moveCount > 1)))
       {
+          bool lmrnull = (ss-1)->currentMove == MOVE_NULL;
           // In general we want to cap the LMR depth search at newDepth, but when
           // reduction is negative, we allow this move a limited search extension
           // beyond the first move depth. This may lead to hidden double extensions.
-          Depth d = std::clamp(newDepth - r, 1, newDepth + 1);
+          Depth d = std::clamp(newDepth - r, 1, newDepth + !lmrnull);
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
