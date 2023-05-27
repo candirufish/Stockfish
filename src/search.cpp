8cf55ec253a61fc136611642a9eaa830d3cfd03f
candirufish <38038147+candirufish@users.noreply.github.com>
2023-05-27 16:03:22 +0200

    d>=2 null r++

--- src/search.cpp
+++ src/search.cpp
@@ -1186,7 +1186,7 @@ moves_loop: // When in check, search starts here
               || (cutNode && (ss-1)->moveCount > 1)))
       {
           if ((ss-1)->currentMove == MOVE_NULL)
-              r += 2;
+              r++;
 
           // In general we want to cap the LMR depth search at newDepth, but when
           // reduction is negative, we allow this move a limited search extension
