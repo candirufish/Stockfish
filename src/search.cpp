349f7b9c66cc844bee3218f0bbe6850b08487769
candirufish <38038147+candirufish@users.noreply.github.com>
2023-05-27 15:21:24 +0200

    s18 null r+2

--- src/search.cpp
+++ src/search.cpp
@@ -1224,7 +1224,7 @@ moves_loop: // When in check, search starts here
               r += 2;
 
           if ((ss-1)->currentMove == MOVE_NULL)
-              r++;
+              r += 2;
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth - (r > 3), !cutNode);
       }
