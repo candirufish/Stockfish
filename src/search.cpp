fb9eb3e8d45401f4cc771c5fe42b38c6b1a39b1c
candirufish <38038147+candirufish@users.noreply.github.com>
2023-05-29 03:09:35 +0200

    null nttm  lmrext 0 shallower 1

--- src/search.cpp
+++ src/search.cpp
@@ -1168,9 +1168,6 @@ moves_loop: // When in check, search starts here
       else if (move == ttMove)
           r--;
 
-      if (lmrnull)
-          r += 2;
-
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
                      + (*contHist[1])[movedPiece][to_sq(move)]
