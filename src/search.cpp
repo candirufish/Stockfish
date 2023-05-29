62039a4a20a25629ad4a364f45e477babffef793
candirufish <38038147+candirufish@users.noreply.github.com>
2023-05-30 00:07:02 +0200

    ss-1 null nttm r++

--- src/search.cpp
+++ src/search.cpp
@@ -1167,7 +1167,7 @@ moves_loop: // When in check, search starts here
           r--;
 
       if ((ss-1)->currentMove == MOVE_NULL && !ttMove)
-          r += 2;
+          r++;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
