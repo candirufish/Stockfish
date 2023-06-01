09bd5fe7b345e132f6ef5f79af97d707a42678de
candirufish <38038147+candirufish@users.noreply.github.com>
2023-06-02 01:44:26 +0200

    pr rd < 5

--- src/search.cpp
+++ src/search.cpp
@@ -969,6 +969,7 @@ moves_loop: // When in check, search starts here
 
       // Step 14. Pruning at shallow depth (~120 Elo). Depth conditions are important for mate finding.
       if (  !rootNode
+          && thisThread->rootDepth > 5
           && pos.non_pawn_material(us)
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
       {
