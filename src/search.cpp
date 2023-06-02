82b6f799bafaf7df929b5907721cbdb4b8101547
candirufish <38038147+candirufish@users.noreply.github.com>
2023-06-02 04:54:08 +0200

    d < sd/2 cnt--

--- src/search.cpp
+++ src/search.cpp
@@ -1325,7 +1325,7 @@ moves_loop: // When in check, search starts here
               }
           }
       }
-      else if (PvNode && depth < thisThread->selDepth / 2)
+      else if (depth < thisThread->selDepth / 2)
           ss->cutoffCnt--;
 
       // If the move is worse than some previously searched move, remember it to update its stats later
