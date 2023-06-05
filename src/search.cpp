3a3d916d35b0c853e5d2e17d02cec57e3e424fc1
candirufish <38038147+candirufish@users.noreply.github.com>
2023-06-05 05:38:14 +0200

    even deep mv ss-1 <=1

--- src/search.cpp
+++ src/search.cpp
@@ -1181,7 +1181,7 @@ moves_loop: // When in check, search starts here
               // Adjust full depth search based on LMR results - if result
               // was good enough search deeper, if it was bad enough search shallower
               const bool doDeeperSearch = value > (bestValue + 64 + 11 * (newDepth - d));
-              const bool doEvenDeeperSearch = value > alpha + 711 && ss->doubleExtensions <= 6;
+              const bool doEvenDeeperSearch = value > alpha + 711 && ss->doubleExtensions <= 6 && (ss-1)->moveCount <= 1;
               const bool doShallowerSearch = value < bestValue + newDepth;
 
               ss->doubleExtensions = ss->doubleExtensions + doEvenDeeperSearch;
