83507b3718bdda564ca097af9fb71bb4a6d17d1a
candirufish <38038147+candirufish@users.noreply.github.com>
2023-06-06 11:29:53 +0200

    s20 nply ss-135

--- src/search.cpp
+++ src/search.cpp
@@ -1297,12 +1297,18 @@ moves_loop: // When in check, search starts here
               }
               else
               {
+              bool plyMv =    (ss->ply & 1)
+                     && (   (ss-1)->moveCount > 1
+                     || (ss-3)->moveCount > 1
+                     || (ss-5)->moveCount > 1);
+
                   // Reduce other moves if we have found at least one score improvement (~1 Elo)
                   // Reduce more for depth > 3 and depth < 12 (~1 Elo)
                   if (   depth > 1
+                      && !plyMv
                       && beta  <  14362
                       && value > -12393)
-                      depth -= ((depth > 3 && depth < 12) || (ss-1)->moveCount <= 1) ? 2 : 1;
+                      depth -= depth > 3 && depth < 12 ? 2 : 1;
 
                   assert(depth > 0);
                   alpha = value; // Update alpha! Always alpha < beta
