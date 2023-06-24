e3f44d3c80d2093437cccee6720cee6ef656cd9e
candirufish <38038147+candirufish@users.noreply.github.com>
2023-06-24 04:29:00 +0200

    ctcnt 1 + nttm or ttc npv

--- src/search.cpp
+++ src/search.cpp
@@ -1312,7 +1312,7 @@ moves_loop: // When in check, search starts here
 
               if (value >= beta)
               {
-                  ss->cutoffCnt += 1 + (!ttMove || ttCapture);
+                  ss->cutoffCnt += 1 + (!ttMove || (ttCapture && !PvNode));
                   assert(value >= beta); // Fail high
                   break;
               }
