9e5d58d0bcdb06963c29cd52235cd82ce84bf4e5
candirufish <38038147+candirufish@users.noreply.github.com>
2023-06-27 04:19:19 +0200

    s10 pv nttm d-5 else pv nrn nss1-ttm d<=8

--- src/search.cpp
+++ src/search.cpp
@@ -827,7 +827,10 @@ namespace {
     // Use qsearch if depth is equal or below zero (~9 Elo)
     if (    PvNode
         && !ttMove)
-        depth -= 2 + 2 * (ss->ttHit && tte->depth() >= depth);
+        depth -= 5 + 2 * (ss->ttHit && tte->depth() >= depth);
+
+    else if (PvNode && depth <= 8 && !rootNode && !(ss-1)->ttm)
+        depth += 3;
 
     if (depth <= 0)
         return qsearch<PV>(pos, ss, alpha, beta);
@@ -1226,7 +1229,7 @@ moves_loop: // When in check, search starts here
       {
           // Increase reduction for cut nodes and not ttMove (~1 Elo)
           if (!ttMove && cutNode)
-              r += 2 + !(ss-1)->ttm;
+              r += 2;
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth - (r > 3), !cutNode);
       }
