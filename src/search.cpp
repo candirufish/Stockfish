a96780ed26a99c50fde9abdf19e4e9d31a13b3ca
candirufish <38038147+candirufish@users.noreply.github.com>
2023-06-27 04:40:28 +0200

    ttc r+1-ss-1sql

--- src/search.cpp
+++ src/search.cpp
@@ -1153,7 +1153,7 @@ moves_loop: // When in check, search starts here
 
       // Increase reduction if ttMove is a capture (~3 Elo)
       if (ttCapture)
-          r += 1 + (ss-1)->singularQuietLMR;
+          r += 1 - (ss-1)->singularQuietLMR;
 
       // Decrease reduction for PvNodes based on depth (~2 Elo)
       if (PvNode)
