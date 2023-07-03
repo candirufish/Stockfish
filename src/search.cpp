af066379653f6824bfe14d01e3f4096a35e66ead
candirufish <38038147+candirufish@users.noreply.github.com>
2023-07-03 11:50:46 +0200

    s10 ss-1pv nrn ttm r--

--- src/search.cpp
+++ src/search.cpp
@@ -598,6 +598,7 @@ namespace {
     ss->doubleExtensions = (ss-1)->doubleExtensions;
     Square prevSq        = is_ok((ss-1)->currentMove) ? to_sq((ss-1)->currentMove) : SQ_NONE;
     ss->statScore        = 0;
+    ss->dr               = false;
 
     // Step 4. Transposition table lookup.
     excludedMove = ss->excludedMove;
@@ -826,7 +827,10 @@ namespace {
     // Use qsearch if depth is equal or below zero (~9 Elo)
     if (    PvNode
         && !ttMove)
-        depth -= 2 + 2 * (ss->ttHit && tte->depth() >= depth);
+    {
+        depth -= 3 + 2 * (ss->ttHit && tte->depth() >= depth);
+        ss->dr = true;
+    }
 
     if (depth <= 0)
         return qsearch<PV>(pos, ss, alpha, beta);
@@ -1161,7 +1165,12 @@ moves_loop: // When in check, search starts here
 
       // Decrease reduction for PvNodes based on depth (~2 Elo)
       if (PvNode)
+      {
           r -= 1 + 12 / (3 + depth);
+
+          if (!rootNode && ttMove && (ss-1)->dr)
+              r--;
+      }
 
       // Decrease reduction if ttMove has been singularly extended (~1 Elo)
       if (singularQuietLMR)
