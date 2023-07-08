d59b2a8ddd6b05fcc36f775506543fdfbe0f73ff
candirufish <38038147+candirufish@users.noreply.github.com>
2023-07-08 23:15:18 +0200

    s10 pv nttm d- + 2 * ev > bt + 80

--- src/search.cpp
+++ src/search.cpp
@@ -827,7 +827,7 @@ namespace {
     // Use qsearch if depth is equal or below zero (~9 Elo)
     if (    PvNode
         && !ttMove)
-        depth -= 2 + 2 * (ss->ttHit && tte->depth() >= depth) + (eval > beta + 80);
+        depth -= 2 + 2 * (ss->ttHit && tte->depth() >= depth) + 2 * (eval > beta + 80);
 
     if (depth <= 0)
         return qsearch<PV>(pos, ss, alpha, beta);
