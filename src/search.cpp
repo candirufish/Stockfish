5fa080b1be57cfc6ae520e83a3b5c3b22cde6a9c
candirufish <38038147+candirufish@users.noreply.github.com>
2023-07-22 13:29:02 +0200

    pv r-1 + 2* ntth ttd < d ttm

--- src/search.cpp
+++ src/search.cpp
@@ -1161,7 +1161,7 @@ moves_loop: // When in check, search starts here
 
       // Decrease reduction for PvNodes based on depth (~2 Elo)
       if (PvNode)
-          r -= 1 + (!ss->ttHit && tte->depth() < depth && ttMove);
+          r -= 1 + 2 * (!ss->ttHit && tte->depth() < depth && ttMove);
 
       // Decrease reduction if ttMove has been singularly extended (~1 Elo)
       if (singularQuietLMR)
