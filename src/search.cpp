0cf03bc277c6f90736d7ffaaa039d659318dc334
candirufish <38038147+candirufish@users.noreply.github.com>
2023-07-24 11:25:11 +0200

    nttpv d<6 ev>b+80 nttm r++

--- src/search.cpp
+++ src/search.cpp
@@ -1174,6 +1174,12 @@ moves_loop: // When in check, search starts here
       else if (move == ttMove)
           r--;
 
+      if (    !ss->ttPv
+        && depth < 6
+        && eval > beta + 80
+        && !ttMove)
+          r++;
+
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
                      + (*contHist[1])[movedPiece][to_sq(move)]
