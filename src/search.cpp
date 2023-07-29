a4aadc09967e4b2592d6c2435930a25dd56ba6e5
candirufish <38038147+candirufish@users.noreply.github.com>
2023-07-29 02:00:42 +0200

    ngext npm ext -1

--- src/search.cpp
+++ src/search.cpp
@@ -1104,6 +1104,9 @@ moves_loop: // When in check, search starts here
               // If the eval of ttMove is less than value, we reduce it (negative extension) (~1 Elo)
               else if (ttValue <= value)
                   extension = -1;
+
+              else if (pos.non_pawn_material(us))
+                  extension = -1;
           }
 
           // Check extensions (~1 Elo)
