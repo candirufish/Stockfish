42430a884ea95f62afa7410d8f093d0c66ad5c4c
candirufish <38038147+candirufish@users.noreply.github.com>
2023-07-29 02:03:24 +0200

    ngext cn d>8d<17 or npm ext -3

--- src/search.cpp
+++ src/search.cpp
@@ -1099,13 +1099,10 @@ moves_loop: // When in check, search starts here
 
               // If we are on a cutNode, reduce it based on depth (negative extension) (~1 Elo)
               else if (cutNode)
-                  extension = depth > 8 && depth < 17 ? -3 : -1;
+                  extension = (depth > 8 && depth < 17 || pos.non_pawn_material(us)) ? -3 : -1;
 
               // If the eval of ttMove is less than value, we reduce it (negative extension) (~1 Elo)
               else if (ttValue <= value)
-                  extension = -1;
-
-              else if (pos.non_pawn_material(us))
                   extension = -1;
           }
 
