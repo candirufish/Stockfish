e43ffe7412b2330bc3843d4be8784727f3c7e918
candirufish <38038147+candirufish@users.noreply.github.com>
2023-08-03 14:31:03 +0200

    ss-1 mcp ss-1 null r-- else r++

--- src/search.cpp
+++ src/search.cpp
@@ -1174,6 +1174,14 @@ moves_loop: // When in check, search starts here
       if ((ss-1)->mcp && (ss-1)->currentMove == MOVE_NULL)
           r--;
 
+      if ((ss - 1)->currentMove == MOVE_NULL) 
+      {
+          if ((ss - 1)->mcp)
+             r--;
+          else
+             r++;
+      }
+
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
                      + (*contHist[1])[movedPiece][to_sq(move)]
