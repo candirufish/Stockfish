7c43abc9a1fe276afb38733940f51192dfe11f4b
candirufish <38038147+candirufish@users.noreply.github.com>
2023-08-11 05:29:27 +0200

    cn mod 2 0 ttm r+1

--- src/search.cpp
+++ src/search.cpp
@@ -1149,7 +1149,7 @@ moves_loop: // When in check, search starts here
 
       // Increase reduction for cut nodes (~3 Elo)
       if (cutNode)
-          r += depth % 2 == 0 && !ttMove ? 3 : 2;
+          r += depth % 2 == 0 && ttMove ? 1 : 2;
 
       // Increase reduction if ttMove is a capture (~3 Elo)
       if (ttCapture)
