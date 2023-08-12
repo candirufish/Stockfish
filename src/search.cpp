8cfe82d55b5b6201607663f8966648df7c071991
candirufish <38038147+candirufish@users.noreply.github.com>
2023-08-12 17:55:14 +0200

    else ss-1 ttpv lfl r++

--- src/search.cpp
+++ src/search.cpp
@@ -1141,7 +1141,10 @@ moves_loop: // When in check, search starts here
       // Decrease further on cutNodes. (~1 Elo)
       if (   ss->ttPv
           && !likelyFailLow)
-          r -= ((cutNode && tte->depth() >= depth + 3) || ((ss-2)->ttPv && !(ss-1)->ttPv)) ? 3 : 2;
+          r -= cutNode && tte->depth() >= depth + 3 ? 3 : 2;
+
+      else if ((ss-1)->ttPv && likelyFailLow)
+          r++;
 
       // Decrease reduction if opponent's move count is high (~1 Elo)
       if ((ss-1)->moveCount > 8)
