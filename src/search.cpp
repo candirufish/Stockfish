1d21ea49cd15442bdd705b582783ea9027779e57
candirufish <38038147+candirufish@users.noreply.github.com>
2023-08-13 03:11:16 +0200

    mcp nimp npv r++

--- src/search.cpp
+++ src/search.cpp
@@ -1147,6 +1147,9 @@ moves_loop: // When in check, search starts here
       if ((ss-1)->moveCount > 8)
           r--;
 
+      if (moveCountPruning && !improving && !PvNode)
+          r++;
+
       // Increase reduction for cut nodes (~3 Elo)
       if (cutNode)
           r += 2;
