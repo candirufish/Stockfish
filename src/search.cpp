964cc944ae8810f999b690d4293a07fc55524dbf
candirufish <38038147+candirufish@users.noreply.github.com>
2023-08-14 11:21:04 +0200

    fpr cap + 64 bv>se

--- src/search.cpp
+++ src/search.cpp
@@ -985,7 +985,8 @@ moves_loop: // When in check, search starts here
               if (   !givesCheck
                   && lmrDepth < 7
                   && !ss->inCheck
-                  && ss->staticEval + 197 + 248 * lmrDepth + PieceValue[pos.piece_on(to_sq(move))]
+                  && ss->staticEval + 197 + 248 * lmrDepth + PieceValue[pos.piece_on(to_sq(move))] 
+                   + 64 * (bestValue > ss->staticEval)
                    + captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] / 7 < alpha)
                   continue;
 
