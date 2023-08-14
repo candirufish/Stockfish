34d21a555a45733670e0dfbbd6655c833ba9fb25
candirufish <38038147+candirufish@users.noreply.github.com>
2023-08-14 03:43:42 +0200

    ncn ntttpv ss-1ttpv lfl r++

--- src/search.cpp
+++ src/search.cpp
@@ -1124,6 +1124,9 @@ moves_loop: // When in check, search starts here
           && !likelyFailLow)
           r -= cutNode && tte->depth() >= depth + 3 ? 3 : 2;
 
+      if (!cutNode && !ss->ttPv && (ss-1)->ttPv && likelyFailLow)
+          r++;
+
       // Decrease reduction if opponent's move count is high (~1 Elo)
       if ((ss-1)->moveCount > 8)
           r--;
