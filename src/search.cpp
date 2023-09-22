f8c5a8c516b517d4a2b31d83a0f553d8cd74d4c4
candirufish <38038147+candirufish@users.noreply.github.com>
2023-09-22 09:14:33 +0200

    max-min se024 >32 r--

--- src/search.cpp
+++ src/search.cpp
@@ -1175,6 +1175,13 @@ moves_loop: // When in check, search starts here
       // Decrease/increase reduction for moves with a good/bad history (~25 Elo)
       r -= ss->statScore / (11124 + 4740 * (depth > 5 && depth < 22));
 
+      if (improving && !ss->inCheck && !(ss-2)->inCheck && !(ss-4)->inCheck)
+      {
+      int range = std::max({(ss-4)->staticEval, (ss-2)->staticEval, ss->staticEval}) - 
+          std::min({(ss-4)->staticEval, (ss-2)->staticEval, ss->staticEval});
+      if (range > 32)
+          r--;
+      }
       // Step 17. Late moves reduction / extension (LMR, ~117 Elo)
       // We use various heuristics for the sons of a node after the first son has
       // been searched. In general, we would like to reduce them, but there are many
