b76ca86ae3d9caaf51eed54e14120be02b34df87
candirufish <38038147+candirufish@users.noreply.github.com>
2023-09-23 18:03:36 +0200

    shallow nd + pv

--- src/search.cpp
+++ src/search.cpp
@@ -1199,7 +1199,7 @@ moves_loop: // When in check, search starts here
               // was good enough search deeper, if it was bad enough search shallower
               const bool doDeeperSearch = value > (bestValue + 64 + 11 * (newDepth - d));
               const bool doEvenDeeperSearch = value > alpha + 711 && ss->doubleExtensions <= 6;
-              const bool doShallowerSearch = value < bestValue + newDepth + 1;
+              const bool doShallowerSearch = value < bestValue + newDepth + PvNode;
 
               ss->doubleExtensions = ss->doubleExtensions + doEvenDeeperSearch;
 
