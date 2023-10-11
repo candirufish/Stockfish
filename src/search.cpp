d5a9120265883a526792c87504aed6f7304434ef
candirufish <38038147+candirufish@users.noreply.github.com>
2023-10-11 11:11:16 +0200

    s10 cnt 3 nttm ev>ap - 256 d-2

--- src/search.cpp
+++ src/search.cpp
@@ -842,8 +842,13 @@ namespace {
         && !ttMove)
         depth -= 2 + 2 * (ss->ttHit && tte->depth() >= depth);
 
+    if ((ss+1)->cutoffCnt > 3
+        && eval <= alpha - 256
+        && !ttMove)
+        depth -= 2;
+
     if (depth <= 0)
-        return qsearch<PV>(pos, ss, alpha, beta);
+        return qsearch<PvNode ? PV : NonPV>(pos, ss, alpha, beta);
 
     if (    cutNode
         &&  depth >= 8
