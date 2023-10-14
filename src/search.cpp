29f2d8970353dd2d9915cfaae5e4d9f2b8cc79c3
candirufish <38038147+candirufish@users.noreply.github.com>
2023-10-14 18:26:01 +0200

    mv cm stat >= 0 r--

--- src/search.cpp
+++ src/search.cpp
@@ -1180,8 +1180,8 @@ moves_loop: // When in check, search starts here
       // Decrease/increase reduction for moves with a good/bad history (~25 Elo)
       r -= ss->statScore / (10216 + 3855 * (depth > 5 && depth < 23));
 
-      if (move == ss->killers[0] && ss->statScore >= 0)
-          r -= 2;
+      if (move == countermove)
+          r--;
 
       // Step 17. Late moves reduction / extension (LMR, ~117 Elo)
       // We use various heuristics for the sons of a node after the first son has
