5f4aa37677cb2ea0817eb47fcc959831cb9b4131
candirufish <38038147+candirufish@users.noreply.github.com>
2023-10-15 07:58:21 +0200

    mv cm r--

--- src/search.cpp
+++ src/search.cpp
@@ -1180,7 +1180,7 @@ moves_loop: // When in check, search starts here
       // Decrease/increase reduction for moves with a good/bad history (~25 Elo)
       r -= ss->statScore / (10216 + 3855 * (depth > 5 && depth < 23));
 
-      if (move == countermove && ss->statScore >= 0)
+      if (move == countermove)
           r--;
 
       // Step 17. Late moves reduction / extension (LMR, ~117 Elo)
