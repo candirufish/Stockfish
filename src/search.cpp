16d8f446ae6701c9d6c053497c03ba6efad79ced
candirufish <38038147+candirufish@users.noreply.github.com>
2023-10-29 01:00:36 +0200

    cnt + 1 nttm qscnt>4

--- src/search.cpp
+++ src/search.cpp
@@ -1284,7 +1284,7 @@ moves_loop:  // When in check, search starts here
 
                 if (value >= beta)
                 {
-                    ss->cutoffCnt += 1 + !ttMove + ((ss + 1)->qsCutoffCnt > 3);
+                    ss->cutoffCnt += 1 + !ttMove + ((ss + 1)->qsCutoffCnt > 4);
                     assert(value >= beta);  // Fail high
                     break;
                 }
