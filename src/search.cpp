6ab0bd50f1e45e19f8bdeae623f5b2e532db9faf
candirufish <38038147+candirufish@users.noreply.github.com>
2023-11-01 11:38:14 +0100

    qscnt>4 ncp ninc r++'

--- src/search.cpp
+++ src/search.cpp
@@ -1140,7 +1140,7 @@ moves_loop:  // When in check, search starts here
         if (move == (ss - 4)->currentMove && pos.has_repeated())
             r += 2;
 
-        if ((ss + 1)->qsCutoffCnt > 4)
+        if ((ss + 1)->qsCutoffCnt > 4 && !capture && !ss->inCheck)
             r++;
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
