dfaba7876d77d7352c9eda4d3a614f02de8f1622
candirufish <38038147+candirufish@users.noreply.github.com>
2023-11-01 10:49:14 +0100

    mv ttm cnt qscnt 0 r-2

--- src/search.cpp
+++ src/search.cpp
@@ -1122,7 +1122,7 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction for cut nodes (~3 Elo)
         if (cutNode)
-            r += 2 + ((ss + 1)->qsCutoffCnt > 4);
+            r += 2;
 
         // Increase reduction if ttMove is a capture (~3 Elo)
         if (ttCapture)
@@ -1146,7 +1146,7 @@ moves_loop:  // When in check, search starts here
 
         // Decrease reduction for first generated move (ttMove)
         else if (move == ttMove)
-            r--;
+            r -= (ss + 1)->qsCutoffCnt == 0 && (ss + 1)->cutoffCnt == 0 ? 2 : 1;
 
         ss->statScore = 2 * thisThread->mainHistory[us][from_to(move)]
                       + (*contHist[0])[movedPiece][to_sq(move)]
