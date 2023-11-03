70d6287cfa084ff707a91fdf5eb61bf0ee28faa8
candirufish <38038147+candirufish@users.noreply.github.com>
2023-11-03 11:49:35 +0100

    pv rstat max 0

--- src/search.cpp
+++ src/search.cpp
@@ -1138,7 +1138,7 @@ moves_loop:  // When in check, search starts here
         if (PvNode)
             {
             r--;
-            rStat = std::max(rStat, -1);
+            rStat = std::max(rStat, 0);
             }
 
         // Decrease reduction if a quiet ttMove has been singularly extended (~1 Elo)
