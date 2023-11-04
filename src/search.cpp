5e96c8dff3d3216c5cc90c15b0546c8979711d2b
candirufish <38038147+candirufish@users.noreply.github.com>
2023-11-04 12:49:03 +0100

    if gc d>9 ext 1

--- src/search.cpp
+++ src/search.cpp
@@ -1080,13 +1080,13 @@ moves_loop:  // When in check, search starts here
                     extension = -1;
             }
 
-            // Check extensions (~1 Elo)
-            else if (givesCheck && depth > 9)
-                extension = 1;
-
             // Quiet ttMove extensions (~1 Elo)
             else if (PvNode && move == ttMove && move == ss->killers[0]
                      && (*contHist[0])[movedPiece][to_sq(move)] >= 4194)
+                extension = 1;
+
+            // Check extensions (~1 Elo)
+            if (givesCheck && depth > 9)
                 extension = 1;
         }
 
