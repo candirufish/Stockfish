f899cea2f4737755ba472d95ab3c93fd2c030b7f
candirufish <38038147+candirufish@users.noreply.github.com>
2023-11-18 23:58:13 +0100

    ttc or ttinc r++

--- src/search.cpp
+++ src/search.cpp
@@ -1137,7 +1137,7 @@ moves_loop:  // When in check, search starts here
             r += 2;
 
         // Increase reduction if ttMove is a capture (~3 Elo)
-        if (ttCapture)
+        if (ttCapture || ttInCheck)
             r++;
 
         // Decrease reduction for PvNodes (~2 Elo)
