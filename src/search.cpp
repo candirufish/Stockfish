ee2dcbf43db2f7dae7808d71cce863dbb2ac2aea
candirufish <38038147+candirufish@users.noreply.github.com>
2023-11-22 08:19:18 +0100

    r0 mv ttm d>9

--- src/search.cpp
+++ src/search.cpp
@@ -1156,7 +1156,7 @@ moves_loop:  // When in check, search starts here
 
         // Set reduction to 0 for first picked move (ttMove) (~2 Elo)
         // Nullifies all previous reduction adjustments to ttMove and leaves only history to do them
-        else if (move == ttMove)
+        else if (move == ttMove && depth > 9)
             r = 0;
 
         ss->statScore = 2 * thisThread->mainHistory[us][from_to(move)]
