1f957caef23c65fe0f7ccbd043dc2b953515839c
candirufish <38038147+candirufish@users.noreply.github.com>
2023-12-09 00:07:38 +0100

    mcp inc ncp ngc r++

--- src/search.cpp
+++ src/search.cpp
@@ -1138,6 +1138,12 @@ moves_loop:  // When in check, search starts here
         if (ttCapture)
             r++;
 
+        if (moveCountPruning
+        && ss->inCheck
+        && !capture
+        && !givesCheck)
+            r++;
+
         // Decrease reduction for PvNodes (~2 Elo)
         if (PvNode)
             r--;
