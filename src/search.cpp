d10f5df253211d920b760c8e9fc5de5dee171215
candirufish <38038147+candirufish@users.noreply.github.com>
2023-12-20 11:42:36 +0100

    razor values tuned 135k

--- src/search.cpp
+++ src/search.cpp
@@ -767,7 +767,7 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
     // If eval is really low check with qsearch if it can exceed alpha, if it can't,
     // return a fail low.
     // Adjust razor margin according to cutoffCnt. (~1 Elo)
-    if (eval < alpha - 472 - (284 - 165 * ((ss + 1)->cutoffCnt > 3)) * depth * depth)
+    if (eval < alpha - 310 - (350 - 194 * ((ss + 1)->cutoffCnt > 4)) * depth * depth)
     {
         value = qsearch<NonPV>(pos, ss, alpha - 1, alpha);
         if (value < alpha)
