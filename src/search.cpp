eff6789eb81da373c8fe4e496060a08ed9e40789
candirufish <38038147+candirufish@users.noreply.github.com>
2024-02-17 16:58:03 +0100

    cnt>3 r+2

--- src/search.cpp
+++ src/search.cpp
@@ -1126,8 +1126,8 @@ moves_loop:  // When in check, search starts here
             r += 2;
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
-        if ((ss + 1)->cutoffCnt > (depth < 5 ? 2 : 3))
-            r++;
+        if ((ss + 1)->cutoffCnt > 3)
+            r += depth < 5 ? 2 : 1;
 
         // Set reduction to 0 for first picked move (ttMove) (~2 Elo)
         // Nullifies all previous reduction adjustments to ttMove and leaves only history to do them
