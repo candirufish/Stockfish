3b885d6326db6d5a59e3a0d67b34c10afdf7d667
candirufish <38038147+candirufish@users.noreply.github.com>
2024-03-03 03:14:24 +0100

    nd - r>3 or cnt>3 && d<5

--- src/search.cpp
+++ src/search.cpp
@@ -1177,7 +1177,7 @@ moves_loop:  // When in check, search starts here
                 r += 2;
 
             // Note that if expected reduction is high, we reduce search depth by 1 here (~9 Elo)
-            value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth - (r > 3 || (ss + 1)->cutoffCnt > 3), !cutNode);
+            value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth - (r > 3 || ((ss + 1)->cutoffCnt > 3 && depth < 5)), !cutNode);
         }
 
         // For PV nodes only, do a full PV search on the first move or after a fail high,
