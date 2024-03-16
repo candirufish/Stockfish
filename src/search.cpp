8c756b4727ff4f086d6568d484796eea0d083634
candirufish <38038147+candirufish@users.noreply.github.com>
2024-03-16 02:23:24 +0100

    bonus + se - bv > 512 pv

--- src/search.cpp
+++ src/search.cpp
@@ -1318,7 +1318,7 @@ moves_loop:  // When in check, search starts here
         int bonus = (depth > 5) + (PvNode || cutNode) + ((ss - 1)->statScore < -14446)
                   + ((ss - 1)->moveCount > 11)
                   + (!ss->inCheck && bestValue <= ss->staticEval - 150)
-                  + (abs(ss->staticEval - bestValue) > 512);
+                  + (PvNode && abs(ss->staticEval - bestValue) > 512);
         update_continuation_histories(ss - 1, pos.piece_on(prevSq), prevSq,
                                       stat_bonus(depth) * bonus);
         thisThread->mainHistory[~us][((ss - 1)->currentMove).from_to()]
