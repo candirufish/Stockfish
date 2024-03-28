a33c1d9ba689830716387d1c26a49fed92bdba70
candirufish <38038147+candirufish@users.noreply.github.com>
2024-03-28 05:22:38 +0100

    cnt +1 +nttm +ncp

--- src/search.cpp
+++ src/search.cpp
@@ -1262,7 +1262,7 @@ moves_loop:  // When in check, search starts here
 
                 if (value >= beta)
                 {
-                    ss->cutoffCnt += 1 + !ttMove;
+                    ss->cutoffCnt += 1 + !ttMove + !capture;
                     assert(value >= beta);  // Fail high
                     break;
                 }
