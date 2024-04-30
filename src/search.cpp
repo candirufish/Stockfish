e1ff2e83d5c7c286bb6349dcca4b2f689f7bebec
candirufish <38038147+candirufish@users.noreply.github.com>
2024-05-01 01:12:51 +0200

    else cnt-- max 0

--- src/search.cpp
+++ src/search.cpp
@@ -1298,6 +1298,12 @@ moves_loop:  // When in check, search starts here
                 }
             }
         }
+      else
+      {
+         ss->cutoffCnt--;
+         ss->cutoffCnt = std::max(ss->cutoffCnt, 0);
+      }
+
 
         // If the move is worse than some previously searched move,
         // remember it, to update its stats later.
