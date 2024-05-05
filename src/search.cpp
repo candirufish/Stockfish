8a0ae3350c5903123f30815f92ece665f01395de
candirufish <38038147+candirufish@users.noreply.github.com>
2024-05-05 12:06:45 +0200

    ttm pv or cn tted<=d-6 r+2

--- src/search.cpp
+++ src/search.cpp
@@ -1140,7 +1140,7 @@ moves_loop:  // When in check, search starts here
             r--;
 
         if (ttMove && (PvNode || cutNode) && tte->depth() <= depth - 6)
-            r++;
+            r += 2;
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
         if ((ss + 1)->cutoffCnt > 3)
