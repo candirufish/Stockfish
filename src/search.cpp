2f9b853dd90104d5cd49390fe66620062bbea863
candirufish <38038147+candirufish@users.noreply.github.com>
2024-05-06 15:11:57 +0200

    s10 pv nttm prcap d-1

--- src/search.cpp
+++ src/search.cpp
@@ -820,8 +820,8 @@ Value Search::Worker::search(
 
     // Step 10. Internal iterative reductions (~9 Elo)
     // For PV nodes without a ttMove, we decrease depth by 3.
-    if (PvNode && !ttMove && !priorCapture)
-        depth -= 3;
+    if (PvNode && !ttMove)
+        depth -= priorCapture ? 1 : 3;
 
     // Use qsearch if depth <= 0.
     if (depth <= 0)
