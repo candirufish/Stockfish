f04472f482a6b710c4934f0015c6a7730fb039ca
candirufish <38038147+candirufish@users.noreply.github.com>
2024-05-19 18:57:24 +0200

    s10 ttcap ttd<=d-7 qs

--- src/search.cpp
+++ src/search.cpp
@@ -824,9 +824,12 @@ Value Search::Worker::search(
     if (PvNode && !ttMove)
         depth -= 3;
 
+    if (ttCapture && tte->depth() <= depth - 7)
+        depth--;
+
     // Use qsearch if depth <= 0.
     if (depth <= 0)
-        return qsearch<PV>(pos, ss, alpha, beta);
+        return qsearch < PvNode ? PV : NonPV > (pos, ss, alpha, beta);
 
     // For cutNodes without a ttMove, we decrease depth by 2 if depth is high enough.
     if (cutNode && depth >= 8 && (!ttMove || tte->bound() == BOUND_UPPER))
